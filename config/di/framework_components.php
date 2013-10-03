80a940f236db44d07f33d4649ae1a94e734ea6a0
Richard Heelin <richardh@channelgrabber.com>
2013-10-03 22:20:14 +0100

    CGIV-336: Updated composer files to zf2 version

--- config/di/framework_components.php
+++ config/di/framework_components.php
@@ -1,5 +1,5 @@
 <?php
-$frameworkComponents = array(
+return array(
     'zendframework_zendframework_library_Zend_ServiceManager',
     'zendframework_zendframework_library_Zend_Di',
     'zendframework_zendframework_library_Zend_Db',
