e67b5d0d50ee5681a64f487a8bf34f705a5dd751
Richard Heelin <richardh@channelgrabber.com>
2013-11-21 12:08:46 +0000

    CGIV-843: Added layout handler event to add link routes to views

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -16,6 +16,8 @@ use Zend\ModuleManager\ModuleManager;
 use Zend\ModuleManager\ModuleEvent;
 use CG\UI\Links\RoutesInterface;
 use CG\UI\Links\NavigationRoutesInterface;
+use Zend\Mvc\Router\RouteMatch;
+use Zend\View\Model\ViewModel;
 
 class Module
 {
@@ -30,8 +32,11 @@ class Module
 
     public function onBootstrap(MvcEvent $e)
     {
-        $eventManager        = $e->getApplication()->getEventManager();
+        $eventManager = $e->getApplication()->getEventManager();
+
+        $eventManager->attach(MvcEvent::EVENT_RENDER, array($this, 'layoutHandler'));
         $eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, array($this, 'httpExceptionHandler'));
+
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
     }
@@ -62,6 +67,46 @@ class Module
                 $this->navigationRoutes[] = $module;
             }
         }
+    }
+
+    public function layoutHandler(MvcEvent $event)
+    {
+        $viewModel = $event->getViewModel();
+        if (!($viewModel instanceof ViewModel)) {
+            return;
+        }
+
+        $viewModel->setVariable('route', $event->getRouteMatch());
+        $viewModel->setVariable('routePath', $this->getRoutePath($event->getRouteMatch()));
+
+        $routes = array();
+        foreach ($this->routes as $route) {
+            $routes = array_merge($routes, $route->getLinkRoutes());
+        }
+        $viewModel->setVariable('routes', $routes);
+
+        $navigationRoutes = array();
+        foreach ($this->navigationRoutes as $navigationRoute) {
+            $navigationRoutes = array_merge($navigationRoutes, $navigationRoute->getNavigationRoutes());
+        }
+        $viewModel->setVariable('navigationRoutes', $navigationRoutes);
+    }
+
+    protected function getRoutePath(RouteMatch $routeMatch = null)
+    {
+        $routePath = array();
+        $fullPath = array();
+
+        if ($routeMatch === null) {
+            return $routePath;
+        }
+
+        foreach (explode('/', $routeMatch->getMatchedRouteName()) as $route) {
+            $fullPath[] = $route;
+            $routePath[implode('/', $fullPath)] = $route;
+        }
+
+        return $routePath;
     }
 
     public function httpExceptionHandler(MvcEvent $event)
