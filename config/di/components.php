5899e99188951ddff6a5c36629cccc366397c504
Richard Heelin <richardh@channelgrabber.com>
2013-12-17 10:33:35 +0000

    CGIV-1110: Added missing components for channelgrabber/zf2-ssoclient

--- config/di/components.php
+++ config/di/components.php
@@ -7,7 +7,9 @@ $libraryComponents = array();
 $vendorComponents = array_merge($frameworkComponents, array(
     'nocarrier_hal_src_Nocarrier',
     'guzzle_guzzle_src_Guzzle',
-    'channelgrabber_stdlib_CG_Stdlib'
+    'channelgrabber_stdlib_CG_Stdlib',
+    'channelgrabber_sso_CG_Sso',
+    'channelgrabber_guzzle_CG_Guzzle'
 ));
 
 $components = array_merge($libraryComponents, $vendorComponents, $phpInternalComponents);
