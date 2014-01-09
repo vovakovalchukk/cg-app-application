a05d6cf1fdc9316a3ad4be9b7a31ddda55a14e43
Richard Heelin <richardh@channelgrabber.com>
2014-01-09 11:01:16 +0000

    CGIV-1207: Ajax route will now return a simple ajax view

--- module/Orders/config/module.config.php
+++ module/Orders/config/module.config.php
@@ -1,4 +1,6 @@
 <?php
+use Orders\Controller;
+
 return [
     'router' => [
         'routes' => [
@@ -26,13 +28,19 @@ return [
         ],
     ],
     'controllers' => [
-        'invokables' => [
-            'Orders\Controller\Orders' => 'Orders\Controller\OrdersController',
+        'factories' => [
+            'Orders\Controller\Orders' => function($controllerManager) {
+                return $controllerManager->getServiceLocator()->get(Controller\OrdersController::Class);
+            },
         ],
+        'invokables' => [],
     ],
     'view_manager' => [
         'template_path_stack' => [
             __DIR__ . '/../view',
+        ],
+        'strategies' => [
+            'ViewJsonStrategy',
         ],
     ],
 ];
--- module/Orders/src/Orders/Controller/OrdersController.php
+++ module/Orders/src/Orders/Controller/OrdersController.php
@@ -2,11 +2,30 @@
 namespace Orders\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use CG_UI\View\Prototyper\JsonModelFactory;
 
 class OrdersController extends AbstractActionController
 {
+    protected $jsonModelFactory;
+
+    public function __construct(JsonModelFactory $jsonModelFactory)
+    {
+        $this->setJsonModelFactory($jsonModelFactory);
+    }
+
+    public function setJsonModelFactory(JsonModelFactory $jsonModelFactory)
+    {
+        $this->jsonModelFactory = $jsonModelFactory;
+        return $this;
+    }
+
+    public function getJsonModelFactory()
+    {
+        return $this->jsonModelFactory;
+    }
+
     public function listAction()
     {
-
+        return $this->getJsonModelFactory()->newInstance();
     }
 }
