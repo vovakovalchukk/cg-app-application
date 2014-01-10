2dc3298753fe5a98aff265e6db02679eab472086
Richard Heelin <richardh@channelgrabber.com>
2014-01-10 14:56:02 +0000

    CGIV-1206: Added bulk actions to orders page

--- module/Orders/src/Orders/Controller/OrdersController.php
+++ module/Orders/src/Orders/Controller/OrdersController.php
@@ -62,6 +62,71 @@ class OrdersController extends AbstractActionController
         $settings->setSource($this->url()->fromRoute('Orders/ajax'));
         $view->addChild($ordersTable, 'ordersTable');
 
+        $bulkItems = $this->getViewModelFactory()->newInstance(
+            [
+                'bulkActions' => [
+                    [
+                        'title' => 'Print',
+                        'class' => 'print',
+                        'sub-actions' => [
+                            ['title' => 'Invoices',         'action' => 'invoices'],
+                            ['title' => 'Invoices by SKU',  'action' => 'invoices-sku'],
+                            ['title' => 'Invoices by Title','action' => 'invoices-title'],
+                            ['title' => 'Picking List',     'action' => 'picking-list'],
+                            ['title' => 'Thermal Print',    'action' => 'thermal-print']
+                        ]
+                    ],
+                    [
+                        'title' => 'Dispatch',
+                        'class' => 'dispatch'
+                    ],
+                    [
+                        'title' => 'Tag / Untag',
+                        'class' => 'tag-untag',
+                        'sub-actions' => [
+                            ['title' => 'Example 1',         'action' => 'example'],
+                            ['title' => 'Example 2',         'action' => 'example']
+                        ]
+                    ],
+                    [
+                        'title' => 'Download',
+                        'class' => 'download',
+                        'sub-actions' => [
+                            ['title' => 'Basic VAT Report', 'action' => 'vat-report']
+                        ]
+                    ],
+                    [
+                        'title' => 'Courier',
+                        'class' => 'courier',
+                        'sub-actions' => [
+                            ['title' => 'Create Royal Mail CSV',      'action' => 'add-tracking-numbers'],
+                            ['title' => 'Create Royal Mail CSV',      'action' => 'royal-mail-csv'],
+                            ['title' => 'Fulfillment by Amazon',      'action' => 'fulfillment-by-amazon'],
+                            ['title' => 'Print DPD Labels',           'action' => 'print-dpd-labels'],
+                            ['title' => 'Print eParcel Label',        'action' => 'print-eparcel-labels'],
+                            ['title' => 'Print Interlink Labels',     'action' => 'print-interlink-labels'],
+                            ['title' => 'Print ParcelForce Labels',   'action' => 'print-parcelforce-labels'],
+                            ['title' => 'Print UPS Labels',           'action' => 'print-ups-labels'],
+                            ['title' => 'Print TNT Labels',           'action' => 'print-tnt-labels'],
+                            ['title' => 'Print Yodel Labels',         'action' => 'print-yodel-labels'],
+                            ['title' => 'UKMail Export',              'action' => 'ukmail-export']
+                        ]
+                    ],
+                    [
+                        'title' => 'Batch',
+                        'class' => 'batch',
+                        'sub-actions' => [
+                            ['title' => 'Archive',                    'action' => 'archive'],
+                            ['title' => 'Mark / Unmark as Printed',   'action' => 'mark-printed'],
+                            ['title' => 'Mark / Unmark as Packing',   'action' => 'mark-packing'],
+                        ]
+                    ]
+                ]
+            ]
+        );
+        $bulkItems->setTemplate('orders/orders/bulk-actions');
+        $view->addChild($bulkItems, 'bulkItems');
+
         return $view;
     }
 
