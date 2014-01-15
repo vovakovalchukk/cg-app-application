94fcc03f51cf161ba7905edc5fd632b672af8952
M King <matthew.king@channelgrabber.com>
2014-01-15 17:00:05 +0000

    CGIV-1335 Code review changes

--- module/Orders/Module.php
+++ module/Orders/Module.php
@@ -11,6 +11,8 @@ namespace Orders;
 
 class Module
 {
+    const PUBLIC_FOLDER = '/channelgrabber/zf2-v4-ui/';
+
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
