97f7eff94b5e81e1f31e91bb1959862e752159f9
M King <matthew.king@channelgrabber.com>
2014-01-20 15:09:29 +0000

    CGIV-1336 convert ot using order batch

--- module/Orders/src/Orders/Controller/BatchController.php
+++ module/Orders/src/Orders/Controller/BatchController.php
@@ -3,149 +3,36 @@ namespace Orders\Controller;
 
 use CG\Stdlib\Exception\Runtime\NotFound;
 use Zend\Mvc\Controller\AbstractActionController;
-use CG\User\ActiveUserInterface;
-use CG\Order\Shared\StorageInterface as OrderInterface;
-use CG\Order\Shared\Batch\StorageInterface as BatchInterface;
-use CG\OrganisationUnit\StorageInterface as OrganisationUnitInterface;
-use CG\Order\Service\Filter\Entity as FilterEntity;
-use CG\Order\Shared\Batch\Entity as BatchEntity;
-use Zend\Di\Di;
+use Orders\Order\Batch\Service as BatchService;
 
 class BatchController extends AbstractActionController
 {
-    protected $activeUserContainer;
-    protected $orderClient;
-    protected $organisationUnitClient;
-    protected $batchClient;
+    protected $batchService;
 
-    const DEFAULT_LIMIT = "ALL";
-    const DEFAULT_PAGE = 0;
-    const DEFAULT_INCLUDE_ARCHIVED = 1;
-
-    public function __construct(Di $di, ActiveUserInterface $activeUserContainer, OrderInterface $orderClient,
-                                OrganisationUnitInterface $organisationUnitClient, BatchInterface $batchClient)
+    public function __construct(BatchService $batchService)
     {
-        $this->setActiveUserContainer($activeUserContainer)
-            ->setOrderClient($orderClient)
-            ->setOrganisationUnitClient($organisationUnitClient)
-            ->setBatchClient($batchClient);
+        $this->setBatchService($batchService);
     }
 
     public function createAction()
     {
-        $userEntity = $this->getService()->getActiveUser();
-        $batch = $this->getDi()->get(BatchEntity::class, array(
-            "organisationUnitId" => $userEntity->getOrganisationUnitId()
-        ));
-        $batch = $this->getBatchApi()->save($batch);
-
-        try {
-            $organisationUnits = $this->getService()->getOrganisationUnitClient()->fetchFiltered(static::DEFAULT_LIMIT,
-                static::DEFAULT_PAGE, $userEntity->getOrganisationUnitId());
-        } catch (NotFound $exception) {
-            $organisationUnits = new \SplObjectStorage();
-        }
-        $organisationUnitIds = array($userEntity->getOrganisationUnitId());
-        foreach ($organisationUnits as $organisationUnit) {
-            $organisationUnitIds[] = $organisationUnit->getId();
-        }
         $orderIds = $this->params('orderIds');
-        $filterEntity = $this->getDi()->get(FilterEntity::class, array(
-            "limit" => static::DEFAULT_LIMIT,
-            "page" => static::DEFAULT_PAGE,
-            "id" => $orderIds,
-            "organisationUnitId" => $organisationUnitIds,
-            "status" => null,
-            "accountId" => null,
-            "channel" => null,
-            "country" => null,
-            "countryExclude" => null,
-            "shippingMethod" => null,
-            "searchTerm" => null,
-            "includeArchived" => static::DEFAULT_INCLUDE_ARCHIVED,
-            "multiLineSameOrder" => null,
-            "multiSameItem" => null,
-            "timeFrom" => null,
-            "timeTo" => null,
-            "orderBy" => null,
-            "orderDirection" => null,
-            "batch" => null
-        ));
-        $orders = $this->getOrderApi()->fetchCollectionByFilter($filterEntity);
-        $rollback = array();
-        try {
-            foreach ($orders as $index => $order) {
-                $rollback[$index] = $order->getBatch();
-                $order->setBatch($batch->getId());
-                $this->getOrderApi()->save($order);
-            }
-        }
-        catch (\Exception $e) {
-            try {
-                foreach ($rollback as $index => $batchId) {
-                    $orders[$index]->setBatch($batchId);
-                    $this->getOrderApi()->save($orders[$index]);
-                }
-                $this->delete($batchId->getId());
-            } catch (\Exception $e) {
-                //Shits Really Hit The Fan
-            }
-            throw $e;
-        }
+        $this->getBatchService()->create($orderIds);
     }
 
     public function deleteAction($batchId)
     {
-        $this->delete($batchId);
+        $this->getBatchService()->delete($batchId);
     }
 
-    protected function delete($batchId)
+    public function setBatchService(BatchService $batchService)
     {
-        $entity = $this->getBatchApi()->fetch($batchId);
-        $this->getBatchApi()->remove($entity);
-    }
-
-    public function setActiveUserContainer(ActiveUserInterface $activeUserContainer)
-    {
-        $this->activeUserContainer = $activeUserContainer;
+        $this->batchService = $batchService;
         return $this;
     }
 
-    public function getActiveUserContainer()
+    public function getBatchService()
     {
-        return $this->activeUserContainer;
-    }
-
-    public function setBatchClient(BatchInterface $batchClient)
-    {
-        $this->batchClient = $batchClient;
-        return $this;
-    }
-
-    public function getBatchClient()
-    {
-        return $this->batchClient;
-    }
-
-    public function setOrderClient(OrderInterface $orderClient)
-    {
-        $this->orderClient = $orderClient;
-        return $this;
-    }
-
-    public function getOrderClient()
-    {
-        return $this->orderClient;
-    }
-
-    public function setOrganisationUnitClient(OrganisationUnitInterface $organisationUnitClient)
-    {
-        $this->organisationUnitClient = $organisationUnitClient;
-        return $this;
-    }
-
-    public function getOrganisationUnitClient()
-    {
-        return $this->organisationUnitClient;
+        return $this->batchService;
     }
 }
--- module/Orders/src/Orders/Order/Batch/Service.php
+++ module/Orders/src/Orders/Order/Batch/Service.php
@@ -4,20 +4,27 @@ namespace Orders\Order\Batch;
 use CG\Order\Shared\Batch\StorageInterface as BatchInterface;
 use CG\User\OrganisationUnit\Service as OrganisationUnitService;
 use CG\Stdlib\Exception\Runtime\NotFound;
+use CG\Order\Service\Filter;
+use CG\Order\Shared\Batch\Entity as BatchEntity;
+use CG\Order\Shared\StorageInterface as OrderInterface;
 
 class Service
 {
     protected $organisationUnitService;
     protected $batchClient;
+    protected $orderClient;
 
     const DEFAULT_LIMIT = 10;
     const DEFAULT_PAGE = 1;
     const ACTIVE = 1;
+    const DEFAULT_INCLUDE_ARCHIVED = 1;
 
-    public function __construct(OrganisationUnitService $organisationUnitService, BatchInterface $batchClient)
+    public function __construct(OrganisationUnitService $organisationUnitService, BatchInterface $batchClient,
+                                OrderInterface $orderClient)
     {
         $this->setOrganisationUnitService($organisationUnitService)
-            ->setBatchClient($batchClient);
+            ->setBatchClient($batchClient)
+            ->setOrderClient($orderClient);
     }
 
     public function getBatches()
@@ -30,6 +37,51 @@ class Service
             $batchCollection = new \SplObjectStorage();
         }
         return $batchCollection;
+    }
+
+    public function create($orderIds)
+    {
+        $userEntity = $this->getOrganisationUnitService()->getActiveUser();
+        $batch = $this->getDi()->get(BatchEntity::class, array(
+            "organisationUnitId" => $userEntity->getOrganisationUnitId()
+        ));
+        $batch = $this->getBatchClient()->save($batch);
+
+        $organisationUnitIds = $this->getOrganisationUnitService()->getAncestorOrganisationUnitIds();
+        $filterEntity = $this->getDi()->get(Filter::class, array(
+            "limit" => "all",
+            "page" => static::DEFAULT_PAGE,
+            "id" => $orderIds,
+            "organisationUnitId" => $organisationUnitIds,
+            "includeArchived" => static::DEFAULT_INCLUDE_ARCHIVED,
+        ));
+        $orders = $this->getOrderClient()->fetchCollectionByFilter($filterEntity);
+        $rollback = array();
+        try {
+            foreach ($orders as $index => $order) {
+                $rollback[$index] = $order->getBatch();
+                $order->setBatch($batch->getId());
+                $this->getOrderClient()->save($order);
+            }
+        }
+        catch (\Exception $e) {
+            try {
+                foreach ($rollback as $index => $batchId) {
+                    $orders[$index]->setBatch($batchId);
+                    $this->getOrderClient()->save($orders[$index]);
+                }
+                $this->delete($batchId->getId());
+            } catch (\Exception $e) {
+                //Shits Really Hit The Fan
+            }
+            throw $e;
+        }
+    }
+
+    public function delete($batchId)
+    {
+        $entity = $this->getBatchClient()->fetch($batchId);
+        $this->getBatchClient()->remove($entity);
     }
 
     public function setBatchClient(BatchInterface $batchClient)
@@ -52,5 +104,16 @@ class Service
     public function getOrganisationUnitService()
     {
         return $this->organisationUnitService;
+    }
+
+    public function setOrderClient(OrderInterface $orderClient)
+    {
+        $this->orderClient = $orderClient;
+        return $this;
+    }
+
+    public function getOrderClient()
+    {
+        return $this->orderClient;
     }
 }
