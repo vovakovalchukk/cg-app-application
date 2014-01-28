9fe356bf5d71562e84671cff6cf961049f2ff152
James Higgins <jamesh@channelgrabber.com>
2014-01-28 09:19:30 +0000

    CGIV-1430 Removing the table array, just using elements instead

--- module/Orders/test/data/accounting.php
+++ module/Orders/test/data/accounting.php
@@ -1,15 +1,13 @@
 <?php
 return [
-    'table' => [
-        'class' => 'table-med',
-        'headers' => [
-            ['title' => 'Invoice ID',        'class' => '' ],
-            ['title' => 'Description',     'class' => '' ],
-            ['title' => 'Total',     'class' => 'right' ]
-         ],
-        'actions' => [],
-        'rows' => [
-            ['123456789', 'Logitech Keyboard - Title: Black',  '&pound;4.20']
-        ]
+    'class' => 'table-med',
+    'headers' => [
+        ['title' => 'Invoice ID',        'class' => '' ],
+        ['title' => 'Description',     'class' => '' ],
+        ['title' => 'Total',     'class' => 'right' ]
+     ],
+    'actions' => [],
+    'rows' => [
+        ['123456789', 'Logitech Keyboard - Title: Black',  '&pound;4.20']
     ]
 ];
--- module/Orders/test/data/product-payment.php
+++ module/Orders/test/data/product-payment.php
@@ -1,17 +1,14 @@
 <?php
 return [
-    'table' =>
-    [
-        'headers' => [
-            [ 'title' => 'SKU',        'class' => '' ],
-            [ 'title' => 'Product Name',     'class' => ''],
-            [ 'title' => 'Qty', 'class' => 'right' ],
-            [ 'title' => 'Line Total',     'class' => 'right' ]
-         ],
-        'actions' => [],
-        'rows' => [
-            [ 'BLACK25545432', 'Logitech Keyboard - Title: Black', '1','&pound;4.20' ],
-            [ 'BLACK25545435', 'Logitech Mouse - Title: Black',    '1','&pound;10.80' ]
-        ]
+    'headers' => [
+        [ 'title' => 'SKU',        'class' => '' ],
+        [ 'title' => 'Product Name',     'class' => ''],
+        [ 'title' => 'Qty', 'class' => 'right' ],
+        [ 'title' => 'Line Total',     'class' => 'right' ]
+     ],
+    'actions' => [],
+    'rows' => [
+        [ 'BLACK25545432', 'Logitech Keyboard - Title: Black', '1','&pound;4.20' ],
+        [ 'BLACK25545435', 'Logitech Mouse - Title: Black',    '1','&pound;10.80' ]
     ]
 ];
