b54480a545dedc5ddf06a492a96b500b55f895e6
Richard Heelin <richardh@channelgrabber.com>
2014-02-03 13:56:28 +0000

    CGIV-1392: Recreated Order Detail Bulk Actions using Di and new classes

--- module/Orders/config/bulk-actions.config.php
+++ module/Orders/config/bulk-actions.config.php
@@ -1,13 +1,33 @@
 <?php
 use Orders\Order\BulkActions\Service;
 use CG_UI\View\BulkActions;
+use CG_UI\View\BulkActions\Action;
+use CG_UI\View\BulkActions\PrintAction;
+use CG_UI\View\BulkActions\DispatchAction;
+use CG_UI\View\BulkActions\TagAction;
 
 return [
     'di' => [
+        'definition' => [
+            'class' => [
+                BulkActions::class => [
+                    'methods' => [
+                        'addAction' => [
+                            'action' => [
+                                'required' => true,
+                                'type' => Action::class,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
         'instance' => [
             'aliases' => [
                 'BulkActions' => BulkActions::class,
                 'OrderDetailBulkActions' => BulkActions::class,
+                'CourierBulkAction' => Action::class,
+                'AccountingBulkAction' => Action::class,
             ],
             Service::class => [
                 'parameters' => [
@@ -21,7 +41,10 @@ return [
                         'id' => 'bulk-actions',
                         'class' => ['fixed-scroll'],
                     ],
-                ]
+                ],
+                'injections' => [
+
+                ],
             ],
             'OrderDetailBulkActions' => [
                 'parameters' => [
@@ -29,7 +52,28 @@ return [
                         'id' => 'bulk-actions',
                         'class' => ['bulk-actions-inline'],
                     ],
-                ]
+                ],
+                'injections' => [
+                    PrintAction::class,
+                    DispatchAction::class,
+                    TagAction::class,
+                    'CourierBulkAction',
+                    'AccountingBulkAction',
+                ],
+            ],
+            'CourierBulkAction' => [
+                'parameters' => [
+                    'icon' => 'courier',
+                    'title' => 'Courier',
+                    'action' => 'courier'
+                ],
+            ],
+            'AccountingBulkAction' => [
+                'parameters' => [
+                    'icon' => 'accounting',
+                    'title' => 'Accounting',
+                    'action' => 'accounting'
+                ],
             ],
         ],
     ],
