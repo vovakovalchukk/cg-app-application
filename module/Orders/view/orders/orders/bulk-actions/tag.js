7a800f297944fb2062699c2682cf911690d515ba
Richard Heelin <richardh@channelgrabber.com>
2014-02-04 10:20:06 +0000

    CGIV-1237: Readded javascript from previous attempt

--- module/Orders/view/orders/orders/bulk-actions/tag.js
+++ module/Orders/view/orders/orders/bulk-actions/tag.js
@@ -1,3 +1,28 @@
 $("#<?= $id ?>").bulkActions("set", "tag", function(event) {
-    alert("Hello World");
+    var orders = [<?= isset($order) ? '"' . $order->getId() . '"' : '' ?>];
+    $("#datatable .order-id:checked").each(function() {
+        orders.push($(this).val());
+        });
+
+    if (!orders.length) {
+        return;
+    }
+
+    var tag = <?= isset($tag) ? '"' . $tag . '"' : '$.trim(window.prompt("Name of Tag:", "tag"))' ?>;
+    if (!tag.length) {
+        return;
+    }
+
+    $("#datatable_processing").css("visibility", "visible");
+    $.ajax({
+        'url': "<?= $this->url('Orders/tag') ?>",
+        'type': "POST",
+        'data': {
+            'tag': tag,
+            'orders': orders
+        },
+        'complete': function() {
+            $("#datatable").cgDataTable("redraw");
+        }
+    });
 });
