b2e2a96308db8206378777067108d9fb676c6d3a
Adam <adamb@channelgrabber.com>
2014-02-04 09:38:15 +0000

    CGIV-1395 generate mo file

--- module/Orders/config/module.config.php
+++ module/Orders/config/module.config.php
@@ -99,6 +99,16 @@ return [
             'Mustache\View\Strategy'
         ],
     ],
+    'translator' => array(
+        'locale' => 'en_US',
+        'translation_file_patterns' => array(
+            array(
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ),
+        ),
+    ),
     'di' => [
         'instance' => [
             'aliases' => [
