88f1a5282f4d66d30fc758b50d3ad927e8a307c2
Richard Heelin <richardh@channelgrabber.com>
2014-02-05 12:00:15 +0000

    CGIV-1237: Now using the data associated with the element

--- module/Orders/view/orders/orders/bulk-actions/tag.js
+++ module/Orders/view/orders/orders/bulk-actions/tag.js
@@ -1,13 +1,2 @@
 <?php $this->headScript()->appendFile($this->baseUrl . Orders\Module::PUBLIC_FOLDER . 'js/tag.js'); ?>
-$("#<?= $id ?>").bulkActions(
-    "set",
-    "<?= $action ?>",
-    function(event) {
-        var tagBulkAction = new TagBulkAction(
-            "<?= $this->url('Orders/tag') ?>",
-            <?= isset($tag) ? '"' . $tag . '"' : '$.trim(window.prompt("Name of Tag:", "tag"))' ?>,
-            <?= isset($order) ? '["' . $order->getId() . '"]' : '$("#datatable").cgDataTable("selected", ".order-id")' ?>
-        );
-        return tagBulkAction.action(event);
-    }
-);
+$("#<?= $id ?>").bulkActions("set", "<?= $action ?>", TagBulkAction);
--- public/channelgrabber/orders/js/tag.js
+++ public/channelgrabber/orders/js/tag.js
@@ -1,43 +1,38 @@
-var TagBulkAction = function(url, tag, orders) {
-    var url = url;
-    var tag = tag;
-    var orders = orders;
-
-    this.action = function(event) {
-        if (!tag.length) {
-            return;
-        }
-
-        if (!orders.length) {
-            return;
-        }
-
-        $("#datatable_processing").css("visibility", "visible");
-        $.ajax({
-            context: this,
-            url: url,
-            type: "POST",
-            dataType: 'json',
-            data: {
-                'orders': orders,
-                'tag': tag
-            },
-            success : function(data) {
-                if (data.tagged || !data.error) {
-                    return
-                }
-                error(data.error);
-            },
-            error: function() {
-                error("Network Error");
-            },
-            complete: function() {
-                $("#datatable").cgDataTable("redraw");
-            }
-        });
-    };
-
+var TagBulkAction = function(event) {
     var error = function(error) {
         alert(error);
     }
+
+    var orders = $("#datatable").cgDataTable("selected", ".order-id");
+    if (!orders.length) {
+        return;
+    }
+
+    var tag = $(this).data("tag") || $.trim(window.prompt("Name of Tag:", "tag"));
+    if (!tag.length) {
+        return;
+    }
+
+    $("#datatable_processing").css("visibility", "visible");
+    $.ajax({
+        url: $(this).data("url"),
+        type: "POST",
+        dataType: 'json',
+        data: {
+            'orders': orders,
+            'tag': tag
+        },
+        success : function(data) {
+            if (data.tagged || !data.error) {
+                return
+            }
+            error(data.error);
+        },
+        error: function() {
+            error("Network Error");
+        },
+        complete: function() {
+            $("#datatable").cgDataTable("redraw");
+        }
+    });
 };
