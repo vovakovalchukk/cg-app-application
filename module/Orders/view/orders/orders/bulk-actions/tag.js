b12e91c4535d4ad90574df9cb049ec6d301bbab1
Richard Heelin <richardh@channelgrabber.com>
2014-02-06 11:41:44 +0000

    CGIV-1237: Converted js to use requirejs

--- module/Orders/view/orders/orders/bulk-actions/tag.js
+++ module/Orders/view/orders/orders/bulk-actions/tag.js
@@ -1,8 +1,17 @@
-<?php $this->headScript()->appendFile($this->baseUrl . Orders\Module::PUBLIC_FOLDER . 'js/tag.js'); ?>
+require.config({
+    paths: {
+        TagBulkAction: "<?= $this->baseUrl . Orders\Module::PUBLIC_FOLDER . 'js/tag' ?>"
+    }
+});
+require(
+    ["TagBulkAction"],
+    function(TagBulkAction) {
+        $("#<?= $id ?>").bulkActions("set", "<?= $action ?>", TagBulkAction);
+    }
+);
 <?php
 if(isset($order)) {
     $this->placeholder($id . '-' . $action)
-        ->append('data-orders="' . htmlentities(json_encode([$order->getId()]), ENT_QUOTES) . '"');
-}
+    ->append('data-orders="' . htmlentities(json_encode([$order->getId()]), ENT_QUOTES) . '"');
+    }
 ?>
-$("#<?= $id ?>").bulkActions("set", "<?= $action ?>", TagBulkAction);
--- public/channelgrabber/orders/js/tag.js
+++ public/channelgrabber/orders/js/tag.js
@@ -1,49 +1,51 @@
-var TagBulkAction = function(event) {
-    var error = function(error) {
-        alert(error);
-    }
-
-    var datatable = $(this).data("datatable");
-    var orders = $(this).data("orders");
-
-    if (!orders && datatable) {
-        orders = $("#" + datatable).cgDataTable("selected", ".order-id");
-    }
-
-    if (!orders.length) {
-        return;
-    }
-
-    var tag = $(this).data("tag") || $.trim(window.prompt("Name of Tag:", "tag"));
-    if (!tag.length) {
-        return;
-    }
-
-    if (datatable) {
-        $("#" + datatable + "_processing").css("visibility", "visible");
-    }
-
-    $.ajax({
-        url: $(this).data("url"),
-        type: "POST",
-        dataType: 'json',
-        data: {
-            'orders': orders,
-            'tag': tag
-        },
-        success : function(data) {
-            if (data.tagged || !data.error) {
-                return
-            }
-            error(data.error);
-        },
-        error: function() {
-            error("Network Error");
-        },
-        complete: function() {
-            if (datatable) {
-                $("#" + datatable).cgDataTable("redraw");
-            }
+define(function() {
+    return function(event) {
+        var error = function(error) {
+            alert(error);
         }
-    });
-};
+
+        var datatable = $(this).data("datatable");
+        var orders = $(this).data("orders");
+
+        if (!orders && datatable) {
+            orders = $("#" + datatable).cgDataTable("selected", ".order-id");
+        }
+
+        if (!orders.length) {
+            return;
+        }
+
+        var tag = $(this).data("tag") || $.trim(window.prompt("Name of Tag:", "tag"));
+        if (!tag.length) {
+            return;
+        }
+
+        if (datatable) {
+            $("#" + datatable + "_processing").css("visibility", "visible");
+        }
+
+        $.ajax({
+            url: $(this).data("url"),
+            type: "POST",
+            dataType: 'json',
+            data: {
+                'orders': orders,
+                'tag': tag
+            },
+            success : function(data) {
+                if (data.tagged || !data.error) {
+                    return
+                }
+                error(data.error);
+            },
+            error: function() {
+                error("Network Error");
+            },
+            complete: function() {
+                if (datatable) {
+                    $("#" + datatable).cgDataTable("redraw");
+                }
+            }
+        });
+    };
+});
