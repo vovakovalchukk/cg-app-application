adfeeec430e3a49d402aae095357ffae8f597684
Richard Heelin <richardh@channelgrabber.com>
2014-02-07 11:14:03 +0000

    CGIV-1284: TableService now adds tag columns to the orders data table

--- module/Orders/src/Orders/Order/TableService.php
+++ module/Orders/src/Orders/Order/TableService.php
@@ -1,17 +1,64 @@
 <?php
 namespace Orders\Order;
 
+use Zend\Di\Di;
+use CG\User\ActiveUserInterface;
+use CG\User\Entity as User;
 use CG_UI\View\DataTable;
 use CG\Order\Shared\Tag\StorageInterface as TagStorage;
+use CG\Stdlib\Exception\Runtime\NotFound;
+use CG\Order\Shared\Tag\Entity as Tag;
 
 class TableService
 {
+    protected $di;
+    protected $activeUserContainer;
     protected $tagClient;
     protected $ordersTable;
 
-    public function __construct(DataTable $ordersTable, TagStorage $tagClient)
+    public function __construct(Di $di, ActiveUserInterface $activeUserContainer, TagStorage $tagClient, DataTable $ordersTable)
     {
-        $this->setTagClient($tagClient)->setOrdersTable($ordersTable);
+        $this
+            ->setDi($di)
+            ->setActiveUserContainer($activeUserContainer)
+            ->setTagClient($tagClient)
+            ->setOrdersTable($ordersTable);
+    }
+
+    public function setDi(Di $di)
+    {
+        $this->di = $di;
+        return $this;
+    }
+
+    /**
+     * @return Di
+     */
+    public function getDi()
+    {
+        return $this->di;
+    }
+
+    public function setActiveUserContainer(ActiveUserInterface $activeUserContainer)
+    {
+        $this->activeUserContainer = $activeUserContainer;
+        return $this;
+    }
+
+    /**
+     * @return ActiveUserInterface
+     */
+    public function getActiveUserContainer()
+    {
+        return $this->activeUserContainer;
+    }
+
+    /**
+     * @return User
+     */
+    public function getActiveUser()
+    {
+        return $this->getActiveUserContainer()->getActiveUser();
     }
 
     public function setTagClient(TagStorage $tagClient)
@@ -31,11 +78,49 @@ class TableService
     public function setOrdersTable(DataTable $ordersTable)
     {
         $this->ordersTable = $ordersTable;
+        $this->configureOrdersTable();
         return $this;
     }
 
+    protected function configureOrdersTable()
+    {
+        $this->addTagColumns();
+    }
+
+    protected function addTagColumns()
+    {
+        try {
+            $tags = $this->getTagClient()->fetchCollectionAll(
+                1,
+                'all',
+                $this->getActiveUser()->getAvailableOrganisationUnitIds(),
+                []
+            );
+
+            foreach ($tags as $tag) {
+                $this->addTagColumn($tag);
+            }
+        } catch (NotFound $exception) {
+            // No Tags -- Nothing to do
+        }
+    }
+
+    protected function addTagColumn(Tag $tag)
+    {
+        $column = $this->getDi()->newInstance(
+            DataTable\Column::class,
+            [
+                'column' => $tag->getTag(),
+                'html' => htmlentities($tag->getTag(), ENT_QUOTES),
+                'defaultContent' => ''
+            ]
+        );
+
+        $this->getOrdersTable()->addColumn($column);
+    }
+
     /**
-     * DataTable
+     * @return DataTable
      */
     public function getOrdersTable()
     {
