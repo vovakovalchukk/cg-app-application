de51d3dc7f28902840b7f4d574c739ee14b131a9
Nick Peirson <nick.peirson@channelgrabber.com>
2014-02-17 16:50:19 +0000

    Bringing compile_di inline across repos

--- bin/compile_di.php
+++ bin/compile_di.php
@@ -1,6 +1,11 @@
 <?php
+define('GREEN', "\033[32m");
+define('WHITE', "\033[0m");
+
 require_once 'bootstrap.php';
 require_once 'config/di/components.php';
+
+echo GREEN . 'Compiling DI definitions' . WHITE . PHP_EOL;
 
 $diDataDir = 'data/di/';
 
@@ -11,20 +16,22 @@ foreach ($it as $file) {
         unlink($file->getPathname());
     }
 }
-
 $componentArray = [];
 foreach ($componentTypes as $type => $components) {
     foreach ($components as $component) {
         $diCompiler = new CG\Zend\Stdlib\Di\Definition\RuntimeCompiler;
         $dir = dirname(__DIR__) . '/' . $type . '/' . stripslashes(preg_replace('|(?<!\\\\)_|', '/', $component));
-        echo "Compiling ".$dir."\n";
+        echo $dir.PHP_EOL;
         $diCompiler->addDirectory($dir);
         $diCompiler->setAllowReflectionExceptions();
         $diCompiler->compile();
         $componentArray = array_merge($componentArray, $diCompiler->toArrayDefinition()->toArray());
     }
 }
+
 file_put_contents(
-     $diDataDir .'di-definition.php',
+    $diDataDir .'di-definition.php',
     '<?php return ' . var_export($componentArray, true) . ';'
 );
+
+echo 'DONE' . PHP_EOL;
