78f9c00698d522d96c0cf038f280ee3c134e5fde
Richard Heelin <richardh@channelgrabber.com>
2014-03-11 16:25:50 +0000

    CGIV-1769: Initial setup of invoice controller

--- module/Orders/src/Orders/Controller/InvoiceController.php
+++ module/Orders/src/Orders/Controller/InvoiceController.php
@@ -7,6 +7,12 @@ class InvoiceController extends AbstractActionController
 {
     public function generateAction()
     {
+        $response = $this->getResponse();
+        $headers = $response->getHeaders();
 
+        $headers->addHeaderLine('Content-Type', 'application/pdf');
+        $headers->addHeaderLine('Content-Disposition', 'attachment; filename="invoice.pdf"');
+
+        return $response->setContent('');
     }
 }
