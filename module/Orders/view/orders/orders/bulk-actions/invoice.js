6c490a91c225d0bd7c6298cd44b79d204c3499e8
Richard Heelin <richardh@channelgrabber.com>
2014-03-11 16:04:03 +0000

    CGIV-1769: Added javascript to handle the invoice bulk action

---
+++ module/Orders/view/orders/orders/bulk-actions/invoice.js
@@ -0,0 +1,15 @@
+require.config({
+    paths: {
+        InvoiceBulkAction: "<?= $this->baseUrl . Orders\Module::PUBLIC_FOLDER . 'js/invoice' ?>"
+    }
+});
+require(
+    ["InvoiceBulkAction"],
+    function(InvoiceBulkAction) {
+        var invoiceBulkAction = new InvoiceBulkAction(n, "<?= $this->translate('Generating Invoice') ?>");
+        $("#<?= $id ?>").bulkActions("set", "<?= $action ?>", function() {
+            invoiceBulkAction.setElement(this);
+            invoiceBulkAction.action();
+        });
+    }
+);
