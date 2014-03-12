6d93f94af5f7a1d48da915fc4749b29baf10e669
Richard Heelin <richardh@channelgrabber.com>
2014-03-12 09:52:25 +0000

    CGIV-1769: Implemented basic factory for template generation - should be replaced when we have CGIV-1769

--- module/Orders/src/Orders/Order/Invoice/Template/Factory.php
+++ module/Orders/src/Orders/Order/Invoice/Template/Factory.php
@@ -1,17 +1,39 @@
 <?php
 namespace Orders\Order\Invoice\Template;
 
+use Zend\Di\Di;
 use CG\Order\Shared\Entity as Order;
 use CG\Template\Entity as Template;
 
 class Factory
 {
+    protected $di;
+
+    public function __construct(Di $di)
+    {
+        $this->setDi($di);
+    }
+
+    public function setDi(Di $di)
+    {
+        $this->di = $di;
+        return $this;
+    }
+
+    /**
+     * @return Di
+     */
+    public function getDi()
+    {
+        return $this->di;
+    }
+
     /**
      * @param Order $order
      * @return Template
      */
     public function getTemplateForOrderEntity(Order $order)
     {
-
+        return $this->getDi()->get(Template::class);
     }
 }
