a1ff289baa45dc4865ab055fe455b3533e873b31
Adam <adamb@channelgrabber.com>
2014-03-17 15:43:12 +0000

    CGIV-1694 added initial unit test setup

---
+++ tests/unit/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+define('PROJECT_ROOT', dirname(__DIR__));
+define('TEST_ROOT', __DIR__);
+
+require_once PROJECT_ROOT.'/../vendor/autoload.php';
+require_once PROJECT_ROOT.'/../vendor/channelgrabber/stdlib/tests/external-bootstrap.php';
