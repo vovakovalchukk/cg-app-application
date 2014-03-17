6406a54c3ec6e926921410d5afe2a04a0a7866d7
Adam <adamb@channelgrabber.com>
2014-03-17 16:57:20 +0000

    CGIV-1694 initialised zf2 module unit tests

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -1,6 +1,11 @@
 <?php
-define('PROJECT_ROOT', dirname(__DIR__));
+define('PROJECT_ROOT', dirname(dirname(__DIR__)));
 define('TEST_ROOT', __DIR__);
 
-require_once PROJECT_ROOT.'/../vendor/autoload.php';
-require_once PROJECT_ROOT.'/../vendor/channelgrabber/stdlib/tests/external-bootstrap.php';
+echo PROJECT_ROOT.'/vendor/autoload.php' . "\n";
+echo PROJECT_ROOT.'/vendor/channelgrabber/stdlib/tests/external-bootstrap.php' . "\n";
+
+require_once PROJECT_ROOT.'/vendor/autoload.php';
+require_once PROJECT_ROOT.'/vendor/channelgrabber/stdlib/tests/external-bootstrap.php';
+
+require 'init_autoloader.php';
