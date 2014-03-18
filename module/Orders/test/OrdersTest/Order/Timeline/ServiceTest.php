c66774bfbbebf24a62fbaafe0d098eae6b78390d
Adam <adamb@channelgrabber.com>
2014-03-18 09:56:54 +0000

    CGIV-1694 Moved test to zf2 module

---
+++ module/Orders/test/OrdersTest/Order/Timeline/ServiceTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace OrdersTest\Orders\Order\Timeline;
+
+use PHPUnit_Framework_TestCase;
+use CG\Order\Shared\Entity as OrderEntity;
+use CG\Stdlib\Timings;
+use Orders\Order\Timeline\Service as TimelineService;
+
+class ServiceTest extends PHPUnit_Framework_TestCase
+{
+    protected $timings;
+    protected $timelineService;
+
+    public function setUp()
+    {
+        $this->timings = $this->getMockBuilder(Timings::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->timelineService = new TimelineService($this->timings);
+    }
+
+    public function testConstruction()
+    {
+        $this->assertInstanceOf(
+            TimelineService::class,
+            $this->timelineService
+        );
+    }
+}
