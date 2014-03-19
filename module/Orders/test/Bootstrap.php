eb232e2e38cf05a8f060456d7a390d034eebc5d0
Adam <adamb@channelgrabber.com>
2014-03-19 15:56:37 +0000

    CGIV-1694 enabled mustache

--- module/Orders/test/Bootstrap.php
+++ module/Orders/test/Bootstrap.php
@@ -1,14 +1,13 @@
 <?php
 
 require dirname(dirname(dirname(__DIR__))) . '/AbstractBootstrap.php';
-//chdir(__DIR__);
 
 class Bootstrap extends AbstractBootstrap
 {
     public function getModuleDependencies()
     {
         return array(
-//            'Mustache',
+            'Mustache',
             'CG_UI',
             'Orders'
         );
