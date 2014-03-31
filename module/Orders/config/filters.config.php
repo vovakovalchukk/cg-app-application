2516797343686cba29f3dc5667ac04dc5b6efd4e
Richard Heelin <richardh@channelgrabber.com>
2014-03-31 17:27:45 +0100

    CGIV-1831: Filters now load from configuration instead of been hard coded

--- module/Orders/config/filters.config.php
+++ module/Orders/config/filters.config.php
@@ -1,6 +1,8 @@
 <?php
 use Orders\Order\FilterService;
 
+$dateFormat = 'd/m/y';
+
 return [
     'di' => [
         'instance' => [
@@ -12,6 +14,133 @@ return [
         ]
     ],
     'filters' => [
-        'orders' => [],
+        'orders' => [
+            'rows' => [
+                [
+                    'type' => 'Row',
+                    'filters' => [
+                        [
+                            'type' => 'DateRange',
+                            'ranges' => [
+                                [
+                                    'title' => 'All Time',
+                                    'from' => 'All',
+                                    'to' => 'All'
+                                ],
+                                [
+                                    'title' => 'Today',
+                                    'from' => date($dateFormat),
+                                    'to' => date($dateFormat)
+                                ],
+                                [
+                                    'title' => 'Last 7 days',
+                                    'from' => date($dateFormat, strtotime("-7 days")),
+                                    'to' => date($dateFormat)
+                                ],
+                                [
+                                    'title' => 'Month to date',
+                                    'from' => date($dateFormat, strtotime('first day of ' . date('F Y'))),
+                                    'to' => date($dateFormat)
+                                ],
+                                [
+                                    'title' => 'Year to date',
+                                    'from' => date($dateFormat, strtotime('first day of January ' . date('Y'))),
+                                    'to' => date($dateFormat)
+                                ],
+                                [
+                                    'title' => 'The previous month',
+                                    'from' => date($dateFormat, strtotime('first day of last month ')),
+                                    'to' => date($dateFormat, strtotime('last day of last month ')),
+                                ]
+                            ],
+                        ],
+                        [
+                            'type' => 'CustomSelectGroup',
+                            'parameters' => [
+                                'title' => 'Status',
+                                'id' => 'filter-status',
+                            ],
+                            'options' => [
+                                [
+                                    'title' => 'New'
+                                ],
+                                [
+                                    'title' => 'Processing'
+                                ],
+                                [
+                                    'title' => 'Dispatched'
+                                ]
+                            ],
+                        ],
+                        [
+                            'type' => 'Text',
+                            'parameters' => [
+                                'placeholder' => 'Search for...',
+                                'class' => '',
+                                'value' => ''
+                            ],
+                        ],
+                        [
+                            'type' => 'Buttons',
+                            'buttons' => [
+                                [
+                                    'name' => 'apply-filters',
+                                    'value' => 'Apply Filters',
+                                    'action' => 'apply-filters',
+                                ],
+                                [
+                                    'name' => 'clear-filters',
+                                    'value' => 'Clear',
+                                    'action' => 'clear-filters',
+                                ],
+                                [
+                                    'name' => 'save-filters',
+                                    'value' => 'Save',
+                                    'action' => 'save-filters',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                [
+                    'type' => 'Row',
+                    'filters' => [
+                        [
+                            'type' => 'CustomSelectGroup',
+                            'parameters' => [
+                                'title' => 'Include Country',
+                                'isOptional' => true,
+                            ],
+                            'options' => [
+                                [
+                                    'title' => 'UK'
+                                ],
+                                [
+                                    'title' => 'Austria'
+                                ],
+                                [
+                                    'title' => 'Croatia'
+                                ],
+                                [
+                                    'title' => 'Cyprus'
+                                ],
+                                [
+                                    'title' => 'France'
+                                ],
+                                [
+                                    'title' => 'Germany'
+                                ],
+                                [
+                                    'title' => 'Italy'
+                                ],
+                                [
+                                    'title' => 'Spain'
+                                ]
+                            ]
+                        ],
+                    ]
+                ],
+            ],
+        ],
     ],
 ];
--- module/Orders/src/Orders/Controller/OrdersController.php
+++ module/Orders/src/Orders/Controller/OrdersController.php
@@ -218,64 +218,11 @@ class OrdersController extends AbstractActionController
 
     protected function getFilterBar()
     {
-        $filterObject = $this->getFilterService()->getPersistentFilter();
         $viewRender = $this->getServiceLocator()->get('Mustache\View\Renderer');
+        $filterValues = $this->getFilterService()->getPersistentFilter();
+        $filters = $this->getOrderService()->getFilterService()->getOrderFilters();
 
-        $filterRows = [];
-        $filterRow = [];
-
-        $dateRangeFilter = $this->getViewModelFactory()->newInstance();
-        $dateRangeFilter->setTemplate('elements/date-range');
-        $dateRangeFilter->setVariable(
-            'options',
-            // Example Data - Should be loaded via Service/Di
-            include dirname(dirname(dirname(__DIR__))) . '/test/data/filterBar/daterange-options.php'
-        );
-        $filterRow[] = $viewRender->render($dateRangeFilter);
-
-        $filterButtons = $this->getViewModelFactory()->newInstance();
-        $filterButtons->setTemplate('elements/custom-select-group');
-        $filterButtons->setVariable(
-            'options',
-            // Example Data - Should be loaded via Service/Di
-            include dirname(dirname(dirname(__DIR__))) . '/test/data/filterBar/status-options.php'
-        );
-        $filterRow[] = $viewRender->render($filterButtons);
-
-        $statusFilter = $this->getViewModelFactory()->newInstance();
-        $statusFilter->setTemplate('elements/text');
-        $statusFilter->setVariable(
-            'options',
-            // Example Data - Should be loaded via Service/Di
-            include dirname(dirname(dirname(__DIR__))) . '/test/data/filterBar/search-options.php'
-        );
-        $filterRow[] = $viewRender->render($statusFilter);
-
-        $filterButtons = $this->getViewModelFactory()->newInstance();
-        $filterButtons->setTemplate('elements/buttons');
-        $filterButtons->setVariable(
-            'options',
-            // Example Data - Should be loaded via Service/Di
-            include dirname(dirname(dirname(__DIR__))) . '/test/data/filterBar/buttons.php'
-        );
-        $filterRow[] = $viewRender->render($filterButtons);
-        $filterRows[] = $filterRow;
-
-        $filterRow = [];
-        $filterButtons = $this->getViewModelFactory()->newInstance();
-        $filterButtons->setTemplate('elements/custom-select-group');
-        $filterButtons->setVariable(
-            'options',
-            // Example Data - Should be loaded via Service/Di
-            include dirname(dirname(dirname(__DIR__))) . '/test/data/filterBar/country-options.php'
-        );
-        $filterRow[] = $viewRender->render($filterButtons);
-        $filterRows[] = $filterRow;
-
-        $filterBar = $this->getViewModelFactory()->newInstance();
-        $filterBar->setTemplate('layout/filters');
-        $filterBar->setVariable('filterRows', $filterRows);
-        return $filterBar;
+        return $filters->prepare($viewRender);
     }
 
     protected function getDetailsSidebar(array $children)
