1a2e8b81b933186b315714d9ee9536ee2672d173
Richard Heelin <richardh@channelgrabber.com>
2014-04-03 12:30:06 +0100

    CGIV-1912: Setup the initial Controllers and basic actions

---
+++ module/Settings/src/Settings/Controller/ChannelController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Settings\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class ChannelController extends AbstractActionController
+{
+    public function listAction()
+    {
+
+    }
+}
