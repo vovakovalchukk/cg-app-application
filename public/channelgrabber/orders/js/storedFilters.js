34b57c75e28bfc37222b3718ba844b5b559d691d
Richard Heelin <richardh@channelgrabber.com>
2014-04-09 17:08:03 +0100

    CGIV-1836: Added and styled the save popup

--- public/channelgrabber/orders/js/storedFilters.js
+++ public/channelgrabber/orders/js/storedFilters.js
@@ -1,44 +1,73 @@
-define(function() {
-    var StoredFilters = function(filters, savedFilterList) {
-        this.getFilters = function() {
-            return $(filters);
+define(
+    ['popup/mustache'],
+    function(Popup) {
+        var StoredFilters = function(filters, savedFilterList) {
+            this.getFilters = function() {
+                return $(filters);
+            };
+
+            this.getSavedFilterList = function() {
+                return $(savedFilterList);
+            };
+
+            var popup;
+            var init = function() {
+                var self = this;
+                var savedFilterList = self.getSavedFilterList();
+
+                savedFilterList.on("click.storedFilters", "li a", function() {
+                    self.activateFilter.call($(this).closest("li"));
+                });
+                savedFilterList.on("click.storedFilters", "li .close", function() {
+                    self.deleteFilter.call($(this).closest("li"));
+                });
+
+                popup = new Popup(
+                    savedFilterList.data("template")
+                );
+
+                popup.getElement().on("click.storedFilters", ".save", function() {
+                    var name = $.trim(popup.getElement().find("input.name").val());
+                    if (!name.length) {
+                        return;
+                    }
+                    self.saveFilter.call(self, name, popup.getElement().data("filter"));
+                });
+                popup.getElement().on("click.storedFilters", ".cancel", function() {
+                    popup.hide();
+                });
+            };
+
+            this.getPopup = function() {
+                return popup;
+            };
+
+            init.call(this);
         };
 
-        this.getSavedFilterList = function() {
-            return $(savedFilterList);
-        };
-
-        var init = function() {
+        StoredFilters.prototype.bindSaveTo = function(element) {
             var self = this;
-            self.getSavedFilterList().on("click.storedFilters", "li a", function() {
-                self.activateFilter.call($(this).closest("li"));
-            });
-            self.getSavedFilterList().on("click.storedFilters", "li .close", function() {
-                self.deleteFilter.call($(this).closest("li"));
+            $(element).bind("click.storedFilters", function() {
+                self.saveCurrentFilter.call(self);
             });
         };
 
-        init.call(this);
-    };
+        StoredFilters.prototype.saveCurrentFilter = function() {
+            this.getPopup().show();
+        };
 
-    StoredFilters.prototype.bindSaveTo = function(element) {
-        var self = this;
-        $(element).bind("click.storedFilters", function() {
-            self.saveCurrentFilter.call(self);
-        });
-    };
+        StoredFilters.prototype.saveFilter = function(name, filter) {
 
-    StoredFilters.prototype.saveCurrentFilter = function() {
+        };
 
-    };
+        StoredFilters.prototype.activateFilter = function(listElement) {
 
-    StoredFilters.prototype.activateFilter = function(listElement) {
+        };
 
-    };
+        StoredFilters.prototype.deleteFilter = function(listElement) {
 
-    StoredFilters.prototype.deleteFilter = function(listElement) {
+        };
 
-    };
-
-    return StoredFilters;
-});
+        return StoredFilters;
+    }
+);
