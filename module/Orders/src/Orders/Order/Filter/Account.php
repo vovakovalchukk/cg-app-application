f95b2e581201e879aa0ce9c0ca90a40635406c00
Richard Heelin <richardh@channelgrabber.com>
2014-04-14 09:59:03 +0100

    CGIV-1948: Switched to new filter options provider interface

--- module/Orders/src/Orders/Order/Filter/Account.php
+++ module/Orders/src/Orders/Order/Filter/Account.php
@@ -1,26 +1,20 @@
 <?php
 namespace Orders\Order\Filter;
 
-use CG_UI\View\Filters\Options\Select;
 use CG\Stdlib\Exception\Runtime\NotFound;
 
 class Account extends Channel
 {
     /**
-     * @return Select[] array of options to be added to filter
+     * {@inherit}
      */
-    public function getOptions()
+    public function getSelectOptions()
     {
         $options = [];
         try {
             $accounts = $this->getAccounts($this->getActiveUser());
             foreach ($accounts as $account) {
-                $options[] = $this->getDi()->newInstance(
-                    Select::class,
-                    [
-                        'title' => htmlentities($account->getDisplayName(), ENT_QUOTES),
-                    ]
-                );
+                $options[$account->getDisplayName()] = $account->getDisplayName();
             }
         } catch (NotFound $exception) {
             // No accounts means no channels so ignore
