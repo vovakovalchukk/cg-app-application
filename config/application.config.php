20de8ea34d5468e66caf3f7a935b169864c6b05a
M King <matthew.king@channelgrabber.com>
2014-04-16 10:13:38 +0100

    CGIV-1925 Sso module

--- config/application.config.php
+++ config/application.config.php
@@ -10,9 +10,10 @@ return array (
     5 => 'CG_Permission',
     6 => 'CG_Login',
     7 => 'CG_Register',
-    8 => 'Orders',
-    9 => 'Settings',
-    10 => 'CG_Email_Template',
+    8 => 'CG_SSO',
+    9 => 'Orders',
+    10 => 'Settings',
+    11 => 'CG_Email_Template',
   ),
   'module_listener_options' => 
   array (
