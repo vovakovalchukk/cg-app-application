2584c1b1ee6c9677d4a97819c724215f504b0f82
Adam <adamb@channelgrabber.com>
2014-05-12 16:51:34 +0100

    CGIV-2006 Added paper type dom listener

---
+++ public/channelgrabber/settings/js/InvoiceDesigner/Template/Module/DomListener/PaperType.js
@@ -0,0 +1,35 @@
+define([
+    'module',
+    'InvoiceDesigner/Module/DomListenerAbstract',
+    'element/customSelect'
+], function(
+    requireModule,
+    DomListenerAbstract,
+    CustomSelect
+    ) {
+
+    var PaperType = function()
+    {
+        DomListenerAbstract.call(this);
+
+        var events = requireModule.config().events;
+
+        this.getEvents = function()
+        {
+            return events;
+        };
+    };
+
+    PaperType.prototype = Object.create(DomListenerAbstract.prototype);
+
+    PaperType.prototype.init = function(module)
+    {
+        var self = this;
+        DomListenerAbstract.prototype.init.call(this, module);
+        $(document).on(CustomSelect.EVENT_SELECT_CHANGED, function (event, selectBox, id) {
+            self.getModule().selectionMade(id);
+        });
+    };
+
+    return new PaperType();
+});
