4311380a2e39aa9acf68781aabe58e1e0e614e10
Adam <adamb@channelgrabber.com>
2014-05-13 09:31:01 +0100

    CGIV-2006 implemented fetchAll for papertype storage

--- public/channelgrabber/settings/js/InvoiceDesigner/Template/PaperType/Storage/Ajax.js
+++ public/channelgrabber/settings/js/InvoiceDesigner/Template/PaperType/Storage/Ajax.js
@@ -9,7 +9,35 @@ define(['InvoiceDesigner/Template/PaperType/StorageAbstract', 'jQuery'], functio
 
     Ajax.prototype.fetchAll = function()
     {
-        return [];
+        var data = [
+            {
+                id: 1,
+                name: "No Label (Blank)",
+                backgroundImage: "/path/to/no/label"
+            },
+            {
+                id: 2,
+                name: "Single Label Top",
+                backgroundImage: "/path/to/single/label/top"
+            },
+            {
+                id: 3,
+                name: "Single Label Bottom",
+                backgroundImage: "/path/to/single/label/bottom"
+            },
+            {
+                id: 4,
+                name: "Double Label Top",
+                backgroundImage: "/path/to/double/label/top"
+            },
+            {
+                id: 5,
+                name: "Double Label Bottom",
+                backgroundImage: "/path/to/double/label/bottom"
+            }
+        ];
+
+        return this.getMapper().fromArray(data);
     };
 
     return new Ajax();
