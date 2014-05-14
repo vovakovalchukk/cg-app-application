a93c9f514247d255d049d24ee221ae3e1c75bbfd
Aaron Baker <aaronb@channelgrabber.com>
2014-05-14 17:06:24 +0100

    CGIV-2023: invoice designer - more work on JS Template Image element rendering - moved html to a Mustache template. W.I.P.

--- public/channelgrabber/settings/js/InvoiceDesigner/Template/Element/Mapper/Image.js
+++ public/channelgrabber/settings/js/InvoiceDesigner/Template/Element/Mapper/Image.js
@@ -12,19 +12,12 @@ define([
 
     Image.prototype.getHtmlContents = function(element)
     {
-        var html = '<div class="placeholder">Image\
-        <input type="file" />\n\
-        <input type="button" class="button" value="Upload" /></div>\n\
-        <script type="text/javascript">\n\
-            require([\'jquery\'], function($) {\n\
-                $(document).ready(function() {\n\
-                    $(\'#'+MapperAbstract.getDomId(element)+' input[type=button]\').click(function() {\n\
-                        $(this).parent().find(\'input[type=file]\').click();\n\
-                    });\n\
-                });\n\
-            });\n\
-        </script>\n';
+        var templateUrl = MapperAbstract.ELEMENT_TEMPLATE_PATH+'image.mustache';
+        var data = {
+            id: MapperAbstract.getDomId(element)
+        };
 
+        var html = this.renderMustacheTemplate(templateUrl, data);
         return html;
     };
 
