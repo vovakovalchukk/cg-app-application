1fa1f281431374b77d75cdc4c4e90aa6e2ed6bf2
Adam <adamb@channelgrabber.com>
2014-05-14 08:54:04 +0100

    CGIV-2006 added inverse background image to storage

--- public/channelgrabber/settings/js/InvoiceDesigner/Template/PaperType/Storage/Ajax.js
+++ public/channelgrabber/settings/js/InvoiceDesigner/Template/PaperType/Storage/Ajax.js
@@ -13,27 +13,32 @@ define(['InvoiceDesigner/Template/PaperType/StorageAbstract', 'jquery'], functio
             {
                 id: 1,
                 name: "No Label (Blank)",
-                backgroundImage: "/path/to/no/label"
+                backgroundImage: "/path/to/no/label",
+                InverseBackgroundImage: "/path/to/no/label/inverse"
             },
             {
                 id: 2,
                 name: "Single Label Top",
-                backgroundImage: "/path/to/single/label/top"
+                backgroundImage: "/path/to/single/label/top",
+                InverseBackgroundImage: "/path/to/single/label/top/inverse"
             },
             {
                 id: 3,
                 name: "Single Label Bottom",
-                backgroundImage: "/path/to/single/label/bottom"
+                backgroundImage: "/path/to/single/label/bottom",
+                InverseBackgroundImage: "/path/to/single/label/bottom/inverse"
             },
             {
                 id: 4,
                 name: "Double Label Top",
-                backgroundImage: "/path/to/double/label/top"
+                backgroundImage: "/path/to/double/label/top",
+                InverseBackgroundImage: "/path/to/double/label/top/inverse"
             },
             {
                 id: 5,
                 name: "Double Label Bottom",
-                backgroundImage: "/path/to/double/label/bottom"
+                backgroundImage: "/path/to/double/label/bottom",
+                InverseBackgroundImage: "/path/to/double/label/bottom/inverse"
             }
         ];
 
