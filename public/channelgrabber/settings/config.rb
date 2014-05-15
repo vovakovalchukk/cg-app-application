459640a950669c497d35b6aaafbe0dc2d29799ab
Adam <adamb@channelgrabber.com>
2014-05-15 15:07:49 +0100

    CGIV-2006 Added css to at least make paper type module functional and unhide the checkbox

---
+++ public/channelgrabber/settings/config.rb
@@ -0,0 +1,10 @@
+# Set paths for compass watch directories
+http_path = "public/"
+css_dir = "public/css"
+sass_dir = "public/scss"
+images_dir = "public/img"
+javascripts_dir = "public/js"
+
+output_style = :expanded
+line_comments = false
+relative_assets = true
