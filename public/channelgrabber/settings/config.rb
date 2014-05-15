ab6d7e8f93c4ea0bd5f7f47679aa8bc63ee4983b
Adam <adamb@channelgrabber.com>
2014-05-15 15:09:14 +0100

    CGIV-2006 Added settings compass config

--- public/channelgrabber/settings/config.rb
+++ public/channelgrabber/settings/config.rb
@@ -1,9 +1,9 @@
 # Set paths for compass watch directories
-http_path = "public/"
-css_dir = "public/css"
-sass_dir = "public/scss"
-images_dir = "public/img"
-javascripts_dir = "public/js"
+http_path = "./"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
 
 output_style = :expanded
 line_comments = false
