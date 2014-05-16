6679c1859e0dd8a9d86b2f21ad30d8adfe3a168c
Adam <adamb@channelgrabber.com>
2014-05-16 15:13:14 +0100

    CGIV-2006 add new instance of page to the template so changes will be notified and rendered

--- public/channelgrabber/settings/js/InvoiceDesigner/Template/Entity.js
+++ public/channelgrabber/settings/js/InvoiceDesigner/Template/Entity.js
@@ -45,6 +45,7 @@ define([
             if (!page) {
                 var pageClass = require(Entity.PATH_TO_PAGE_ENTITY);
                 page = new pageClass();
+                this.addElement(page, false);
             }
             return page;
         };
