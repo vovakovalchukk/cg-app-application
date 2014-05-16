d7eb5de9d7ee0f185d75e5a77fc1236baeee4883
Adam <adamb@channelgrabber.com>
2014-05-16 17:26:22 +0100

    CGIV-2006 added todo to remember what to change once papertype storage data has been moved to php

--- public/channelgrabber/settings/js/InvoiceDesigner/Template/Module/DomListener/PaperType.js
+++ public/channelgrabber/settings/js/InvoiceDesigner/Template/Module/DomListener/PaperType.js
@@ -30,7 +30,7 @@ define([
         var self = this;
         DomListenerAbstract.prototype.init.call(this, module);
         console.log("listening: " + paperTypeDropdownId + " ("+$(paperTypeDropdownId).length+")");
-        $(document).on('change', paperTypeDropdownId, function (event, selectBox, id) {
+        $(document).on('change', paperTypeDropdownId, function (event, selectBox, id) { // TODO change back to non delegated once papertype storage is in php side
             console.log("Dropdown changed");
             var isInverse = $("#" + PaperType.CHECKBOX_ID).is(":checked");
             self.getModule().selectionMade(id, isInverse);
