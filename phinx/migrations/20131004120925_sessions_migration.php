7f3dcf542e8018200b1774e0731cf471b0dacc39
James Moorhouse <jamesm@channelgrabber.com>
2014-06-24 08:47:18 +0100

    CGIV-2481 saving

--- phinx/migrations/20131004120925_sessions_migration.php
+++ phinx/migrations/20131004120925_sessions_migration.php
@@ -2,38 +2,20 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class SessionsMigration extends AbstractMigration
+class RemoveSessions extends AbstractMigration
 {
-    /**
-     * Change.
-     */
-    public function change()
-    {
-        $this->createSessionsTable();
-    }
-    
-    /**
-     * Migrate Up.
-     */
     public function up()
-    {
-
-    }
-
-    /**
-     * Migrate Down.
-     */
-    public function down()
-    {
-
-    }
-
-    private function createSessionsTable()
     {
         $table = $this->table('sessions', array('id' => false, 'primary_key' => 'session_id'));
         $table->addColumn('session_id', 'string', array('limit' => 32))
-              ->addColumn('session_data', 'text')
-              ->addColumn('session_expiration', 'integer', array('limit' => 11))
-              ->create();        
+            ->addColumn('session_data', 'text')
+            ->addColumn('session_expiration', 'integer', array('limit' => 11))
+            ->create();
     }
+
+    public function down()
+    {
+        $this->dropTable('sessions');
+    }
+
 }
