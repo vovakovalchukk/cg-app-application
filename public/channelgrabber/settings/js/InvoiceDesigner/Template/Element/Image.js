4513ef2d1a5d7a4922f857ef47a7a49629ca60a5
M King <matthew.king@channelgrabber.com>
2014-06-29 23:05:20 +0100

    CGIV-2287 Untested image resizing

--- public/channelgrabber/settings/js/InvoiceDesigner/Template/Element/Image.js
+++ public/channelgrabber/settings/js/InvoiceDesigner/Template/Element/Image.js
@@ -1,3 +1,4 @@
+var jsImage = new Image();
 define(['InvoiceDesigner/Template/ElementAbstract'], function(ElementAbstract)
 {
     var Image = function()
@@ -34,6 +35,23 @@ define(['InvoiceDesigner/Template/ElementAbstract'], function(ElementAbstract)
     };
 
     Image.prototype = Object.create(ElementAbstract.prototype);
+
+    Image.prototype.resizeImageData = function()
+    {
+        jsImage.src = 'data:image/' + this.getFormat().toLowerCase() + ';base64,' + this.getSource();
+        var canvas = document.createElement('canvas'), ctx = canvas.getContext('2d');
+        canvas.width = Number(this.getWidth()).mmToPx();
+        canvas.height = Number(this.getHeight()).mmToPx();
+        ctx.drawImage(jsImage, 0, 0, Number(this.getWidth()).mmToPx(), Number(this.getHeight()).mmToPx());
+        return canvas.toDataURL().split(',')[1];
+    };
+
+    Image.prototype.toJson = function()
+    {
+        var json = ElementAbstract.prototype.toJson.call(this);
+        json.source = this.resizeImageData();
+        return json;
+    };
 
     return Image;
 });
