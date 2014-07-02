10818e73a050e598e9bd470a6fc9e7fca27482f4
M King <matthew.king@channelgrabber.com>
2014-07-02 16:39:31 +0100

    CGIV-2390 Add usage to application config

--- config/application.config.php
+++ config/application.config.php
@@ -18,7 +18,7 @@ return array (
     13 => 'CG_Ebay',
     14 => 'CG_Channel',
     15 => 'CG_Sessions',
-    16 => 'CG_Name',
+    16 => 'CG_Usage',
   ),
   'module_listener_options' => 
   array (
