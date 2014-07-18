62c042326a693826e2c254e3f80a9bbbd2e050db
Paul Keelan <paulk@channelgrabber.com>
2014-07-18 13:50:37 +0100

    CGIV-2685 - Added addiitonal error protection to the invoice designer for a NotModified exception so that if an item is attempted to be saved when no modifications exist it outputs an appropriate message

--- module/Settings/src/Settings/Controller/InvoiceController.php
+++ module/Settings/src/Settings/Controller/InvoiceController.php
@@ -7,9 +7,12 @@ use Zend\Mvc\Controller\AbstractActionController;
 use Settings\Module;
 use Settings\Invoice\Service as InvoiceService;
 use Settings\Invoice\Mapper as InvoiceMapper;
+use CG\Http\Exception\Exception3xx\NotModified;
 use CG\Template\ReplaceManager\OrderContent as OrderTagManager;
 use CG\Template\Service as TemplateService;
 use CG\User\OrganisationUnit\Service as UserOrganisationUnitService;
+use CG\Zend\Stdlib\View\Model\Exception as ViewModelException;
+use Zend\I18n\Translator\Translator;
 
 class InvoiceController extends AbstractActionController
 {
@@ -29,6 +32,7 @@ class InvoiceController extends AbstractActionController
     protected $orderTagManager;
     protected $invoiceService;
     protected $invoiceMapper;
+    protected $translator;
 
     public function __construct(
         ViewModelFactory $viewModelFactory,
@@ -37,7 +41,8 @@ class InvoiceController extends AbstractActionController
         UserOrganisationUnitService $userOrganisationUnitService,
         OrderTagManager $orderTagManager,
         InvoiceService $invoiceService,
-        InvoiceMapper $invoiceMapper
+        InvoiceMapper $invoiceMapper,
+        Translator $translator
     ) {
         $this->setViewModelFactory($viewModelFactory)
             ->setJsonModelFactory($jsonModelFactory)
@@ -45,7 +50,8 @@ class InvoiceController extends AbstractActionController
             ->setUserOrganisationUnitService($userOrganisationUnitService)
             ->setOrderTagManager($orderTagManager)
             ->setInvoiceService($invoiceService)
-            ->setInvoiceMapper($invoiceMapper);
+            ->setInvoiceMapper($invoiceMapper)
+            ->setTranslator($translator);
     }
 
     public function indexAction()
@@ -228,9 +234,28 @@ class InvoiceController extends AbstractActionController
 
     public function saveAction()
     {
-        $template = $this->getTemplateService()->saveFromJson($this->params()->fromPost('template'));
-        $view = $this->getJsonModelFactory()->newInstance(["template" => json_encode($template)]);
-        return $view;
+        try{
+            $template = $this->getTemplateService()->saveFromJson($this->params()->fromPost('template'));
+            $view = $this->getJsonModelFactory()->newInstance(["template" => json_encode($template)]);
+            return $view;
+        } catch (NotModified $e) {
+            $this->handleAccountUpdateException($e, 'There were no changes to be saved');
+        } catch (ViewModelException $e) {
+            $this->handleAccountUpdateException($e, 'Template could not be saved.');
+        }
+        return false;
+    }
+
+    protected function handleAccountUpdateException(\Exception $e, $message)
+    {
+        $status = $this->getJsonModelFactory()->newInstance();
+        $status->setVariable('valid', false);
+        throw new ViewModelException(
+            $status,
+            $this->getTranslator()->translate($message),
+            $e->getCode(),
+            $e
+        );
     }
 
     public function getViewModelFactory()
@@ -333,5 +358,15 @@ class InvoiceController extends AbstractActionController
     {
         $this->invoiceMapper = $invoiceMapper;
         return $this;
+    }
+
+    protected function getTranslator()
+    {
+        return $this->translator;
+    }
+
+    protected function setTranslator(Translator $translator)
+    {
+        $this->translator = $translator;
     }
 }
