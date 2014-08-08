e8ebcb3f25c33d7752da81149b898b2d4ec2ad74
Jamie Holland <jamie.holland@channelgrabber.com>
2014-08-08 12:11:27 +0100

    Revert "Merge branch 'CGIV-2139-stock-fields-product-list' into develop"
    This reverts commit 92dc4de07417d11a54be5428d44f3d1ecb6e7627, reversing
    changes made to f32551b239b4689efaf97aa1aa8bd0eea67bada6.
    
    Conflicts:
    	composer.lock

--- module/Orders/src/Orders/Order/Service.php
+++ module/Orders/src/Orders/Order/Service.php
@@ -34,7 +34,6 @@ use Exception;
 use CG\Stdlib\Log\LoggerAwareInterface;
 use CG\Stdlib\Log\LogTrait;
 use CG\Order\Shared\Status as OrderStatus;
-use CG\Channel\Carrier;
 use CG\OrganisationUnit\Service as OrganisationUnitService;
 
 class Service implements LoggerAwareInterface
@@ -60,7 +59,6 @@ class Service implements LoggerAwareInterface
     protected $orderDispatcher;
     protected $orderCanceller;
     protected $shippingConversionService;
-    protected $carriers;
     protected $organisationUnitService;
 
     public function __construct(
@@ -75,7 +73,6 @@ class Service implements LoggerAwareInterface
         OrderDispatcher $orderDispatcher,
         OrderCanceller $orderCanceller,
         ShippingConversionService $shippingConversionService,
-        Carrier $carriers,
         OrganisationUnitService $organisationUnitService
     )
     {
@@ -92,7 +89,6 @@ class Service implements LoggerAwareInterface
             ->setOrderDispatcher($orderDispatcher)
             ->setOrderCanceller($orderCanceller)
             ->setShippingConversionService($shippingConversionService)
-            ->setCarriers($carriers)
             ->setOrganisationUnitService($organisationUnitService);
     }
 
@@ -696,11 +692,6 @@ class Service implements LoggerAwareInterface
         );
     }
 
-    public function getCarriersData()
-    {
-        return $this->getCarriers()->getAllCarriers();
-    }
-
     public function setAccountService(AccountService $accountService)
     {
         $this->accountService = $accountService;
@@ -752,15 +743,6 @@ class Service implements LoggerAwareInterface
     protected function getShippingConversionService()
     {
         return $this->shippingConversionService;
-    }
-
-    protected function getCarriers() {
-        return $this->carriers;
-    }
-
-    protected function setCarriers(Carrier $carriers) {
-        $this->carriers = $carriers;
-        return $this;
     }
 
     protected function getOrganisationUnitService()
