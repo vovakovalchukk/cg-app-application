c849c17d052ad6891d495db35990f00f875ca34c
Richard Heelin <richardh@channelgrabber.com>
2014-08-20 11:25:46 +0100

    CGIV-2955: Disable the border and background inspectors as not used for PPI

--- public/channelgrabber/settings/js/InvoiceDesigner/Template/Element/PPI.js
+++ public/channelgrabber/settings/js/InvoiceDesigner/Template/Element/PPI.js
@@ -6,6 +6,16 @@ define(['InvoiceDesigner/Template/ElementAbstract'], function(ElementAbstract) {
 
         ElementAbstract.call(this, additionalData);
         this.set('type', 'PPI', true);
+        this.set('borderWidth', undefined, true);
+
+        var ignoredAttributes = ['backgroundColour', 'borderWidth', 'borderColour'];
+        baseInspectableAttributes = this.getBaseInspectableAttributes();
+        for (var key in ignoredAttributes) {
+            var index = baseInspectableAttributes.indexOf(ignoredAttributes[key]);
+            if (index >= 0) {
+                baseInspectableAttributes.splice(index, 1);
+            }
+        }
 
         this.getOption = function() {
             return this.get('option');
