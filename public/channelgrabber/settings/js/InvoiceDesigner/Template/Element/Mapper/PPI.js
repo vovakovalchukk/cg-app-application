15c9bddc650fcc0fd8c2072ad17b6e6c73906b96
Richard Heelin <richardh@channelgrabber.com>
2014-08-20 10:58:41 +0100

    CGIV-2955: Added images to display in invoice designer based on the users selection

--- public/channelgrabber/settings/js/InvoiceDesigner/Template/Element/Mapper/PPI.js
+++ public/channelgrabber/settings/js/InvoiceDesigner/Template/Element/Mapper/PPI.js
@@ -3,10 +3,15 @@ define(['InvoiceDesigner/Template/Element/MapperAbstract'], function(MapperAbstr
         MapperAbstract.call(this);
     };
 
+    PPI.IMAGE_PATH = '/channelgrabber/settings/img/InvoiceDesigner/Template/PPI/';
+
     PPI.prototype = Object.create(MapperAbstract.prototype);
 
     var images = {
-
+        1: 'option-1.jpg',
+        2: 'option-2.jpg',
+        3: 'option-3.jpg',
+        4: 'option-4.jpg'
     };
 
     PPI.prototype.getHtmlContents = function(element) {
@@ -18,7 +23,7 @@ define(['InvoiceDesigner/Template/Element/MapperAbstract'], function(MapperAbstr
 
         var templateUrl = MapperAbstract.ELEMENT_TEMPLATE_PATH + 'ppi.mustache';
         var data = {
-            'imgSrc': image
+            'imgSrc': PPI.IMAGE_PATH + image
         };
 
         return this.renderMustacheTemplate(templateUrl, data);
