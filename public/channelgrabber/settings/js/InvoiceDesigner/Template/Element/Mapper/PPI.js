7d1d31231266178ec0e044163bfff2222fa3545b
Richard Heelin <richardh@channelgrabber.com>
2014-08-20 11:58:22 +0100

    CGIV-2955: Removed global variable in favor of helper method

--- public/channelgrabber/settings/js/InvoiceDesigner/Template/Element/Mapper/PPI.js
+++ public/channelgrabber/settings/js/InvoiceDesigner/Template/Element/Mapper/PPI.js
@@ -1,21 +1,25 @@
 define(['InvoiceDesigner/Template/Element/MapperAbstract'], function(MapperAbstract) {
     var PPI = function() {
         MapperAbstract.call(this);
+
+        var images = {
+            1: 'option-1.jpg',
+            2: 'option-2.jpg',
+            3: 'option-3.jpg',
+            4: 'option-4.jpg'
+        };
+
+        this.getImage = function(option) {
+            return images[option];
+        };
     };
 
     PPI.IMAGE_PATH = '/channelgrabber/settings/img/InvoiceDesigner/Template/PPI/';
 
     PPI.prototype = Object.create(MapperAbstract.prototype);
 
-    var images = {
-        1: 'option-1.jpg',
-        2: 'option-2.jpg',
-        3: 'option-3.jpg',
-        4: 'option-4.jpg'
-    };
-
     PPI.prototype.getHtmlContents = function(element) {
-        var image = images[element.getOption()];
+        var image = this.getImage(element.getOption());
 
         if (typeof(image) === 'undefined') {
             return '';
