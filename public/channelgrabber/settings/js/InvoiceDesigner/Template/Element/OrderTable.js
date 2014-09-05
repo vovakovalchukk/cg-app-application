2cc0db72b333a457bf08eea59373a3b1d9f97283
dangerous <dangerousdan@channelgrabber.com>
2014-09-05 15:33:26 +0100

    CGIV-2725 order table resizing

--- public/channelgrabber/settings/js/InvoiceDesigner/Template/Element/OrderTable.js
+++ public/channelgrabber/settings/js/InvoiceDesigner/Template/Element/OrderTable.js
@@ -2,11 +2,14 @@ define(['InvoiceDesigner/Template/ElementAbstract'], function(ElementAbstract)
 {
     var OrderTable = function()
     {
-        var elementWidth = 700;
+        var elementWidth = 700; // px
+        var minHeight = 200; // px
+
         ElementAbstract.call(this);
         this.set('type', 'OrderTable', true);
         this.setMinWidth(elementWidth)
-            .setMaxWidth(elementWidth);
+            .setMaxWidth(elementWidth)
+            .setMinHeight(minHeight);
     };
 
     OrderTable.prototype = Object.create(ElementAbstract.prototype);
