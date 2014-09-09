f181731723104b21fe001cd8e39795ee5363a149
Richard Heelin <richardh@channelgrabber.com>
2014-09-09 10:33:49 +0100

    CGIV-3167: Resize element to match uploaded image

--- public/channelgrabber/settings/js/InvoiceDesigner/Template/Module/ImageUpload.js
+++ public/channelgrabber/settings/js/InvoiceDesigner/Template/Module/ImageUpload.js
@@ -2,12 +2,14 @@ define([
     'InvoiceDesigner/Template/ModuleAbstract',
     'InvoiceDesigner/Template/Module/DomListener/ImageUpload',
     'InvoiceDesigner/Template/Element/MapperAbstract',
-    'element/FileUpload'
+    'element/FileUpload',
+    'InvoiceDesigner/Template/DomManipulator'
 ], function(
     ModuleAbstract,
     ImageUploadListener,
     ElementMapperAbstract,
-    FileUpload
+    FileUpload,
+    domManipulator
 ) {
     var ImageUpload = function()
     {
@@ -19,19 +21,41 @@ define([
         {
             return fileUpload;
         };
+
+        this.getDomManipulator = function()
+        {
+            return domManipulator;
+        };
     };
 
     ImageUpload.prototype = Object.create(ModuleAbstract.prototype);
 
     ImageUpload.prototype.imageElementFileSelected = function(elementDomId, file)
     {
+        var self = this;
         var elementId = ElementMapperAbstract.getElementIdFromDomId(elementDomId);
         var element = this.getTemplate().getElements().getById(elementId);
         var format = file.type.replace('image/', '');
+
         this.getFileUpload().loadBinaryDataFromFile(file, function(data)
         {
             element.setFormat(format);
-            element.setSource(btoa(data)); 
+            element.setSource(btoa(data));
+
+            var image = new Image();
+            image.src = 'data:image/' + element.getFormat().toLowerCase() + ';base64,' + element.getSource();
+
+            self.getDomManipulator().triggerElementResizedEvent(
+                element.getId(),
+                {
+                    top: Number(element.getX()).mmToPx,
+                    left: Number(element.getY()).mmToPx
+                },
+                {
+                    width: image.naturalWidth,
+                    height: image.naturalHeight
+                }
+            );
         });
     };
 
