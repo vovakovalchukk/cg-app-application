2839dfe6200ab0fec1518a641110ee9831480d64
dangerous <dangerousdan@channelgrabber.com>
2014-09-09 13:43:59 +0100

    CGIV-3024 Fix for Zf2-directory scanner.

--- config/di/framework_components.php
+++ config/di/framework_components.php
@@ -3,5 +3,4 @@ return array(
     'zendframework_zendframework_library_Zend_ServiceManager',
     'zendframework_zendframework_library_Zend_Di',
     'zendframework_zendframework_library_Zend_Db',
-    'channelgrabber_zf2-hal_CG_Zend_Hal',
 );
