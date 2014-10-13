2d2b76554902e43a0e828b29d83f7f5d6dca83f2
M King <matthew.king@channelgrabber.com>
2014-10-13 14:45:24 +0100

    CGIV-3756 only load the products once the seachbox is ready

--- public/channelgrabber/products/js/Product/DomListener/Search.js
+++ public/channelgrabber/products/js/Product/DomListener/Search.js
@@ -1,9 +1,11 @@
 define([
     'Product/Service',
-    'KeyPress'
+    'KeyPress',
+    'ElementWatcher'
 ], function (
     service,
-    KeyPress
+    KeyPress,
+    elementWatcher
 ) {
     var Search = function()
     {
@@ -12,18 +14,31 @@ define([
     Search.prototype.init = function(inputSelector, buttonSelector, baseUrl)
     {
         var self = this;
+        if (elementWatcher.isInitialised()) {
+            this.listen(inputSelector, buttonSelector, baseUrl);
+            return;
+        }
+
+        $(document).on(elementWatcher.getEventInitialised(), function() {
+            self.listen(inputSelector, buttonSelector, baseUrl);
+        });
+    };
+
+    Search.prototype.listen = function(inputSelector, buttonSelector, baseUrl)
+    {
+        var self = this;
         $(inputSelector).off('keypress').on('keypress', function(event){
             if (event.which == KeyPress.ENTER) {
-                self.search(inputSelector);
+                self.search();
             }
         });
         $(buttonSelector).off('click').on('click', function(event){
-            self.search(inputSelector);
+            self.search();
         });
         service.init(baseUrl);
     };
 
-    Search.prototype.search = function(inputSelector)
+    Search.prototype.search = function()
     {
         service.refresh();
     };
