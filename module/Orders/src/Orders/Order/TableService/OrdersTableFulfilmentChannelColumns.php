b7016346591ca27a5af757b5cdb7a6f2a5956409
dangerous <dangerousdan@channelgrabber.com>
2015-04-15 12:36:33 +0100

    CGIV-5353 fixed filter to search FBA also

--- module/Orders/src/Orders/Order/TableService/OrdersTableFulfilmentChannelColumns.php
+++ module/Orders/src/Orders/Order/TableService/OrdersTableFulfilmentChannelColumns.php
@@ -1,6 +1,7 @@
 <?php
 namespace Orders\Order\TableService;
 
+use CG\Amazon\Order\FulfilmentChannel\Mapper as AmazonFulfilmentMapper;
 use CG\Order\Shared\Entity as OrderEntity;
 use CG\User\ActiveUserInterface;
 use CG_UI\View\Filters\SelectOptionsInterface;
@@ -20,7 +21,8 @@ class OrdersTableFulfilmentChannelColumns implements SelectOptionsInterface
     public function getSelectOptions()
     {
         return [
-            OrderEntity::DEFAULT_FULFILMENT_CHANNEL => OrderEntity::DEFAULT_FULFILMENT_CHANNEL
+            OrderEntity::DEFAULT_FULFILMENT_CHANNEL => OrderEntity::DEFAULT_FULFILMENT_CHANNEL,
+            AmazonFulfilmentMapper::CG_FBA          => AmazonFulfilmentMapper::CG_FBA
         ];
     }
 
