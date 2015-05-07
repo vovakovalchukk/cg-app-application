f7d31b5fa52646dc7fecc7ad29a9855d2e528682
Andu <andua@channelgrabber.com>
2015-05-07 15:48:10 +0100

    CGIV-5350 Cache tax rates in Product TaxRate Service in order to improve performance and maintain the same interface

--- module/Products/src/Products/Product/TaxRate/Service.php
+++ module/Products/src/Products/Product/TaxRate/Service.php
@@ -3,41 +3,59 @@ namespace Products\Product\TaxRate;
 
 use CG\Product\Entity as Product;
 use CG\OrganisationUnit\Service as OrganisationUnitService;
-use CG\OrganisationUnit\Entity as OrganisationUnit;
 use CG\OrganisationUnit\MemberState\Decider as MemberStateDecider;
-use CG\Stdlib\Exception\Runtime\NotFound;
 use CG\Tax\Rates\Factory as TaxRatesFactory;
 
 class Service
 {
     protected $organisationUnitService;
+    protected $cache;
 
     public function __construct(OrganisationUnitService $organisationUnitService)
     {
         $this->setOrganisationUnitService($organisationUnitService);
+        $this->cache = [];
     }
 
     public function getTaxRatesOptionsForProduct(Product $product)
     {
-        //TODO: SLOW
-        $organisationUnit = $this->getOrganisationUnitService()->fetch($product->getOrganisationUnitId());
+        $organisationUnitId = $product->getOrganisationUnitId();
+
+        if(isset($this->cache[$organisationUnitId])) {
+            return $this->markRateOptionSelectedForProduct($product, $this->cache[$organisationUnitId]);
+        }
+
+        $organisationUnit = $this->getOrganisationUnitService()->fetch($organisationUnitId);
 
         $decider = new MemberStateDecider();
         $memberState = $decider($organisationUnit);
 
         $taxRateFactory = new TaxRatesFactory();
-        $rates = $taxRateFactory($memberState);
-        $ratesArray = $rates->getAll();
+        $rates = $taxRateFactory($memberState)->getAll();
 
-        $formattedRates = [];
-        foreach($ratesArray as $rateId => $rate) {
-            $formattedRates[$rateId] = [
+        $ratesOptions = [];
+        foreach($rates as $rateId => $rate) {
+            $ratesOptions[$rateId] = [
                 'name' => $rate->getName(),
-                'rate' => (int) ($rate->getCurrent() * 100),
-                'selected' => $product->getTaxRateId() === $rateId
+                'rate' => (int) ($rate->getCurrent() * 100)
             ];
         }
-        return $formattedRates;
+
+        if(!isset($this->cache[$organisationUnitId])) {
+            $this->cache[$organisationUnitId] = $ratesOptions;
+        }
+
+        return $this->markRateOptionSelectedForProduct($product, $ratesOptions);
+    }
+
+    protected function markRateOptionSelectedForProduct(Product $product, $ratesOptions)
+    {
+        if($product->getTaxRateId() == null || !isset($ratesOptions[$product->getTaxRateId()])) {
+            return $ratesOptions;
+        }
+
+        $ratesOptions[$product->getTaxRateId()]['selected'] = true;
+        return $ratesOptions;
     }
 
     /**
