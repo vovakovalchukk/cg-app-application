3acddb3f98e67d83e3b135b6253c1f84201978f9
Aaron Baker <aaronb@channelgrabber.com>
2015-05-08 15:38:30 +0100

    CGIV-5563: brought in Product library changes with improvements to embedding variations as links in HAL

--- module/Products/src/Products/Controller/ProductsJsonController.php
+++ module/Products/src/Products/Controller/ProductsJsonController.php
@@ -52,7 +52,7 @@ class ProductsJsonController extends AbstractActionController
             $filterParams['deleted'] = false;
         }
         $requestFilter = $this->getFilterMapper()->fromArray($filterParams);
-        $requestFilter->setVariationLinks(true);
+        $requestFilter->setEmbedVariationsAsLinks(true);
         $productsArray = [];
         try {
             $products = $this->getProductService()->fetchProducts($requestFilter, $requestFilter->getParentProductId(), $limit);
@@ -87,26 +87,8 @@ class ProductsJsonController extends AbstractActionController
             'accounts' => $accounts
         ]);
 
-        $product['variationCount'] = $productEntity->getVariations()->count();
-        if ($product['variationCount'] > 0) {
-            try {
-                $variations = $this->getProductService()->fetchProducts(
-                    $this->getFilterMapper()->fromArray(
-                        [
-                            'id' => $productEntity->getVariations()->getIds(),
-                        ]
-                    ),
-                    [$productEntity->getId()],
-                    2
-                );
-
-                foreach ($variations as $variation) {
-                    $product['variations'][] = $this->toArrayProductEntityWithEmbeddedData($variation, $accounts);
-                }
-            } catch (NotFound $exception) {
-                // Noop
-            }
-        }
+        $product['variationCount'] = count($productEntity->getVariationIds());
+        $product['variationIds'] = $productEntity->getVariationIds();
 
         if (!$productEntity->getStock() || count($productEntity->getVariations())) {
             return $product;
