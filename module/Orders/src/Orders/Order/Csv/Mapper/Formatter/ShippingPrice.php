fe92c0b72f844a254c532649a7e9ca31ef1d3dd9
Richard Heelin <richardh@channelgrabber.com>
2015-05-27 15:35:18 +0100

    CGIV-5702: Ensure we always return the shipping price for the first row

--- module/Orders/src/Orders/Order/Csv/Mapper/Formatter/ShippingPrice.php
+++ module/Orders/src/Orders/Order/Csv/Mapper/Formatter/ShippingPrice.php
@@ -9,10 +9,8 @@ class ShippingPrice implements FormatterInterface
     public function __invoke(Order $order, $fieldName)
     {
         $column = [];
-        if($order->getItems()->count() < 2) {
-            $column[] = $order->getShippingPrice();
-        }
 
+        $column[] = $order->getShippingPrice();
         for($i = 1; $i < $order->getItems()->count(); $i++) {
             $column[] = '';
         }
