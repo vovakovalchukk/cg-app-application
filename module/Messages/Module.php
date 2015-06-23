d4032e343f4fdac5b4b7b3ec78edcc8c2eece3ef
Aaron Baker <aaronb@channelgrabber.com>
2015-06-23 16:18:35 +0100

    CGIV-4707: added Messages module config

---
+++ module/Messages/Module.php
@@ -0,0 +1,41 @@
+<?php
+namespace Messages;
+
+use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
+use Zend\Config\Factory as ConfigFactory;
+use Zend\Mvc\MvcEvent;
+
+class Module implements DependencyIndicatorInterface
+{
+    const PUBLIC_FOLDER = '/cg-built/messages/';
+    const ROUTE = 'Messages';
+
+    public function onBootstrap(MvcEvent $event)
+    {
+    }
+
+    public function getConfig()
+    {
+        return ConfigFactory::fromFiles(
+            glob(__DIR__ . '/config/*.config.php')
+        );
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getModuleDependencies()
+    {
+        return [
+            'CG_UI',
+        ];
+    }
+}
