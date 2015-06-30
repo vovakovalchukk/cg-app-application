1ac30e9b689b454c55f6e7bfaa3811348b25f3a8
Aaron Baker <aaronb@channelgrabber.com>
2015-06-30 17:08:39 +0100

    CGIV-5839: Messages - added the buttons to the response form. W.I.P.

--- public/channelgrabber/messages/js/Messages/Module/ThreadDetails/Panel/Respond.js
+++ public/channelgrabber/messages/js/Messages/Module/ThreadDetails/Panel/Respond.js
@@ -21,16 +21,31 @@ define([
 
     Respond.SELECTOR = '.reply-section';
     Respond.TEMPLATE = '/channelgrabber/messages/template/Messages/ThreadDetails/Panel/respond.mustache';
+    Respond.TEMPLATE_BUTTONS = '/channelgrabber/zf2-v4-ui/templates/elements/buttons.mustache';
 
     Respond.prototype = Object.create(PanelAbstract.prototype);
 
     Respond.prototype.render = function(thread)
     {
         var self = this;
-        CGMustache.get().fetchTemplate(Respond.TEMPLATE, function(template, cgmustache) {
-            var html = cgmustache.renderTemplate(template, {
+        CGMustache.get().fetchTemplates({main: Respond.TEMPLATE, buttons: Respond.TEMPLATE_BUTTONS}, function(templates, cgmustache) {
+            var buttonHtml = cgmustache.renderTemplate(templates, {
+                'buttons': [{
+                    'id': 'respond-send-resolve',
+                    'action': 'send-resolve',
+                    'value': 'Send & Resolve',
+                    'type': 'button',
+                    'class': 'blue'
+                }, {
+                    'id': 'respond-send',
+                    'action': 'send',
+                    'value': 'Send',
+                    'type': 'button'
+                }]
+            }, 'buttons');
+            var html = cgmustache.renderTemplate(templates, {
                 'threadId': thread.getId()
-            });
+            }, 'main', {buttons: buttonHtml});
             self.getDomManipulator().append(PanelAbstract.SELECTOR_CONTAINER+' .message-section', html);
         });
     };
