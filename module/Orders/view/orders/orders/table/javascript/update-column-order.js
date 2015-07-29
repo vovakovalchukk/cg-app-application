eebef5e46b180a4dbb18521f7f328510970a4c71
Richard Heelin <richardh@channelgrabber.com>
2015-07-29 15:14:28 +0100

    CGIV-5896: Display a message to the user to let them know we have saved there column order

--- module/Orders/view/orders/orders/table/javascript/update-column-order.js
+++ module/Orders/view/orders/orders/table/javascript/update-column-order.js
@@ -15,6 +15,18 @@ $("#<?= $tableId ?>").on("fnPreDrawCallback", function(event, oSettings) {
     $.ajax({
         url: "<?= $this->url($route) ?>",
         type: 'POST',
-        data: columnData
+        data: columnData,
+        success: function(data) {
+            if (!data.updated) {
+                var errorMsg = (data.error ? data.error : 'There was a problem saving column order (code: 1)');
+                n.error(errorMsg);
+                return;
+            }
+            n.success('Column order saved');
+        },
+        error: function (error, textStatus, errorThrown) {
+            n.error('There was a problem saving column order (code: 2)');
+            return;
+        }
     });
 });
