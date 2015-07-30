8e4f893f0dc47d04912b268de55e3f53550e4f14
Richard Heelin <richardh@channelgrabber.com>
2015-07-30 12:48:02 +0100

    CGIV-5896: We can't trust data tables column order, so always base the column order on the dom

--- module/Orders/view/orders/orders/table/javascript/update-column-order.js
+++ module/Orders/view/orders/orders/table/javascript/update-column-order.js
@@ -1,44 +1,61 @@
-$("#<?= $tableId ?>").on("fnPreDrawCallback", function(event, oSettings) {
-    if (!oSettings.aoColumns || !oSettings.aoColumnsOrder) {
-        return;
-    }
+(function() {
+    var columnOrder;
 
-    var columnOrder = {};
-    $("th", oSettings.nTHead).each(function(position, column) {
-        columnOrder[position] = $(column).data("columnIndex");
-    });
-
-    var match = true;
-    var columnData = {};
-    for (var position in columnOrder) {
-        var column = oSettings.aoColumns[position];
-        if (!column || !column.mData) {
-            continue;
-        } else if (columnOrder[position] != oSettings.aoColumnsOrder[position]) {
-            match = false;
-        }
-        columnData['mDataProp_' + position] = column.mData;
-    }
-
-    if (match) {
-        return;
-    }
-
-    $.ajax({
-        url: "<?= $this->url($route) ?>",
-        type: 'POST',
-        data: columnData,
-        success: function(data) {
-            if (!data.updated) {
-                var errorMsg = (data.error ? data.error : 'There was a problem saving column order (code: 1)');
-                n.error(errorMsg);
-                return;
-            }
-            n.success('Column order saved');
-        },
-        error: function (error, textStatus, errorThrown) {
-            n.error('There was a problem saving column order (code: 2)');
+    $("#<?= $tableId ?>").on("fnInitComplete", function(event, oSettings) {
+        if (columnOrder || !oSettings.nTHead) {
             return;
         }
+
+        columnOrder = {};
+        $("th", oSettings.nTHead).each(function(position, column) {
+            columnOrder[position] = $(column).data("columnIndex");
+        });
     });
-});
+
+    $("#<?= $tableId ?>").on("fnPreDrawCallback", function(event, oSettings) {
+        if (!columnOrder || !oSettings.nTHead || !oSettings.aoColumns) {
+            return;
+        }
+
+        var currentOrder = {};
+        $("th", oSettings.nTHead).each(function(position, column) {
+            currentOrder[position] = $(column).data("columnIndex");
+        });
+
+        var match = true;
+        var columnData = {};
+        for (var position in currentOrder) {
+            var column = oSettings.aoColumns[position];
+            if (!column || !column.mData) {
+                continue;
+            } else if (currentOrder[position] != columnOrder[position]) {
+                match = false;
+            }
+            columnData['mDataProp_' + position] = column.mData;
+        }
+
+        if (match) {
+            return;
+        }
+
+        columnOrder = currentOrder;
+
+        $.ajax({
+            url: "<?= $this->url($route) ?>",
+            type: 'POST',
+            data: columnData,
+            success: function(data) {
+                if (!data.updated) {
+                    var errorMsg = (data.error ? data.error : 'There was a problem saving column order (code: 1)');
+                    n.error(errorMsg);
+                    return;
+                }
+                n.success('Column order saved');
+            },
+            error: function (error, textStatus, errorThrown) {
+                n.error('There was a problem saving column order (code: 2)');
+                return;
+            }
+        });
+    });
+})();
