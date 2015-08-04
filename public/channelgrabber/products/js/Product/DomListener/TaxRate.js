418430ff324082c9c8d02fcd52107eed838e48a0
Richard Heelin <richardh@channelgrabber.com>
2015-08-04 15:51:06 +0100

    CGIV-6057: Removed requirement for jquery to be loaded via require as was breaking other js that needed the global version

--- public/channelgrabber/products/js/Product/DomListener/TaxRate.js
+++ public/channelgrabber/products/js/Product/DomListener/TaxRate.js
@@ -1,55 +1,21 @@
 define([
-    'Product/Service',
-    'EventCollator'
-], function (service, eventCollator)
+    'Product/Service'
+], function (service)
 {
     var TaxRate = function()
     {
-        var vatRateSelector;
-
-        this.getVatRateSelector = function()
-        {
-            return vatRateSelector;
-        };
-
-        this.setVatRateSelector = function(selector)
-        {
-            vatRateSelector = selector;
-        };
-
-        this.getEventCollator = function()
-        {
-            return eventCollator;
-        };
-
         this.getService = function()
         {
             return service;
         }
     };
 
-    TaxRate.EVENT_COLLATOR_TYPE = 'ProductTaxRate';
-
     TaxRate.prototype.init = function(vatRateSelector)
     {
-        this.setVatRateSelector(vatRateSelector);
         var self = this;
-
-        $(document).on('change', this.getVatRateSelector(), function(event, container){
-            self.triggerRequestMadeEvent(container);
+        $(document).on("change", vatRateSelector, function(event, container) {
+            self.getService().saveTaxRate(container);
         });
-
-        $(document).on(eventCollator.getQueueTimeoutEventPrefix() + TaxRate.EVENT_COLLATOR_TYPE, function(event, data) {
-            self.getService().saveTaxRate(data[0]);
-        });
-    };
-
-    TaxRate.prototype.triggerRequestMadeEvent = function(container)
-    {
-        var unique = true;
-        $(document).trigger(this.getEventCollator().getRequestMadeEvent(), [
-            TaxRate.EVENT_COLLATOR_TYPE, container, unique
-        ]);
     };
 
     return new TaxRate();
