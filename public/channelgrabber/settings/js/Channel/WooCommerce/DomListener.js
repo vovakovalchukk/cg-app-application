c86b7908302a4c02d5e2ebb2fe76235fb66f9ce4
Richard Heelin <richardh@channelgrabber.com>
2015-09-16 16:59:20 +0100

    CGIV-6187: Setup dom listener to handle submitting account details to ajax url

---
+++ public/channelgrabber/settings/js/Channel/WooCommerce/DomListener.js
@@ -0,0 +1,11 @@
+define([], function() {
+    var DomListener = function() {};
+
+    DomListener.DOM_SELECTOR_ACCOUNT_FORM = '#woocommerce-form';
+
+    DomListener.prototype.init = function(saveUrl, accountId) {
+
+    };
+
+    return new DomListener();
+});
