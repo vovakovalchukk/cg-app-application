33cc1f099b2b7f6dfd2fc5e7c339b4e42459d8be
Richard Heelin <richardh@channelgrabber.com>
2015-09-17 09:19:41 +0100

    CGIV-6187: Updated the controller to create/update the account based on the passed details

--- module/Settings/src/Settings/Controller/ChannelControllerAbstract.php
+++ module/Settings/src/Settings/Controller/ChannelControllerAbstract.php
@@ -1,11 +1,12 @@
 <?php
 namespace Settings\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
 use CG\Account\CreationServiceAbstract as AccountCreationService;
 use CG\User\ActiveUserInterface;
+use CG\User\Entity as User;
 use CG_UI\View\Prototyper\JsonModelFactory;
 use CG_UI\View\Prototyper\ViewModelFactory;
+use Zend\Mvc\Controller\AbstractActionController;
 
 abstract class ChannelControllerAbstract extends AbstractActionController
 {
@@ -26,45 +27,77 @@ abstract class ChannelControllerAbstract extends AbstractActionController
             ->setViewModelFactory($viewModelFactory);
     }
 
+    /**
+     * @return self
+     */
     protected function setAccountCreationService(AccountCreationService $accountCreationService)
     {
         $this->accountCreationService = $accountCreationService;
         return $this;
     }
 
+    /**
+     * @return AccountCreationService
+     */
     protected function getAccountCreationService()
     {
         return $this->accountCreationService;
     }
 
+    /**
+     * @return self
+     */
     protected function setActiveUserContainer(ActiveUserInterface $activeUserContainer)
     {
         $this->activeUserContainer = $activeUserContainer;
         return $this;
     }
 
+    /**
+     * @return ActiveUserInterface
+     */
     protected function getActiveUserContainer()
     {
         return $this->activeUserContainer;
     }
 
+    /**
+     * @return User
+     */
+    protected function getActiveUser()
+    {
+        return $this->getActiveUserContainer()->getActiveUser();
+    }
+
+    /**
+     * @return self
+     */
     protected function setJsonModelFactory(JsonModelFactory $jsonModelFactory)
     {
         $this->jsonModelFactory = $jsonModelFactory;
         return $this;
     }
 
+    /**
+     * @return JsonModelFactory
+     */
     protected function getJsonModelFactory()
     {
         return $this->jsonModelFactory;
     }
 
+    /**
+     * @return self
+     */
     protected function setViewModelFactory(ViewModelFactory $viewModelFactory)
     {
         $this->viewModelFactory = $viewModelFactory;
         return $this;
     }
 
+    /**
+     * @return ViewModelFactory
+     */
     protected function getViewModelFactory()
     {
         return $this->viewModelFactory;
--- module/Settings/src/Settings/Controller/WooCommerceController.php
+++ module/Settings/src/Settings/Controller/WooCommerceController.php
@@ -1,6 +1,10 @@
 <?php
 namespace Settings\Controller;
 
+use CG\Account\Shared\Entity as Account;
+use CG\Channel\Type as ChannelType;
+use Settings\Module;
+use Zend\View\Model\JsonModel;
 use Zend\View\Model\ViewModel;
 
 class WooCommerceController extends ChannelControllerAbstract
@@ -17,5 +21,29 @@ class WooCommerceController extends ChannelControllerAbstract
         $index->setVariable('isSidebarVisible', false);
         $index->setVariable('accountId', $this->params()->fromQuery('accountId'));
         return $index;
+    }
+
+    public function saveAction()
+    {
+        /** @var JsonModel $save */
+        $save = $this->getJsonModelFactory()->newInstance();
+        $account = $this->getAccount();
+        $route = implode('/', [Module::ROUTE, ChannelController::ROUTE, ChannelController::ROUTE_CHANNELS, ChannelController::ROUTE_ACCOUNT]);
+        return $save->setVariable(
+            'redirectUrl',
+            $this->url()->fromRoute($route, ['type' => ChannelType::SALES, 'accountId' => $account->getId()])
+        );
+    }
+
+    /**
+     * @return Account
+     */
+    protected function getAccount()
+    {
+        return $this->getAccountCreationService()->connectAccount(
+            $this->getActiveUser()->getOrganisationUnitId(),
+            $this->params()->fromPost('accountId'),
+            $this->params()->fromPost()
+        );
     }
 } 
