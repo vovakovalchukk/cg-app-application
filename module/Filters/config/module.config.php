f59dcc823c583a877952589dc15578f9eab166b3
Richard Heelin <richardh@channelgrabber.com>
2015-10-09 09:02:57 +0100

    CGIV-6087: Marketplace filter now uses the new endpoint to get a list of avaliable marketplaces

--- module/Filters/config/module.config.php
+++ module/Filters/config/module.config.php
@@ -1,3 +1,18 @@
 <?php
+use CG\Listing\Unimported\Marketplace\Storage\Api as UnimportedListingMarketplaceApiStorage;
+use CG\Listing\Unimported\Marketplace\StorageInterface as UnimportedListingMarketplaceStorage;
 
-return [];
+return [
+    'di' => [
+        'instance' => [
+            'preferences' => [
+                UnimportedListingMarketplaceStorage::class => UnimportedListingMarketplaceApiStorage::class,
+            ],
+            UnimportedListingMarketplaceApiStorage::class => [
+                'parameters' => [
+                    'client' => 'cg_app_guzzle',
+                ],
+            ],
+        ],
+    ],
+];
--- module/Filters/src/Filters/Options/Marketplace.php
+++ module/Filters/src/Filters/Options/Marketplace.php
@@ -1,16 +1,68 @@
 <?php
 namespace Filters\Options;
 
+use CG\Listing\Unimported\Marketplace\Entity;
+use CG\Listing\Unimported\Marketplace\Filter;
+use CG\Listing\Unimported\Marketplace\Service;
+use CG\Stdlib\Exception\Runtime\NotFound;
+use CG\User\ActiveUserInterface;
+use CG\User\Entity as User;
 use CG_UI\View\Filters\SelectOptionsInterface;
 
 class Marketplace implements SelectOptionsInterface
 {
+    /** @var ActiveUserInterface $activeUserContainer */
+    protected $activeUserContainer;
+    /** @var Service $service */
+    protected $service;
+
+    public function __construct(ActiveUserInterface $activeUserContainer, Service $service)
+    {
+        $this->setActiveUserContainer($activeUserContainer)->setService($service);
+    }
+
     /**
      * @return array key => value pairs to be added to select filter options
      */
     public function getSelectOptions()
     {
-        // TODO: Return avaliable marketplaces
-        return [];
+        $marketplaces = [];
+        try {
+            $filter = new Filter($this->getActiveUser()->getOuList());
+            
+            /** @var Entity $marketplace */
+            foreach ($this->service->fetchCollectionByFilter($filter) as $marketplace) {
+                $marketplaces[$marketplace->getMarketplace()] = $marketplace->getMarketplace();
+            }
+        } catch (NotFound $exception) {
+            // No marketplacces have been assigned to OU
+        }
+        return $marketplaces;
+    }
+
+    /**
+     * @return User
+     */
+    public function getActiveUser()
+    {
+        return $this->activeUserContainer->getActiveUser();
+    }
+
+    /**
+     * @return self
+     */
+    protected function setActiveUserContainer(ActiveUserInterface $activeUserContainer)
+    {
+        $this->activeUserContainer = $activeUserContainer;
+        return $this;
+    }
+
+    /**
+     * @return self
+     */
+    protected function setService(Service $service)
+    {
+        $this->service = $service;
+        return $this;
     }
 } 
