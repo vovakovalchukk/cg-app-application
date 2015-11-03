5d88ad89ecec6401c7b367416362b84237f9fe33
Anddrew Fenna <andrew.fenna@channelgrabber.com>
2015-11-03 09:28:29 +0000

    CGIV-6346 first draft

--- public/channelgrabber/settings/js/ajaxSwitch.js
+++ public/channelgrabber/settings/js/ajaxSwitch.js
@@ -1,8 +1,10 @@
 define(
     ["ajaxCheckbox", "mustache"],
     function(AjaxCheckbox, Mustache) {
-        return function(notifications, baseSelector, selector, ajaxOptions, messages) {
-            var ajaxCheckbox = new AjaxCheckbox(notifications, baseSelector, selector, ajaxOptions);
+        console.log('ajaxSwitch pulled in');
+        return function(notifications, baseSelector, selector, ajaxOptions, messages, blockAction) {
+            console.log("ajaxSwitch", blockAction);
+            var ajaxCheckbox = new AjaxCheckbox(notifications, baseSelector, selector, ajaxOptions, blockAction);
 
             ajaxCheckbox.bindAjax(function() {
                 if (messages.info !== undefined) {
