096b01134acd9bc280c8c8979d0b233402a2e532
Anddrew Fenna <andrew.fenna@channelgrabber.com>
2015-11-19 12:05:40 +0000

    CGIV-6341 testing front end file location

--- module/Orders/config/filters.config.php
+++ module/Orders/config/filters.config.php
@@ -372,7 +372,7 @@ return [
                 )
             ],
             [
-                'name' => 'Cancelled & Refunded',
+                'name' => 'Cancelled & Refunded**',
                 'filter' => json_encode(
                     [
                         'status' => [
