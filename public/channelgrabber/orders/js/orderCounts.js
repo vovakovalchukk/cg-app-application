6eccf4487a0b292bdbb2902a249a4153aef970df
Anddrew Fenna <andrew.fenna@channelgrabber.com>
2015-11-26 11:50:56 +0000

    CGIV-6341 modified ordercounts js using prototype rather than 'this' and brought in JS using require

--- public/channelgrabber/orders/js/orderCounts.js
+++ public/channelgrabber/orders/js/orderCounts.js
@@ -1,61 +1,79 @@
-function orderCountsByAjax(organisationUnitId) {
+define([], function() {
 
-    var d = new Date();
-    var n = d.getTime();
-    var dataString = +'&time=' + n;
-    $.ajax
+    var OrderCounts = function()
+    {
+        ///organisationUnitId is taken from global
+        this.organisationUnitId = organisationUnitId;
+
+        var init = function() {
+            var self = this;
+            self.ajax();
+        };
+
+        init.call(this);
+    };
+
+    OrderCounts.prototype.ajax = function ()
+    {
+        var self = this;
+        var d = new Date();
+        var n = d.getTime();
+        var dataString = +'&time=' + n;
+
+        $.ajax
             ({
                 type: 'POST',
-                url: 'https://cg_app.orderhub.io.local/orderCounts/' + organisationUnitId,
+                url: '/orders/orderCounts/' + this.organisationUnitId,
                 data: dataString,
                 cache: false,
                 success: function (json)
                 {
-
-                    displayCounts(json);
-
-
-
+                    self.displayCounts(json);
+                },
+                error: function()
+                {
+                    alert('Order count request failed');
                 }
             });
-}
+    };
 
-function displayCounts() {
+    OrderCounts.prototype.displayCounts = function (json)
+    {
+        var self = this;
+        var status = json.status;
+        $('#allOrdersCount').html(status.allOrders);
+        $('#awaitingPaymentCount').html(status.awaitingPayment);
+        $('#newOrdersCount').html(status.newOrders);
+        $('#processingCount').html(status.processing);
+        $('#dispatchedCount').html(status.dispatched);
+        $('#cancelledAndRefundedCount').html(status.cancelledAndRefunded);
+        var maxCount = 0;
+        var batches = json.batches;
+        var batchesKeys = Object.keys(batches);
+        for (var b = 0; b < batchesKeys.length; b++) {
+            var batchId = batchesKeys[b];
+            var batchesSpanId = "batchCount-" + batchId;
+            var count = batches[batchId];
+            if(count > maxCount){maxCount = count;}
+            $('#' + batchesSpanId).html(count);
+        };
+        self.changeMarginOfDeleteCrossBasedOnBatchCountStringLength(maxCount);
+    };
 
-
-    ///this will eventually be the real json response
-    var json = ' { '
-            + '"status": {'
-            + '  "organisationUnitID": 1,'
-            + ' "allOrders": 26,'
-            + '  "awaitingPayment": 2,'
-            + '  "newOrders": 2,'
-            + '  "processing": 6,'
-            + ' "dispatched": 2,'
-            + '  "cancelledAndRefunded": 4'
-            + '},'
-            + '"batches":{"2-1":11,"2-2":21,"2-3":41,"2-4":51,"2-5":61}'
-            + '}';
-
-
-    var jsonArray = JSON.parse(json);
-
-    //status'
-    var status = jsonArray.status;
-    $('#allOrdersCount').html(status.allOrders);
-    $('#awaitingPaymentCount').html(status.awaitingPayment);
-    $('#newOrdersCount').html(status.newOrders);
-    $('#processingCount').html(status.processing);
-    $('#dispatchedCount').html(status.dispatched);
-    $('#cancelledAndRefundedCount').html(status.cancelledAndRefunded);
-
-    //batches
-    var batches = jsonArray.batches;
-    var batchesKeys = Object.keys(batches);
-    for (var b = 0; b < batchesKeys.length; b++) {
-        var batchId = batchesKeys[b];
-        var batchesSpanId = "batchCount-" + batchId;
-        var count = batches[batchId];
-        $('#' + batchesSpanId).html(count);
-    }
-}
+    OrderCounts.prototype.changeMarginOfDeleteCrossBasedOnBatchCountStringLength = function(maxCount)
+    {
+        var maxBatchCountCharacterLengthAllowed = 4;
+        var pixelsPerCharacter = 5;
+        var maxCountLength = maxCount.toString().length;
+            if(maxCountLength > maxBatchCountCharacterLengthAllowed){
+                var difference = maxCountLength - maxBatchCountCharacterLengthAllowed;
+                var pixelsToMoveCross = difference * pixelsPerCharacter;
+                var currentRight = $('.deletebatch').css("right").slice(0,-2);
+                var newRight = parseInt(currentRight) + parseInt(pixelsToMoveCross);
+                var cssPixels = newRight + "px";
+                $('.deletebatch').css("right",cssPixels);
+            };
+    };
+    
+    return OrderCounts;
+});
