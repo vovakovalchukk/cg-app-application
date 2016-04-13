0563da4f18b0abaaa36499be24bb12ea990c3fed
Richard Heelin <richardh@channelgrabber.com>
2016-04-13 12:20:28 +0100

    CGIV-6930: Only display provided accounts that support the passed order

--- module/Orders/src/Orders/Courier/Service.php
+++ module/Orders/src/Orders/Courier/Service.php
@@ -114,23 +114,33 @@ class Service implements LoggerAwareInterface
     /**
      * @return array
      */
-    public function getCourierOptions($selectedAccountId = null)
+    public function getCourierOptionsForOrder(Order $order, $selectedAccountId = null)
     {
-        return $this->getShippingAccountOptions($selectedAccountId);
+        $shippingAccounts = $this->getShippingAccounts($order);
+        return $this->convertShippingAccountsToOptions($shippingAccounts, $selectedAccountId);
     }
 
-    public function getShippingAccounts()
+    public function getShippingAccounts(Order $order = null)
     {
         $accounts = $this->traitGetShippingAccounts();
         $carrierAccounts = new AccountCollection(Account::class, __FUNCTION__);
+
+        /** @var Account $account */
         foreach ($accounts as $account)
         {
             // Only show 'provided' accounts (i.e. from Dataplug or NetDespatch)
             if (!$this->shippingChannelsProvider->isProvidedChannel($account->getChannel())) {
                 continue;
             }
+
+            // Only show accounts that support the requested order
+            if ($order && !$this->shippingChannelsProvider->isOrderSupported($account->getChannel(), $order)) {
+                continue;
+            }
+
             $carrierAccounts->attach($account);
         }
+
         return $carrierAccounts;
     }
 
@@ -209,7 +219,7 @@ class Service implements LoggerAwareInterface
                 'class' => 'courier-review-courier-select',
                 'blankOption' => false,
                 'searchField' => false,
-                'options' => $this->getCourierOptions($courierId),
+                'options' => $this->getCourierOptionsForOrder($order, $courierId),
             ],
             'service' => $service,
         ];
