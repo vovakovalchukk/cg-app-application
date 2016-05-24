78a83bc6147fae247ea93036f92c40bbec0d1630
Aaron Baker <aaronb@channelgrabber.com>
2016-05-24 10:15:42 +0100

    CGIV-7062: Setup Wizard - moved logic to determine the first step on to the Service.

--- module/SetupWizard/src/SetupWizard/Controller/IndexController.php
+++ module/SetupWizard/src/SetupWizard/Controller/IndexController.php
@@ -1,7 +1,6 @@
 <?php
 namespace SetupWizard\Controller;
 
-use CG_UI\View\Helper\NavigationMenu;
 use CG_UI\View\Prototyper\ViewModelFactory;
 use SetupWizard\Controller\Service;
 use Zend\Mvc\Controller\AbstractActionController;
@@ -14,21 +13,16 @@ class IndexController extends AbstractActionController
     protected $service;
     /** @var ViewModelFactory */
     protected $viewModelFactory;
-    /** @var NavigationMenu */
-    protected $navigationMenu;
 
-    public function __construct(Service $service, ViewModelFactory $viewModelFactory, NavigationMenu $navigationMenu)
+    public function __construct(Service $service, ViewModelFactory $viewModelFactory)
     {
         $this->setService($service)
-            ->setViewModelFactory($viewModelFactory)
-            ->setNavigationMenu($navigationMenu);
+            ->setViewModelFactory($viewModelFactory);
     }
 
     public function indexAction()
     {
-        $navMenuHelper = $this->navigationMenu->__invoke('setup-navigation');
-        $firstStepUri = $navMenuHelper->getFirstPageUri();
-        $this->redirect()->toUrl($firstStepUri);
+        $this->redirect()->toUrl($this->service->getFirstStepUri());
     }
 
     public function exampleAction()
@@ -48,12 +42,6 @@ class IndexController extends AbstractActionController
     protected function setViewModelFactory(ViewModelFactory $viewModelFactory)
     {
         $this->viewModelFactory = $viewModelFactory;
-        return $this;
-    }
-
-    protected function setNavigationMenu(NavigationMenu $navigationMenu)
-    {
-        $this->navigationMenu = $navigationMenu;
         return $this;
     }
 }
--- module/SetupWizard/src/SetupWizard/Controller/Service.php
+++ module/SetupWizard/src/SetupWizard/Controller/Service.php
@@ -2,15 +2,20 @@
 namespace SetupWizard\Controller;
 
 use CG_UI\View\Prototyper\ViewModelFactory;
+use CG_UI\View\Helper\NavigationMenu;
 use Zend\View\Model\ViewModel;
 
 class Service
 {
+    /** @var ViewModelFactory */
     protected $viewModelFactory;
+    /** @var NavigationMenu */
+    protected $navigationMenu;
 
-    public function __construct(ViewModelFactory $viewModelFactory)
+    public function __construct(ViewModelFactory $viewModelFactory, NavigationMenu $navigationMenu)
     {
-        $this->setViewModelFactory($viewModelFactory);
+        $this->setViewModelFactory($viewModelFactory)
+            ->setNavigationMenu($navigationMenu);
     }
 
     public function getSetupView($heading, $body, $footer = null)
@@ -47,9 +52,22 @@ class Service
         return $view;
     }
 
+    public function getFirstStepUri()
+    {
+        return $this->navigationMenu->getFirstPageUri();
+    }
+
     protected function setViewModelFactory(ViewModelFactory $viewModelFactory)
     {
         $this->viewModelFactory = $viewModelFactory;
+        return $this;
+    }
+
+
+    protected function setNavigationMenu(NavigationMenu $navigationMenu)
+    {
+        $navigationMenu->__invoke('setup-navigation');
+        $this->navigationMenu = $navigationMenu;
         return $this;
     }
 }
