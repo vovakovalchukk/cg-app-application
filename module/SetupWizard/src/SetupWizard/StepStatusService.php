bebce9982b2eb48afd4a71bb955df7be91f6fa7f
Aaron Baker <aaronb@channelgrabber.com>
2016-05-27 10:34:40 +0100

    CGIV-7064: Setup Wizard - added stats to the step status service so we can see how many people skip / complete each step. Also improved the logging.

--- module/SetupWizard/src/SetupWizard/StepStatusService.php
+++ module/SetupWizard/src/SetupWizard/StepStatusService.php
@@ -5,15 +5,19 @@ use CG\Intercom\Event\Service as EventService;
 use CG\Intercom\Event\Request as Event;
 use CG\Stdlib\Log\LoggerAwareInterface;
 use CG\Stdlib\Log\LogTrait;
+use CG\Stats\StatsAwareInterface;
+use CG\Stats\StatsTrait;
 use CG\User\ActiveUserInterface;
 
-class StepStatusService implements LoggerAwareInterface
+class StepStatusService implements LoggerAwareInterface, StatsAwareInterface
 {
     use LogTrait;
+    use StatsTrait;
 
     const EVENT_NAME_PREFIX = 'Setup ';
     const LOG_CODE = 'SetupStepStatus';
     const LOG_STATUS = 'User %d (OU %d) %s setup step \'%s\'';
+    const STAT_NAME = 'setup-wizard.%s.%s';
 
     /** @var EventService */
     protected $eventService;
@@ -32,7 +36,9 @@ class StepStatusService implements LoggerAwareInterface
         $user = $this->activeUserContainer->getActiveUser();
         $userId = $user->getId();
         $metaData = ['status' => $status];
-        $this->logInfo(static::LOG_STATUS, [$userId, $user->getOrganisationUnitId(), $status, $step], static::LOG_CODE);
+
+        $this->logInfo(static::LOG_STATUS, ['user' => $userId, 'ou' => $user->getOrganisationUnitId(), 'setupWizardStepStatus' => $status, 'setupWizardStep' => $step], static::LOG_CODE);
+        $this->statsIncrement(sprintf(static::STAT_NAME, $step, $status));
 
         $event = new Event($eventName, $userId, $metaData);
         $this->eventService->save($event);
