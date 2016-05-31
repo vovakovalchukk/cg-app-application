d00ec9277eec3b96bdcab92d0b9c2cea1959e145
Aaron Baker <aaronb@channelgrabber.com>
2016-05-31 12:06:11 +0100

    CGIV-7065: Setup Wizard - handled the skip / next buttons on the Channels step - checked some accounts have been added before allowing next and hid skip when accounts have been added (as it doesnt make sense after that).

--- public/channelgrabber/setup-wizard/js/steps/Channels.js
+++ public/channelgrabber/setup-wizard/js/steps/Channels.js
@@ -1,18 +1,51 @@
 define(['../SetupWizard.js'], function(setupWizard)
 {
-    function Channels()
+    function Channels(notifications)
     {
         this.getSetupWizard = function()
         {
             return setupWizard;
         };
 
+        this.getNotifications = function()
+        {
+            return notifications;
+        };
+
         var init = function()
         {
-            
+            this.registerNextCallback()
+                .hideSkipIfAccountsAdded();
         };
         init.call(this);
     }
+
+    Channels.SELECTOR_CHANNEL = '.setup-wizard-account-badge';
+    Channels.SELECTOR_SKIP = '.setup-wizard-skip-button';
+
+    Channels.prototype.registerNextCallback = function()
+    {
+        this.getSetupWizard().registerNextCallback(function()
+        {
+            if ($(Channels.SELECTOR_CHANNEL).length == 0) {
+                n.error('You must add at least one channel (or you can choose to skip this step)');
+                return false;
+            }
+            return true;
+        });
+
+        return this;
+    };
+
+    Channels.prototype.hideSkipIfAccountsAdded = function()
+    {
+        if ($(Channels.SELECTOR_CHANNEL).length == 0) {
+            return;
+        }
+        // If the user has added accounts then skipping doesnt make sense
+        $(Channels.SELECTOR_SKIP).hide();
+        return this;
+    };
 
     return Channels;
 });
