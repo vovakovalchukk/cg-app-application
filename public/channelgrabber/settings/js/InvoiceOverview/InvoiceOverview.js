74f9de5578fe088ff8cb67596c7c1906e6aca9cd
Jack Monk <jack.monk@channelgrabber.com>
2016-06-17 13:28:49 +0100

    CGIV-6919 Added the index view template and some placeholder HTML with some styling also added the start of the JS class for bringing in templates

---
+++ public/channelgrabber/settings/js/InvoiceOverview/InvoiceOverview.js
@@ -0,0 +1,16 @@
+define([
+    //'InvoiceOverview/TemplateList'
+], function(
+    //TemplateList
+) {
+    var InvoiceOverview = function(mountNode)
+    {
+
+    };
+
+    InvoiceOverview.prototype.addTemplate = function()
+    {
+    };
+
+    return InvoiceOverview;
+});
