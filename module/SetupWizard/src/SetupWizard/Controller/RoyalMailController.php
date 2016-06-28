6aaff16dbe9cb3ac3e0e0f9221b2017780c139a4
Jack Monk <jack.monk@channelgrabber.com>
2016-06-28 16:27:43 +0100

    CGIV-7104 Added a new controller for use with the new Royal Mail step in the setup wizard

---
+++ module/SetupWizard/src/SetupWizard/Controller/RoyalMailController.php
@@ -0,0 +1,71 @@
+<?php
+namespace SetupWizard\Controller;
+
+use CG_UI\View\Prototyper\ViewModelFactory;
+use SetupWizard\Module;
+use SetupWizard\Controller\Service as SetupService;
+use SetupWizard\Company\Service as CompanyService;
+use Zend\Mvc\Controller\AbstractActionController;
+
+class RoyalMailController extends AbstractActionController
+{
+    const ROUTE_ROYAL_MAIL = 'Royal Mail';
+
+    /** @var SetupService */
+    protected $setupService;
+    /** @var ViewModelFactory */
+    protected $viewModelFactory;
+    /** @var CompanyService */
+    protected $companyService;
+
+    public function __construct(
+        SetupService $setupService,
+        ViewModelFactory $viewModelFactory,
+        CompanyService $companyService
+    ) {
+        $this->setSetupService($setupService)
+            ->setViewModelFactory($viewModelFactory)
+            ->setCompanyService($companyService);
+    }
+
+    public function indexAction()
+    {
+        $rootOuId = $this->setupService->getActiveRootOuId();
+        $view = $this->viewModelFactory->newInstance();
+        $view->setTemplate('cg_netdespatch/setup_form/new')
+            ->setVariable('ou', $this->companyService->fetchOrganisationUnit($rootOuId))
+            ->setVariable('pickUri', $this->url()->fromRoute(Module::ROUTE . '/' . static::ROUTE_ROYAL_MAIL));
+
+        return $this->setupService->getSetupView('Add Royal Mail', $view, $this->getMainFooterView());
+    }
+
+    protected function getMainFooterView()
+    {
+        // No skip button for this step as it must be completed
+        $footer = $this->viewModelFactory->newInstance([
+            'buttons' => [
+                $this->setupService->getNextButtonViewConfig(),
+            ]
+        ]);
+        $footer->setTemplate('elements/buttons.mustache');
+        return $footer;
+    }
+
+    protected function setSetupService(SetupService $setupService)
+    {
+        $this->setupService = $setupService;
+        return $this;
+    }
+
+    protected function setViewModelFactory(ViewModelFactory $viewModelFactory)
+    {
+        $this->viewModelFactory = $viewModelFactory;
+        return $this;
+    }
+
+    protected function setCompanyService(CompanyService $companyService)
+    {
+        $this->companyService = $companyService;
+        return $this;
+    }
+}
