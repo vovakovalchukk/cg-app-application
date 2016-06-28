fb29244e6f5bd7c581cdeb1d7e47ddfc19f53f16
Jack Monk <jack.monk@channelgrabber.com>
2016-06-28 15:31:59 +0100

    CGIV-7104 Added config for the new royal mail step, and brought in the zf2-v4-ui library change for the new van icon

---
+++ module/SetupWizard/config/steps/royal-mail.config.php
@@ -0,0 +1,43 @@
+<?php
+
+use SetupWizard\Controller\RoyalMailController;
+use SetupWizard\Module;
+use Zend\Mvc\Router\Http\Literal;
+
+return [
+    'navigation' => [
+        'setup-navigation' => [
+            'Steps' => [
+                'pages' => [
+                    'royal-mail' => [
+                        'label' => 'Royal Mail',
+                        'title' => 'Royal Mail',
+                        'route' => Module::ROUTE . '/' . RoyalMailController::ROUTE_ROYAL_MAIL,
+                        'order' => 999,
+                        'sprite' => 'sprite-delivery-van-circle-25-white',
+                        'link' => false,
+                    ],
+                ]
+            ],
+        ],
+    ],
+    'router' => [
+        'routes' => [
+            Module::ROUTE => [
+                'child_routes' => [
+                    RoyalMailController::ROUTE_ROYAL_MAIL => [
+                        'type' => Literal::class,
+                        'options' => [
+                            'route' => '/royal-mail',
+                            'defaults' => [
+                                'controller' => RoyalMailController::class,
+                                'action' => 'index',
+                            ]
+                        ],
+                        'may_terminate' => true,
+                    ]
+                ],
+            ]
+        ]
+    ],
+];
