f4e8993f98f17be804137de2a1fc3d75d2519da4
Richard Heelin <richardh@channelgrabber.com>
2016-07-12 10:30:49 +0100

    CGIV-7194: Created the outline of the BigCommerce module

---
+++ module/BigCommerce/Module.php
@@ -0,0 +1,28 @@
+<?php
+namespace BigCommerce;
+
+use Zend\Config\Factory as ConfigFactory;
+use Zend\Loader\StandardAutoloader;
+
+class Module
+{
+    const PUBLIC_FOLDER = '/cg-built/bigcommerce';
+
+    public function getConfig()
+    {
+        return ConfigFactory::fromFiles(
+            glob(__DIR__ . '/config/*.config.php')
+        );
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return [
+            StandardAutoloader::class => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ],
+            ],
+        ];
+    }
+}
