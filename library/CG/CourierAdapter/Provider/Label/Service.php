e42105a95bbe97141e3e6da1e8765a2d1b65a879
Aaron Baker <aaronb@channelgrabber.com>
2016-07-19 12:04:30 +0100

    CGIV-7246: courier adapters - implemented manifest generation.

--- library/CG/CourierAdapter/Provider/Label/Service.php
+++ library/CG/CourierAdapter/Provider/Label/Service.php
@@ -2,16 +2,20 @@
 namespace CG\CourierAdapter\Provider\Label;
 
 use CG\Account\Shared\Entity as Account;
+use CG\Account\Shared\Manifest\Entity as AccountManifest;
 use CG\Channel\CarrierProviderServiceInterface;
+use CG\Channel\CarrierProviderServiceManifestInterface;
+use CG\CourierAdapter\Manifest\GeneratingInterface as ManifestGeneratingInterface;
 use CG\CourierAdapter\Provider\Implementation\Service as AdapterImplementationService;
 use CG\CourierAdapter\Provider\Label\Cancel as LabelCancelService;
 use CG\CourierAdapter\Provider\Label\Create as LabelCreateService;
+use CG\CourierAdapter\Provider\Manifest\Service as ManifestService;
 use CG\Order\Shared\Collection as OrderCollection;
 use CG\Order\Shared\Label\Collection as OrderLabelCollection;
 use CG\OrganisationUnit\Entity as OrganisationUnit;
 use CG\User\Entity as User;
 
-class Service implements CarrierProviderServiceInterface
+class Service implements CarrierProviderServiceInterface, CarrierProviderServiceManifestInterface
 {
     /** @var AdapterImplementationService */
     protected $adapterImplementationService;
@@ -19,15 +23,19 @@ class Service implements CarrierProviderServiceInterface
     protected $labelCreateService;
     /** @var LabelCancelService */
     protected $labelCancelService;
+    /** @var ManifestService */
+    protected $manifestService;
 
     public function __construct(
         AdapterImplementationService $adapterImplementationService,
         LabelCreateService $labelCreateService,
-        LabelCancelService $labelCancelService
+        LabelCancelService $labelCancelService,
+        ManifestService $manifestService
     ) {
         $this->setAdapterImplementationService($adapterImplementationService)
             ->setLabelCreateService($labelCreateService)
-            ->setLabelCancelService($labelCancelService);
+            ->setLabelCancelService($labelCancelService)
+            ->setManifestService($manifestService);
     }
 
     /**
@@ -54,6 +62,31 @@ class Service implements CarrierProviderServiceInterface
     public function cancelOrderLabels(OrderLabelCollection $orderLabels, Account $shippingAccount)
     {
         $this->labelCancelService->cancelOrderLabels($orderLabels, $shippingAccount);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isManifestingAllowedForAccount(Account $account)
+    {
+        $courierInstance = $this->adapterImplementationService->getAdapterImplementationCourierInstanceForAccount($account);
+        return ($courierInstance instanceof ManifestGeneratingInterface);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isManifestingOnlyAllowedOncePerDayForAccount(Account $account)
+    {
+        return false;
+    }
+
+    /**
+     * @return null
+     */
+    public function createManifestForAccount(Account $shippingAccount, AccountManifest $accountManifest)
+    {
+        $this->manifestService->createManifestForAccount($shippingAccount, $accountManifest);
     }
 
     /**
@@ -87,6 +120,12 @@ class Service implements CarrierProviderServiceInterface
     protected function setLabelCancelService(LabelCancelService $labelCancelService)
     {
         $this->labelCancelService = $labelCancelService;
+        return $this;
+    }
+
+    protected function setManifestService(ManifestService $manifestService)
+    {
+        $this->manifestService = $manifestService;
         return $this;
     }
 }
