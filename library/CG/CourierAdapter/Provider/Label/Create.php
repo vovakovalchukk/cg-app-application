cfbfa6fb96cda5683ebf9ca0e02bb4205449697f
Aaron Baker <aaronb@channelgrabber.com>
2016-07-20 10:33:07 +0100

    CGIV-7246: courier adapters - handled exceptions from the adapter during shopment creation as well as booking in case they do their validation early.

--- library/CG/CourierAdapter/Provider/Label/Create.php
+++ library/CG/CourierAdapter/Provider/Label/Create.php
@@ -3,6 +3,7 @@ namespace CG\CourierAdapter\Provider\Label;
 
 use CG\Account\Shared\Entity as Account;
 use CG\CourierAdapter\CourierInterface;
+use CG\CourierAdapter\DeliveryServiceInterface;
 use CG\CourierAdapter\Exception\OperationFailed;
 use CG\CourierAdapter\Exception\UserError;
 use CG\CourierAdapter\LabelInterface;
@@ -40,6 +41,8 @@ class Create implements LoggerAwareInterface
     const LOG_START = 'Create labels request received for OU %d, Shipping Account %d (carrier %s), orders %s';
     const LOG_END = 'Finished creating labels for OU %d, Shipping Account %d (carrier %s), orders %s';
     const LOG_SHIPMENTS = 'Creating shipments from Orders for OU %d, Shipping Account %d (carrier %s)';
+    const LOG_SHIPMENT_INVALID = 'Creating shipment for Order %s failed with a UserError exception, message "%s"';
+    const LOG_SHIPMENT_ERROR = 'Creating shipment for Order %s resulted in an OperationFailed exception with message "%s"';
     const LOG_BOOK = 'Booking shipments for OU %d, Shipping Account %d (carrier %s)';
     const LOG_BOOK_INVALID = 'Booking shipment for Order %s failed with a UserError exception, message "%s"';
     const LOG_BOOK_ERROR = 'Booking shipment for Order %s resulted in an OperationFailed exception with message "%s"';
@@ -137,7 +140,7 @@ class Create implements LoggerAwareInterface
             $caShipmentData = $this->mapper->ohOrderAndDataToCAShipmentData(
                 $order, $orderData, $shippingAccount, $rootOu, $shipmentClass, $packages
             );
-            $shipments[$order->getId()] = $deliveryService->createShipment($caShipmentData);
+            $shipments[$order->getId()] = $this->createShipment($deliveryService, $caShipmentData, $order->getId());
 
             $this->removeGlobalLogEventParam('order');
         }
@@ -157,6 +160,37 @@ class Create implements LoggerAwareInterface
         return $packages;
     }
 
+    protected function createShipment(DeliveryServiceInterface $deliveryService, array $caShipmentData, $orderId)
+    {
+        try {
+            return $deliveryService->createShipment($caShipmentData);
+
+        } catch (UserError $e) {
+            $this->logNotice(static::LOG_SHIPMENT_INVALID, [$orderId, $e->getMessage()], [static::LOG_CODE, 'ShipmentInvalid']);
+            return $this->userErrorToValidationMessagesException($e, $orderId);
+
+        } catch (OperationFailed $e) {
+            $this->logWarning(static::LOG_SHIPMENT_ERROR, [$orderId, $e->getMessage()], [static::LOG_CODE, 'ShipmentError']);
+            return $this->operationFailedToValidationMessagesException($e, $orderId);
+        }
+    }
+
+    protected function userErrorToValidationMessagesException(UserError $e, $orderId)
+    {
+        $exception = new ValidationMessagesException('Validation error');
+        $errorCode = ($e->getCode() ?: HttpStatusCode::BAD_REQUEST);
+        $exception->addError($e->getMessage(), $orderId . ':' . $errorCode);
+        return $exception;
+    }
+
+    protected function operationFailedToValidationMessagesException(OperationFailed $e, $orderId)
+    {
+        $exception = new ValidationMessagesException('Validation error');
+        $errorCode = ($e->getCode() ?: HttpStatusCode::INTERNAL_SERVER_ERROR);
+        $exception->addError('There was an unexpected problem creating this shipment', $orderId . ':' . $errorCode);
+        return $exception;
+    }
+
     protected function bookShipments(
         array $shipments,
         OrderCollection $orders,
@@ -167,8 +201,14 @@ class Create implements LoggerAwareInterface
     ) {
         $results = [];
         foreach ($shipments as $orderId => $shipment) {
-            $this->addGlobalLogEventParam('order', $orderId);
 
+            // Handle shipments that errored at the creation stage
+            if ($shipment instanceof ValidationMessagesException) {
+                $results[$orderId] = $shipment;
+                continue;
+            }
+
+            $this->addGlobalLogEventParam('order', $orderId);
             try {
                 $bookedShipment = $courierInstance->bookShipment($shipment);
 
@@ -182,16 +222,12 @@ class Create implements LoggerAwareInterface
 
             } catch (UserError $e) {
                 $this->logNotice(static::LOG_BOOK_INVALID, [$orderId, $e->getMessage()], [static::LOG_CODE, 'BookInvalid']);
-                $exception = new ValidationMessagesException('Validation error');
-                $errorCode = ($e->getCode() ?: HttpStatusCode::BAD_REQUEST);
-                $exception->addError($e->getMessage(), $orderId . ':' . $errorCode);
+                $exception = $this->userErrorToValidationMessagesException($e, $orderId);
                 $results[$orderId] = $exception;
 
             } catch (OperationFailed $e) {
                 $this->logWarning(static::LOG_BOOK_ERROR, [$orderId, $e->getMessage()], [static::LOG_CODE, 'BookError']);
-                $exception = new ValidationMessagesException('Validation error');
-                $errorCode = ($e->getCode() ?: HttpStatusCode::INTERNAL_SERVER_ERROR);
-                $exception->addError('There was an unexpected problem creating this shipment', $orderId . ':' . $errorCode);
+                $exception = $this->operationFailedToValidationMessagesException($e, $orderId);
                 $results[$orderId] = $exception;
             }
 
