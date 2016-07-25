6c668447b9c8195e6d7e5e44561cfb065e19079d
Jack Monk <jack.monk@channelgrabber.com>
2016-07-25 15:32:40 +0100

    CGIV-7068 Added code to deal with the account badge sections of the messages page

--- module/SetupWizard/src/SetupWizard/Controller/MessagesController.php
+++ module/SetupWizard/src/SetupWizard/Controller/MessagesController.php
@@ -29,9 +29,30 @@ class MessagesController extends AbstractActionController
 
     public function indexAction()
     {
+        /**
+         * Get flag value for Email Invoice on Dispatch
+         *
+         *  For each amazon account
+         *      Check if we have messaging setup for this account
+         *      if we do then show the forwarding form
+         *      else show the add messaging form
+         *
+         *
+         *  POST to /amazon/account/save
+         *  with variable originalEmailAddress as the forwarding email address
+         */
         $view = $this->viewModelFactory->newInstance()->setTemplate('setup-wizard/messages/index');
 
         $view->setVariable('emailInvoiceDispatchToggleValue', $this->messagesService->getEmailInvoiceOnDispatchToggleValue());
+        
+        foreach ($this->messagesService->fetchAccountsForActiveUser() as $account) {
+            if ($account->getChannel() !== 'amazon') {
+                continue;
+            }
+
+            $section = $this->messagesService->getAccountBadgeSection($account);
+            $view->addChild($section, 'accountBadges', true);
+        }
 
         return $this->setupService->getSetupView('Set Up Customer Messages', $view);
     }
--- module/SetupWizard/src/SetupWizard/Messages/Service.php
+++ module/SetupWizard/src/SetupWizard/Messages/Service.php
@@ -5,10 +5,11 @@ use CG\Account\Client\Filter as AccountFilter;
 use CG\Account\Client\Service as AccountService;
 use CG\Account\Credentials\Cryptor as AmazonCryptor;
 use CG\Channel\Type as ChannelType;
-use CG\Channel\Service as ChannelService;
 use CG\Settings\Invoice\Service\Service as InvoiceSettingsService;
 use CG\User\ActiveUserInterface;
+use SetupWizard\Channels\Service as ChannelService;
 use SetupWizard\Module;
+use CG_UI\View\Prototyper\ViewModelFactory;
 
 class Service
 {
@@ -22,19 +23,23 @@ class Service
     protected $channelService;
     /** @var InvoiceSettingsService */
     protected $invoiceSettingsService;
+    /** @var ViewModelFactory */
+    protected $viewModelFactory;
 
     public function __construct(
         ActiveUserInterface $activeUserContainer,
         AccountService $accountService,
         AmazonCryptor $amazonCryptor,
         ChannelService $channelService,
-        InvoiceSettingsService $invoiceSettingsService
+        InvoiceSettingsService $invoiceSettingsService,
+        ViewModelFactory $viewModelFactory
     ) {
         $this->setActiveUserContainer($activeUserContainer)
             ->setAccountService($accountService)
             ->setAmazonCryptor($amazonCryptor)
             ->setChannelService($channelService)
-            ->setInvoiceSettingsService($invoiceSettingsService);
+            ->setInvoiceSettingsService($invoiceSettingsService)
+            ->setViewModelFactory($viewModelFactory);
     }
 
     public function getEmailInvoiceOnDispatchToggleValue()
@@ -45,6 +50,31 @@ class Service
             return true;
         }
         return false;
+    }
+
+    public function getAccountBadgeSection($account)
+    {
+        //$setupFlag = $account->isMessagingSetup();
+
+        $badgeSection = $this->viewModelFactory->newInstance([
+            'setupFlag' => true,
+        ]);
+        $badgeSection->addChild($this->getAccountBadge($account), 'badge');
+        $badgeSection->setTemplate('setup-wizard/messages/accountBadgeSection');
+
+        return $badgeSection;
+    }
+    
+    protected function getAccountBadge($account)
+    {
+        $img = $this->channelService->getImageFromAccount($account);
+        $badgeView = $this->viewModelFactory->newInstance([
+            'image' => $img,
+            'id' => $account->getId(),
+            'name' => $account->getDisplayName(),
+        ]);
+        $badgeView->setTemplate('setup-wizard/channels/account-badge.mustache');
+        return $badgeView;
     }
 
     public function fetchAccountsForActiveUser()
@@ -65,33 +95,57 @@ class Service
         return $this->accountService->fetchByFilter($filter);
     }
 
+    /**
+     * @return self
+     */
     protected function setActiveUserContainer(ActiveUserInterface $activeUserContainer)
     {
         $this->activeUserContainer = $activeUserContainer;
         return $this;
     }
 
+    /**
+     * @return self
+     */
     protected function setAccountService(AccountService $accountService)
     {
         $this->accountService = $accountService;
         return $this;
     }
 
+    /**
+     * @return self
+     */
     protected function setAmazonCryptor(AmazonCryptor $amazonCryptor)
     {
         $this->amazonCryptor = $amazonCryptor;
         return $this;
     }
 
+    /**
+     * @return self
+     */
     protected function setChannelService(ChannelService $channelService)
     {
         $this->channelService = $channelService;
         return $this;
     }
 
+    /**
+     * @return self
+     */
     public function setInvoiceSettingsService(InvoiceSettingsService $invoiceSettingsService)
     {
         $this->invoiceSettingsService = $invoiceSettingsService;
+        return $this;
+    }
+
+    /**
+     * @return self
+     */
+    protected function setViewModelFactory(ViewModelFactory $viewModelFactory)
+    {
+        $this->viewModelFactory = $viewModelFactory;
         return $this;
     }
 }
