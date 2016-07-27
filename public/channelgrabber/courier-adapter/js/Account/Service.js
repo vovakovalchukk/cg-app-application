13a99f32ef312fdc64e4f45d287412a9605da3e9
Aaron Baker <aaronb@channelgrabber.com>
2016-07-27 12:04:57 +0100

    CGIV-7257: courier adapters - improved JS validation of the setup / request credentials forms to show better error messages.

--- public/channelgrabber/courier-adapter/js/Account/Service.js
+++ public/channelgrabber/courier-adapter/js/Account/Service.js
@@ -47,6 +47,10 @@ define([
         $(EventHandler.SELECTOR_FORM).ajaxSubmit({
             "dataType": "json",
             "success": function(response) {
+                if (!response.valid && response.messages) {
+                    self.showErrorMessages(response.messages);
+                    return;
+                }
                 n.success(self.getSavedNotification());
                 window.location = response.redirectUrl;
             },
@@ -66,10 +70,30 @@ define([
             }
         });
         if (errors.length > 0) {
-            n.error('The following fields are required: '+errors.join(', '));
+            n.error('The following fields are required:<br /><ul><li>' + errors.join('</li><li>') + '</li></ul>');
             return false;
         }
         return true;
+    };
+
+    Service.prototype.showErrorMessages = function(messages)
+    {
+        var errors = this.parseErrorMessages(messages);
+        n.error('There were errors when submitting the form:<br /><ul><li>' + errors.join('</li><li>') + '</li></ul>');
+    };
+
+    Service.prototype.parseErrorMessages = function(messages, prefix)
+    {
+        prefix = prefix || '';
+        var errors = [];
+        for (var index in messages) {
+            if (typeof messages[index] == 'object') {
+                errors = errors.concat(this.parseErrorMessages(messages[index], prefix + index+': '));
+                continue;
+            }
+            errors.push(prefix + index + ': ' + messages[index]);
+        }
+        return errors;
     };
 
     return Service;
