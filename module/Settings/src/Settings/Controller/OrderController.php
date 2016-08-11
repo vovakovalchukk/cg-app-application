b2172600159fbac022d7dd66e0690ca88ed803b9
Aaron Baker <aaronb@channelgrabber.com>
2016-08-11 16:09:53 +0100

    CGIV-7043: settings - renamed the 'Picking Management' section to 'Order Management' and added an 'Orders' section for managing the new 'Auto-archive timeframe' setting.

---
+++ module/Settings/src/Settings/Controller/OrderController.php
@@ -0,0 +1,117 @@
+<?php
+namespace Settings\Controller;
+
+use CG\Http\Exception\Exception3xx\NotModified;
+use CG\Settings\Order\AutoArchiveTimeframe;
+use CG\Settings\Order\Service as OrderSettingsService;
+use CG\User\ActiveUserInterface as ActiveUserContainer;
+use CG_UI\View\Prototyper\JsonModelFactory;
+use CG_UI\View\Prototyper\ViewModelFactory;
+use Zend\Mvc\Controller\AbstractActionController;
+
+class OrderController extends AbstractActionController
+{
+    const ROUTE = 'Orders';
+    const ROUTE_SAVE = 'Save';
+
+    /** @var ViewModelFactory */
+    protected $viewModelFactory;
+    /** @var JsonModelFactory */
+    protected $jsonModelFactory;
+    /** @var ActiveUserContainer */
+    protected $activeUserContainer;
+    /** @var OrderSettingsService */
+    protected $orderSettingsService;
+
+    public function __construct(
+        ViewModelFactory $viewModelFactory,
+        JsonModelFactory $jsonModelFactory,
+        ActiveUserContainer $activeUserContainer,
+        OrderSettingsService $orderSettingsService
+    ) {
+        $this->setViewModelFactory($viewModelFactory)
+            ->setJsonModelFactory($jsonModelFactory)
+            ->setActiveUserContainer($activeUserContainer)
+            ->setOrderSettingsService($orderSettingsService);
+    }
+
+
+    public function indexAction()
+    {
+        $settings = $this->getOrderSettings();
+        return $this->viewModelFactory->newInstance([
+            'autoArchiveTimeframeSelect' => $this->getAutoArchiveTimeframeSelect($settings->getAutoArchiveTimeframe()),
+            'eTag' => $settings->getStoredETag(),
+        ]);
+    }
+
+    protected function getOrderSettings()
+    {
+        $rootOuId = $this->activeUserContainer->getActiveUserRootOrganisationUnitId();
+        return $this->orderSettingsService->fetch($rootOuId);
+    }
+
+    protected function getAutoArchiveTimeframeSelect($selected = null)
+    {
+        $options = [];
+        foreach (AutoArchiveTimeframe::getAllTimeframes() as $timeframe) {
+            $options[] = [
+                'value' => $timeframe,
+                'text' => $timeframe,
+                'selected' => ($selected == $timeframe),
+            ];
+        }
+
+        $customSelect = $this->getViewModelFactory()->newInstance([
+            'name' => 'autoArchiveTimeframe',
+            'id' => 'autoArchiveTimeframe-custom-select',
+            'options' => $options
+        ]);
+        $customSelect->setTemplate('elements/custom-select.mustache');
+        return $customSelect;
+    }
+
+    public function saveAction()
+    {
+        $eTag = $this->params()->fromPost('eTag');
+        $autoArchiveTimeframe = $this->params()->fromPost('autoArchiveTimeframe');
+
+        $settings = $this->getOrderSettings();
+        $settings->setAutoArchiveTimeframe($autoArchiveTimeframe)
+            ->setStoredETag($eTag);
+
+        try {
+            $settings = $this->orderSettingsService->save($settings);
+        } catch (NotModified $e) {
+            // No-op
+        }
+
+        return $this->jsonModelFactory->newInstance([
+            'eTag' => $settings->getStoredETag()
+        ]);
+    }
+
+    protected function setViewModelFactory(ViewModelFactory $viewModelFactory)
+    {
+        $this->viewModelFactory = $viewModelFactory;
+        return $this;
+    }
+
+    protected function setJsonModelFactory(JsonModelFactory $jsonModelFactory)
+    {
+        $this->jsonModelFactory = $jsonModelFactory;
+        return $this;
+    }
+
+    protected function setActiveUserContainer(ActiveUserContainer $activeUserContainer)
+    {
+        $this->activeUserContainer = $activeUserContainer;
+        return $this;
+    }
+
+    protected function setOrderSettingsService(OrderSettingsService $orderSettingsService)
+    {
+        $this->orderSettingsService = $orderSettingsService;
+        return $this;
+    }
+}
