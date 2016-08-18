86b10036a5cd400771b3d384ac82d520d31a244e
Jack Monk <jack.monk@channelgrabber.com>
2016-08-18 15:07:33 +0100

    CGIV-7254 Added the functionality to modify the input value, and moved the onClicks to the list element so it registers when clicking anywhere on the area rather than just the visual

--- public/channelgrabber/products/js/Product/Components/Input.jsx
+++ public/channelgrabber/products/js/Product/Components/Input.jsx
@@ -25,13 +25,17 @@ define([
             this.setState({ editable: true });
         },
         cancelInput: function () {
-            this.setState({ editable: false });
+            this.setState({
+                editable: false,
+                newValue: this.state.oldValue
+            });
         },
         submitInput: function () {
-            if (this.state.editable) {
-                //  call submit callback
-                console.log('Submitting Input as '+this.state.newValue);
+            if (! this.state.editable) {
+                return;
             }
+            //  call submit callback
+            console.log('Submitting Input as '+this.state.newValue);
         },
         mouseOver: function () {
             this.setState({ hover: true });
@@ -39,20 +43,28 @@ define([
         mouseOut: function () {
             this.setState({ hover: false });
         },
+        onChange: function (e) {
+            if (! this.state.editable) {
+                return;
+            }
+            this.setState({
+                newValue: e.target.value
+            });
+        },
         render: function () {
             return (
                 <div className="detail-text-holder">
                     <div className="submit-input active">
-                        <input type={this.props.type} className="submit-inputbox product-detail" placeholder="" value={this.props.value} name={this.props.name} step={this.props.step} />
+                        <input type={this.props.type} className="submit-inputbox product-detail" onChange={this.onChange} value={this.state.newValue} name={this.props.name} step={this.props.step} />
                         <div className="edit-btn" style={{display: (this.state.editable ? "none" : "inline-block")}}>
                             <ul>
-                                <li><span className="edit" onClick={this.editInput}></span></li>
+                                <li onClick={this.editInput}><span className="edit"></span></li>
                             </ul>
                         </div>
                         <div className="submit-cancel" style={{display: (this.state.editable ? "inline-block" : "none")}}>
                             <ul>
-                                <li><span className="submit" onClick={this.submitInput}></span></li>
-                                <li><span className="cancel" onClick={this.cancelInput}></span></li>
+                                <li onClick={this.submitInput}><span className="submit"></span></li>
+                                <li onClick={this.cancelInput}><span className="cancel"></span></li>
                             </ul>
                         </div>
                     </div>
