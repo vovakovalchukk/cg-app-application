b81e9aa71e3daabc91923786f80b4c3d818bab55
Jack Monk <jack.monk@channelgrabber.com>
2016-08-30 15:34:53 +0100

    CGIV-7254 Altered the variation/image/sku table to keep the image and sku columns the same width no matter what, the variations will automatically overflow with a scroll bar if they exceed their allowed dimensions

--- public/channelgrabber/products/js/Product/Components/VariationView.jsx
+++ public/channelgrabber/products/js/Product/Components/VariationView.jsx
@@ -7,23 +7,23 @@ define([
 
     var VariationViewComponent = React.createClass({
         getAttributeHeaders: function() {
-            var headers = [
-                <th key="image" className="image-col">Image</th>,
-                <th key="sky" className="sku-col">SKU</th>
-            ];
+            var headers = [];
             this.props.attributeNames.forEach(function(attributeName) {
                 headers.push(<th key={attributeName}>{attributeName}</th>);
             });
+            if (! headers.length) {
+                headers.push(<th></th>);
+            }
             return headers;
         },
         getAttributeValues: function(variation) {
-            var values = [
-                <td key="image"><img src={variation.images.length > 0 ? variation.images[0]['url'] : this.context.imageBasePath + '/noproductsimage.png'} /></td>,
-                <td key="sku">{variation.sku}</td>
-            ];
+            var values = [];
             this.props.attributeNames.forEach(function (attributeName) {
                 values.push(<td key={attributeName}>{variation.attributeValues[attributeName]}</td>);
             });
+            if (! values.length) {
+                values.push(<td></td>);
+            }
             return values;
         },
         getDefaultProps: function() {
@@ -37,22 +37,47 @@ define([
             var count = 0;
             return (
                 <div className="variation-table">
-                    <table>
-                        <thead>
-                            <tr>
-                                {this.getAttributeHeaders()}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.props.variations.map(function (variation) {
-                                if ((! this.props.fullView) && count > 1) {
-                                    return;
-                                }
-                                count++;
-                                return <tr key={variation.id}>{this.getAttributeValues(variation)}</tr>;
-                            }.bind(this))}
-                        </tbody>
-                    </table>
+                    <div className="image-sku-table">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th key="image" className="image-col">Image</th>
+                                    <th key="sky" className="sku-col">SKU</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.props.variations.map(function (variation) {
+                                    if ((! this.props.fullView) && count > 1) {
+                                        return;
+                                    }
+                                    return (
+                                        <tr key={variation.id}>
+                                            <td key="image"><img src={variation.images.length > 0 ? variation.images[0]['url'] : this.context.imageBasePath + '/noproductsimage.png'} /></td>
+                                            <td key="sku">{variation.sku}</td>
+                                        </tr>
+                                    );
+                                }.bind(this))}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="variations-table">
+                        <table>
+                            <thead>
+                                <tr>
+                                    {this.getAttributeHeaders()}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.props.variations.map(function (variation) {
+                                    if ((! this.props.fullView) && count > 1) {
+                                        return;
+                                    }
+                                    count++;
+                                    return <tr key={variation.id}>{this.getAttributeValues(variation)}</tr>;
+                                }.bind(this))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             );
         }
