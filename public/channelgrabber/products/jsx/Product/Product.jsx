5770dcccd10b922826f9e8431b53fecbff7d882b
Jack Monk <jack.monk@channelgrabber.com>
2016-09-05 09:11:12 +0100

    CGIV-7254 Removed debug

--- public/channelgrabber/products/jsx/Product/Product.jsx
+++ public/channelgrabber/products/jsx/Product/Product.jsx
@@ -9,7 +9,6 @@ define([
 ) {
     var Product = function(mountingNode, imageBasePath, searchAvailable)
     {
-        console.log('producstsda here compiles');
         ReactDOM.render(<RootComponent productsUrl="/products/ajax" imageBasePath={imageBasePath} searchAvailable={searchAvailable}/>, mountingNode);
     };
 
