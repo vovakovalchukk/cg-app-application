18f27dee98637d61ad270d324638dfb842b88f06
Jack Monk <jack.monk@channelgrabber.com>
2016-09-05 10:16:53 +0100

    CGIV-7254 Removed extra entry for requirejs from an incorrect merge

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,27 +78,6 @@ module.exports = function(grunt) {
                 }
             }
         },
-        requirejs: {
-            compile: {
-                options: {
-                    appDir: "public/channelgrabber",
-                    baseUrl: "zf2-v4-ui/js/",
-                    mainConfigFile: "public/channelgrabber/zf2-v4-ui/js/main.js",
-                    dir: "public/cg-built",
-                    paths: {
-                        orders: "../../../public/channelgrabber/orders",
-                        Filters: "../../../public/channelgrabber/filters/js",
-                    },
-                    modules: [{
-                        name: "main"
-                    }, {
-                        name: "element/moreButton",
-                    }, {
-                        name: "popup/mustache",
-                    }]
-                }
-            }
-        },
         shell: {
             options: {
                 stderr: false
