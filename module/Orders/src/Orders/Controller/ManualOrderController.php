ec66afaae7469772dbdb27530f8e9d796ed9baca
Aaron Baker <aaronb@channelgrabber.com>
2016-09-05 10:18:31 +0100

    CGIV-7392: added usage limit check to the new manual order controller.

--- module/Orders/src/Orders/Controller/ManualOrderController.php
+++ module/Orders/src/Orders/Controller/ManualOrderController.php
@@ -2,20 +2,29 @@
 namespace Orders\Controller;
 
 use CG_UI\View\Prototyper\ViewModelFactory;
+use CG_Usage\Exception\Exceeded as UsageExceeded;
+use CG_Usage\Service as UsageService;
 use Zend\Mvc\Controller\AbstractActionController;
 
 class ManualOrderController extends AbstractActionController
 {
     /** @var ViewModelFactory */
     protected $viewModelFactory;
+    /** @var UsageService */
+    protected $usageService;
 
-    public function __construct(ViewModelFactory $viewModelFactory)
+    public function __construct(ViewModelFactory $viewModelFactory, UsageService $usageService)
     {
-        $this->setViewModelFactory($viewModelFactory);
+        $this->setViewModelFactory($viewModelFactory)
+            ->setUsageService($usageService);
     }
 
     public function indexAction()
     {
+        if ($this->usageService->hasUsageBeenExceeded()) {
+            throw new UsageExceeded();
+        }
+
         $view = $this->viewModelFactory->newInstance();
         // TODO: CGIV-7391
         return $view;
@@ -24,6 +33,12 @@ class ManualOrderController extends AbstractActionController
     protected function setViewModelFactory(ViewModelFactory $viewModelFactory)
     {
         $this->viewModelFactory = $viewModelFactory;
+        return $this;
+    }
+
+    protected function setUsageService(UsageService $usageService)
+    {
+        $this->usageService = $usageService;
         return $this;
     }
 }
