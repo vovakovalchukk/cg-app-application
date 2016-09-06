90c17b45e98a9ccb12e4f3934af76b0849717ab2
Aaron Baker <aaronb@channelgrabber.com>
2016-09-06 10:09:18 +0100

    CGIV-7400: manual orders - further work on the creation of an order - creating the Items. W.I.P.

--- module/Orders/src/Orders/ManualOrder/Service.php
+++ module/Orders/src/Orders/ManualOrder/Service.php
@@ -2,16 +2,25 @@
 namespace Orders\ManualOrder;
 
 use CG\Account\Shared\Entity as Account;
+use CG\Http\SaveCollectionHandleErrorsTrait;
 use CG\ManualOrder\Account\Service as ManualOrderAccountService;
 use CG\Order\Client\Alert\Service as OrderAlertService;
 use CG\Order\Client\Item\Service as OrderItemService;
 use CG\Order\Client\Note\Service as OrderNoteService;
 use CG\Order\Client\Service as OrderService;
+use CG\Order\Shared\Entity as Order;
+use CG\Order\Shared\Item\Collection as OrderItemCollection;
+use CG\Order\Shared\Item\Entity as OrderItem;
+use CG\Order\Shared\Item\Mapper as OrderItemMapper;
 use CG\Order\Shared\Mapper as OrderMapper;
 use CG\Order\Shared\Status as OrderStatus;
+use CG\Order\Shared\Tax\Service as TaxService;
 use CG\OrganisationUnit\Service as OrganisationUnitService;
 use CG\Product\Client\Service as ProductService;
+use CG\Product\Entity as Product;
+use CG\Product\Filter as ProductFilter;
 use CG\Stdlib\DateTime as StdlibDateTime;
+use CG\Stdlib\Exception\Runtime\NotFound;
 use CG\Stdlib\Log\LoggerAwareInterface;
 use CG\Stdlib\Log\LogTrait;
 use CG\User\ActiveUserInterface;
@@ -19,6 +28,7 @@ use CG\User\ActiveUserInterface;
 class Service implements LoggerAwareInterface
 {
     use LogTrait;
+    use SaveCollectionHandleErrorsTrait;
 
     /** @var ManualOrderAccountService */
     protected $manualOrderAccountService;
@@ -38,6 +48,10 @@ class Service implements LoggerAwareInterface
     protected $orderNoteService;
     /** @var OrderMapper */
     protected $orderMapper;
+    /** @var OrderItemMapper */
+    protected $orderItemMapper;
+    /** @var TaxService */
+    protected $taxService;
 
     public function __construct(
         ManualOrderAccountService $manualOrderAccountService,
@@ -48,7 +62,9 @@ class Service implements LoggerAwareInterface
         OrderItemService $orderItemService,
         OrderAlertService $orderAlertService,
         OrderNoteService $orderNoteService,
-        OrderMapper $orderMapper
+        OrderMapper $orderMapper,
+        OrderItemMapper $orderItemMapper,
+        TaxService $taxService
     ) {
         $this->setManualOrderAccountService($manualOrderAccountService)
             ->setActiveUserContainer($activeUserContainer)
@@ -58,7 +74,9 @@ class Service implements LoggerAwareInterface
             ->setOrderItemService($orderItemService)
             ->setOrderAlertService($orderAlertService)
             ->setOrderNoteService($orderNoteService)
-            ->setOrderMapper($orderMapper);
+            ->setOrderMapper($orderMapper)
+            ->setOrderItemMapper($orderItemMapper)
+            ->setTaxService($taxService);
     }
 
     public function createOrderFromPostData(array $orderData)
@@ -68,7 +86,8 @@ class Service implements LoggerAwareInterface
         $account = $this->manualOrderAccountService->getAccountForOrganisationUnit($organisationUnit);
 
         $order = $this->createOrder($orderData, $account);
-        // TODO: items, alert, notes
+        $this->createItems($orderData['Item'], $order);
+        // TODO: alert, notes
     }
     
     protected function getOrganisationUnitIdForOrderCreation(array $orderData)
@@ -109,6 +128,80 @@ class Service implements LoggerAwareInterface
             $shippingField = str_replace('billing', 'shipping', $field);
             $orderData[$shippingField] = $value;
         }
+    }
+
+    protected function createItems(array $itemsData, Order $order)
+    {
+        $count = 0;
+        $collection = new OrderItemCollection(OrderItem::class, 'fetchCollectionByOrderId', ['orderId' => $order->getId()]);
+        $products = $this->fetchProductsForItemsData($itemsData);
+        foreach ($itemsData as $itemData) {
+            $count++;
+            $itemProducts = $products->getById($itemData['productId']);
+            $itemProducts->rewind();
+            $item = $this->createItem($itemData, $order, $itemProducts->current(), $count);
+            $collection->attach($item);
+        }
+
+        $this->saveCollectionHandleErrors($this->orderItemService, $collection);
+        $order->setItems($collection);
+    }
+
+    protected function createItem(array $itemData, Order $order, Product $product, $index)
+    {
+        $itemData['id'] = $order->getId() . '-' . $index;
+        $itemData['orderId'] = $order->getId();
+        $itemData['accountId'] = $order->getAccountId();
+        $itemData['organisationUnitId'] = $order->getOrganisationUnitId();
+        $itemData['purchaseDate'] = $order->getPurchaseDate();
+        $itemData['status'] = $order->getStatus();
+        $itemData['stockManaged'] = true;
+        $itemData['itemVariationAttribute'] = $product->getAttributeValues();
+        if (!isset($itemData['itemName'])) {
+            $itemData['itemName'] = $product->getName();
+        }
+        if (!isset($itemData['itemSku'])) {
+            $itemData['itemSku'] = $product->getSku();
+        }
+
+        $item = $this->orderItemMapper->fromArray($itemData);
+
+        $taxPercentage = $this->calculateItemTaxPercentage($item, $order);
+        $item->setItemTaxPercentage($taxPercentage)
+            ->setCalculatedTaxPercentage($taxPercentage);
+
+        return $item;
+    }
+
+    protected function fetchProductsForItemsData(array $itemsData)
+    {
+        $productIds = array_column($itemsData, 'productId');
+        $filter = (new ProductFilter())
+            ->setLimit('all')
+            ->setPage(1)
+            ->setId($productIds);
+        return $this->productService->fetchCollectionByFilter($filter);
+    }
+
+    protected function calculateItemTaxPercentage(OrderItem $item, Order $order)
+    {
+        try {
+            $rate = $this->taxService->getRateForItemAndCountry(
+                $item,
+                $order->getCountryCode()
+            );
+            return $rate->getActive();
+
+        } catch (NotFound $e) {
+            return 0;
+        }
+    }
+
+    // This is required to satisfy SaveCollectionHandleErrorsTrait but we shouldn't get conflicts as we're creating a new order
+    protected function reapplyChangesToEntityAfterConflict($fetchedEntity, $passedEntity)
+    {
+        $passedEntity->setStoredETag($fetchedEntity->getStoredETag());
+        return $passedEntity;
     }
 
     protected function setManualOrderAccountService(ManualOrderAccountService $manualOrderAccountService)
@@ -162,6 +255,18 @@ class Service implements LoggerAwareInterface
     protected function setOrderMapper(OrderMapper $orderMapper)
     {
         $this->orderMapper = $orderMapper;
+        return $this;
+    }
+
+    protected function setOrderItemMapper(OrderItemMapper $orderItemMapper)
+    {
+        $this->orderItemMapper = $orderItemMapper;
+        return $this;
+    }
+
+    protected function setTaxService(TaxService $taxService)
+    {
+        $this->taxService = $taxService;
         return $this;
     }
 }
