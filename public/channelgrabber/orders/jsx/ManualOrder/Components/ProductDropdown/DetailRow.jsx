8dd28a9fe8d62187687d13436ffa86de5015adb9
Jack Monk <jack.monk@channelgrabber.com>
2016-09-06 12:41:29 +0100

    CGIV-7393 Added variation rows per product, added image basepath for when there is no image, and added some styling

--- public/channelgrabber/orders/jsx/ManualOrder/Components/ProductDropdown/DetailRow.jsx
+++ public/channelgrabber/orders/jsx/ManualOrder/Components/ProductDropdown/DetailRow.jsx
@@ -4,10 +4,37 @@ define([
     React
 ) {
     "use strict";
-    var Row = React.createClass({
+    var DetailRow = React.createClass({
+        getAttributeValues: function (attributes) {
+            var attributeValues = [];
+            for (var value in attributes) {
+                if (!attributes.hasOwnProperty(value)) continue;
+                attributeValues.push(<span className="variation-attribute">{value}</span>);
+            }
+            return attributeValues;
+        },
         getVariationRow: function (variation) {
             return (
-                <p>{variation.sku}</p>
+                <div className="detail-variation-row">
+                    <div className="variation-row-img">
+                        <img src={variation.images.length > 0 ? variation.images[0]['url'] : this.context.imageBasePath + '/noproductsimage.png'} />
+                    </div>
+                    <div className="variation-row-sku">
+                        {variation.sku}
+                    </div>
+                    <div className="variation-row-attributes">
+                        {this.getAttributeValues(variation.attributeValues)}
+                    </div>
+                    <div className="variation-row-stock">
+                        {variation.stockLevel} Available
+                    </div>
+                    <div className="variation-row-qty-input">
+                        <input value={variation.stockLevel} />
+                    </div>
+                    <div className="variation-row-actions">
+                        <span className="variation-add-action">Add</span>
+                    </div>
+                </div>
             );
         },
         getDefaultProps: function () {
@@ -33,5 +60,9 @@ define([
         }
     });
 
-    return Row;
+    DetailRow.contextTypes = {
+        imageBasePath: React.PropTypes.string
+    };
+
+    return DetailRow;
 });
