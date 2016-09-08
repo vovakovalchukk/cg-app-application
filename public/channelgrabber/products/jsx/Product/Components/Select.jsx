96d3dbfa90e910b8aee3e222895a0285a7fc60da
Jack Monk <jack.monk@channelgrabber.com>
2016-09-08 12:22:04 +0100

    CGIV-7254 Changed the select box to rely solely on the selected option property for decided what is selected

--- public/channelgrabber/products/jsx/Product/Components/Select.jsx
+++ public/channelgrabber/products/jsx/Product/Components/Select.jsx
@@ -13,8 +13,15 @@ define([
         },
         getInitialState: function () {
             return {
-                currentSelection: {},
+                selectedOption: this.props.selectedOption,
                 active: false
+            }
+        },
+        componentWillReceiveProps: function (newProps) {
+            if (newProps.selectedOption.name !== "") {
+                this.setState({
+                    selectedOption: newProps.selectedOption,
+                });
             }
         },
         onClick: function (e) {
@@ -24,21 +31,15 @@ define([
         },
         onOptionSelected: function (e) {
             var selectedOption = this.props.options.find(function (option) {
-                if (option.value === e.target.value) {
-                    return option;
-                }
+                return option.value === e.target.value;
             });
             this.setState({
-                currentSelection: selectedOption
+                selectedOption: selectedOption,
             });
             this.props.onNewOption(selectedOption);
         },
         render: function () {
-            var selectedValue = (this.state.currentSelection.name ? this.state.currentSelection.name : this.props.defaultValue);
             var options = this.props.options.map(function(opt) {
-                if (opt.selected && (selectedValue === null || selectedValue === undefined)) {
-                    selectedValue = opt.name;
-                }
                 return (
                     <li className={"custom-select-item "+(opt.selected ? "active" : "")} value={opt.value} key={opt.value} onClick={this.onOptionSelected}>
                         <a value={opt.value}>{opt.name}</a>
@@ -48,7 +49,7 @@ define([
             return (
                 <div className={"custom-select "+ (this.state.active ? 'active' : '')} onClick={this.onClick}>
                         <div className="selected">
-                            <span className="selected-content"><b>{this.props.prefix ? (this.props.prefix + ": ") : ""}</b>{selectedValue}</span>
+                            <span className="selected-content"><b>{this.props.prefix ? (this.props.prefix + ": ") : ""}</b>{this.state.selectedOption.name}</span>
                             <span className="sprite-arrow-down-10-black">&nbsp;</span>
                         </div>
                         <div className="animated fadeInDown open-content">
