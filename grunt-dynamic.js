55678442e1a29746c6f4a6ceb71a565826dad961
Jack Monk <jack.monk@channelgrabber.com>
2016-09-09 14:24:39 +0100

    CGIV-7466 Rearranged the dynamically generated grunt tasks to prevent wiping the compiled output before copying

--- grunt-dynamic.js
+++ grunt-dynamic.js
@@ -17,11 +17,11 @@ module.exports = function (grunt) {
     function generateShellSubTasks() {
         var shellTasks = grunt.config('shell');
         modulesNames.forEach(function(module) {
-            shellTasks["compile" + replaceAll(module, '-', '') + "Css"] = {
-                command: "compass compile --force public/channelgrabber/" + module
-            };
             shellTasks["cleanSrc" + replaceAll(module, '-', '') + "Css"] = {
                 command: "rm -rf public/channelgrabber/"+ module +"/css/*"
+            };
+            shellTasks["compile" + replaceAll(module, '-', '') + "Css"] = {
+                command: "compass compile --force public/channelgrabber/" + module
             };
             shellTasks["cleanDest" + replaceAll(module, '-', '') + "Css"] = {
                 command: "rm -rf public/cg-built/"+ module +"/css/*"
@@ -33,17 +33,18 @@ module.exports = function (grunt) {
                 command: "cp -r public/channelgrabber/"+ module +"/css/* public/cg-built/"+ module +"/css/ 2>/dev/null || :"
             };
         });
+        //console.log(shellTasks);
         grunt.config.set('shell', shellTasks);
     }
 
     function generateInstallCommand() {
         var tasks = [];
         modulesNames.forEach(function(module){
-             tasks.push("shell:compile" + replaceAll(module,'-',  '') + "Css");
-             tasks.push("shell:cleanSrc" + replaceAll(module, '-', '') + "Css");
-             tasks.push("shell:cleanDest" + replaceAll(module, '-', '') + "Css");
-             tasks.push("shell:makeDir" + replaceAll(module, '-', '') + "Css");
-             tasks.push("shell:copy" + replaceAll(module, '-', '') + "Css");
+            tasks.push("shell:cleanSrc" + replaceAll(module, '-', '') + "Css");
+            tasks.push("shell:compile" + replaceAll(module,'-',  '') + "Css");
+            tasks.push("shell:cleanDest" + replaceAll(module, '-', '') + "Css");
+            tasks.push("shell:makeDir" + replaceAll(module, '-', '') + "Css");
+            tasks.push("shell:copy" + replaceAll(module, '-', '') + "Css");
         });
         grunt.registerTask('compileCss-gen', tasks);
     }
