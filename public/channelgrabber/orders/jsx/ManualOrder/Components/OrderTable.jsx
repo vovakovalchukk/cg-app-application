7ca2eb0adb57c292b3b38e7b54802f14766c13d6
Jack Monk <jack.monk@channelgrabber.com>
2016-09-12 16:23:37 +0100

    CGIV-7394 Added the start of the Order Table with some styling

--- public/channelgrabber/orders/jsx/ManualOrder/Components/OrderTable.jsx
+++ public/channelgrabber/orders/jsx/ManualOrder/Components/OrderTable.jsx
@@ -1,7 +1,11 @@
 define([
-    'react'
+    'react',
+    'Product/Components/Input',
+    'Product/Components/Select'
 ], function(
-    React
+    React,
+    Input,
+    Select
 ) {
     "use strict";
     var OrderTable = React.createClass({
@@ -10,16 +14,59 @@ define([
                 orderRows: []
             }
         },
+        getVariationSwitcherDropdown: function (product) {
+            if (! product.variations) {
+                return;
+            }
+            var options = product.variations.map(function (variation) {
+                return <p>{variation.sku}</p>
+            });
+            return options;
+        },
+        onStockQuantityUpdated: function (sku, quantity) {
+            console.log(sku);
+            console.log(quantity);
+        },
+        getOrderRows: function () {
+            return (
+                this.props.orderRows.map(function (row) {
+                    return (
+                        <div className="order-row">
+                            <div className="order-row-img">
+                                <img src={row.product.images.length > 0 ? row.product.images[0]['url'] : this.context.imageBasePath + '/noproductsimage.png'} />
+                            </div>
+                            <div className="order-row-description">
+                                <div className="order-row-name">{row.product.name}</div>
+                                <div className="order-row-sku">{row.sku}</div>
+                            </div>
+                            <div className="order-row-attributes">
+                                {this.getVariationSwitcherDropdown(row.product)}
+                            </div>
+                            <div className="order-row-qty-input">
+                                <span className="multiplier">X</span>
+                                <Input name='quantity' initialValue={row.quantity} submitCallback={this.onStockQuantityUpdated.bind(this, row.product.sku)} />
+                            </div>
+                        </div>
+                    )
+                }.bind(this))
+            );
+        },
         render: function () {
             return (
                 <div className="order-table-wrapper">
-                    {this.props.orderRows.map(function (row) {
-                        return <p>{row.variation.sku + ": " + row.quantity}</p>
-                    })}
+                    <div className="order-rows-wrapper">
+                        {this.getOrderRows()}
+                    </div>
+                    <div className="order-footer-wrapper">
+                    </div>
                 </div>
             );
         }
     });
+
+    OrderTable.contextTypes = {
+        imageBasePath: React.PropTypes.string
+    };
 
     return OrderTable;
 });
