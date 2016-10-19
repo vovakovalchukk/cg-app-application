abdfd231c5aa882cb6c432da7bb48408bb6e6a5e
Jack Monk <jack.monk@channelgrabber.com>
2016-10-19 11:14:31 +0100

    CGIV-7391 Extracted more functions out of the constructor into prototype functions and tidied up the constructor

--- public/channelgrabber/orders/jsx/ManualOrder/ManualOrder.jsx
+++ public/channelgrabber/orders/jsx/ManualOrder/ManualOrder.jsx
@@ -17,6 +17,22 @@ define([
     {
         var self = this;
         this.popupContent = <p className="center-align">Create the order?</p>;
+        this.orderSubmitEvent = new CustomEvent('orderSubmit');
+        this.manualOrder = ReactDOM.render(<RootComponent utilities={utilities} onCreateOrder={this.collectFormData.bind(this)}/>, mountingNodes.productInfo);
+        this.note = ReactDOM.render(<NoteComponent author={currentUser}/>, mountingNodes.orderNotes);
+        this.popup = ReactDOM.render(<PopupComponent onNoButtonPressed={onPopupClickNo} onYesButtonPressed={onPopupClickYes}>{this.popupContent}</PopupComponent>, mountingNodes.popup);
+
+        var tradingCompanies = utilities.ou.getTradingCompanies();
+        if (tradingCompanies.length > 1) {
+            var selectedCompany = null;
+            tradingCompanies.forEach(function(company) {
+                if (company.selected) {
+                    selectedCompany = company;
+                }
+            });
+            this.companySelect = ReactDOM.render(<Select options={utilities.ou.getTradingCompanies()} selectedOption={selectedCompany}/>, mountingNodes.companySelect);
+        }
+
         function onPopupClickNo() {
             self.popup.setState({
                 active: false
@@ -30,68 +46,20 @@ define([
             n.notice("Creating order...");
         }
 
-        this.collectFormData = function() {
-            var rawOrderData = self.manualOrder.state.order;
-            var rawCurrencyData = self.manualOrder.state.selectedCurrency;
-            var rawNoteData = self.note.state;
-            var rawCompanySelectData = self.companySelect ? self.companySelect.state: null;
-
-            self.submitFormData({
-                "organisationUnitId": self.companySelect ? rawCompanySelectData.selectedOption.value : '',
-                "currencyCode": rawCurrencyData.name,
-                "shippingPrice": rawOrderData.shippingMethod.cost,
-                "shippingMethod": rawOrderData.shippingMethod.name,
-                "totalDiscount": (rawOrderData.discount.active ? rawOrderData.discount.value : 0),
-                "item": self.mapOrderItems(rawOrderData.orderRows),
-                "note": self.mapNotes(rawNoteData.notes),
-                "billingAddressCompanyName": $("input[name='billingAddressCompanyName']").val(),
-                "billingAddressFullName": $("input[name='billingAddressFullName']").val(),
-                "billingAddress1": $("input[name='billingAddress1']").val(),
-                "billingAddress2": $("input[name='billingAddress2']").val(),
-                "billingAddress3": $("input[name='billingAddress3']").val(),
-                "billingAddressCity": $("input[name='billingAddressCity']").val(),
-                "billingAddressCounty": $("input[name='billingAddressCounty']").val(),
-                "billingAddressCountry": $("input[name='billingAddressCountry']").val(),
-                "billingAddressPostcode": $("input[name='billingAddressPostcode']").val(),
-                "billingEmailAddress": $("input[name='billingEmailAddress']").val(),
-                "billingPhoneNumber": $("input[name='billingPhoneNumber']").val(),
-                "billingAddressCountryCode": "",
-                "shippingAddressSameAsBilling": $("input[name='shippingAddressUseBilling']").val(),
-                "shippingAddressCompanyName": $("input[name='shippingAddressCompanyName']").val(),
-                "shippingAddressFullName": $("input[name='shippingAddressFullName']").val(),
-                "shippingAddress1": $("input[name='shippingAddress1']").val(),
-                "shippingAddress2": $("input[name='shippingAddress2']").val(),
-                "shippingAddress3": $("input[name='shippingAddress3']").val(),
-                "shippingAddressCity": $("input[name='shippingAddressCity']").val(),
-                "shippingAddressCounty": $("input[name='shippingAddressCounty']").val(),
-                "shippingAddressCountry": $("input[name='shippingAddressCountry']").val(),
-                "shippingAddressPostcode": $("input[name='shippingAddressPostcode']").val(),
-                "shippingEmailAddress": $("input[name='shippingEmailAddress']").val(),
-                "shippingPhoneNumber": $("input[name='shippingPhoneNumber']").val(),
-                "shippingAddressCountryCode": "",
-                "buyerMessage":$('#buyer-message').val(),
-                "alert": $("input[name='orderAlertText']").val()
-            });
+        this.getOrderData = function () {
+            return this.manualOrder.state.order;
         };
 
-        this.submitFormData = function (formData) {
-            $.ajax({
-                url: '/orders/new/create',
-                type: 'POST',
-                data: formData,
-                dataType: 'json',
-                success: function (data) {
-                    if (data.success) {
-                        n.success('Order Created');
-                        window.location = data.url;
-                        return;
-                    }
-                    n.error(data.message);
-                }.bind(this),
-                error: function (error, textStatus, errorThrown) {
-                    n.ajaxError(error, textStatus, errorThrown);
-                }
-            });
+        this.getCurrencyData = function () {
+            return this.manualOrder.state.selectedCurrency
+        };
+
+        this.getNoteData = function () {
+            return this.note.state;
+        };
+
+        this.getCompanySelectData = function () {
+            return this.companySelect ? this.companySelect.state: null
         };
 
         this.listenForCreateOrderAction = function()
@@ -108,22 +76,70 @@ define([
             this.listenForCreateOrderAction();
         };
         init.call(this);
+    };
 
-        this.orderSubmitEvent = new CustomEvent('orderSubmit');
-        this.manualOrder = ReactDOM.render(<RootComponent utilities={utilities} onCreateOrder={this.collectFormData}/>, mountingNodes.productInfo);
-        this.note = ReactDOM.render(<NoteComponent author={currentUser}/>, mountingNodes.orderNotes);
-        this.popup = ReactDOM.render(<PopupComponent onNoButtonPressed={onPopupClickNo} onYesButtonPressed={onPopupClickYes}>{this.popupContent}</PopupComponent>, mountingNodes.popup);
-
-        var tradingCompanies = utilities.ou.getTradingCompanies();
-        if (tradingCompanies.length > 1) {
-            var selectedCompany = null;
-            tradingCompanies.forEach(function(company) {
-                if (company.selected) {
-                    selectedCompany = company;
+    ManualOrder.prototype.submitFormData = function (formData) {
+        $.ajax({
+            url: '/orders/new/create',
+            type: 'POST',
+            data: formData,
+            dataType: 'json',
+            success: function (data) {
+                if (data.success) {
+                    n.success('Order Created');
+                    window.location = data.url;
+                    return;
                 }
-            });
-            this.companySelect = ReactDOM.render(<Select options={utilities.ou.getTradingCompanies()} selectedOption={selectedCompany}/>, mountingNodes.companySelect);
-        }
+                n.error(data.message);
+            }.bind(this),
+            error: function (error, textStatus, errorThrown) {
+                n.ajaxError(error, textStatus, errorThrown);
+            }
+        });
+    };
+
+    ManualOrder.prototype.collectFormData = function() {
+        var rawOrderData = this.getOrderData();
+        var rawCurrencyData = this.getCurrencyData();
+        var rawNoteData = this.getNoteData();
+        var rawCompanySelectData = this.getCompanySelectData();
+
+        this.submitFormData({
+            "organisationUnitId": rawCompanySelectData ? rawCompanySelectData.selectedOption.value : '',
+            "currencyCode": rawCurrencyData.name,
+            "shippingPrice": rawOrderData.shippingMethod.cost,
+            "shippingMethod": rawOrderData.shippingMethod.name,
+            "totalDiscount": (rawOrderData.discount.active ? rawOrderData.discount.value : 0),
+            "item": this.mapOrderItems(rawOrderData.orderRows),
+            "note": this.mapNotes(rawNoteData.notes),
+            "billingAddressCompanyName": $("input[name='billingAddressCompanyName']").val(),
+            "billingAddressFullName": $("input[name='billingAddressFullName']").val(),
+            "billingAddress1": $("input[name='billingAddress1']").val(),
+            "billingAddress2": $("input[name='billingAddress2']").val(),
+            "billingAddress3": $("input[name='billingAddress3']").val(),
+            "billingAddressCity": $("input[name='billingAddressCity']").val(),
+            "billingAddressCounty": $("input[name='billingAddressCounty']").val(),
+            "billingAddressCountry": $("input[name='billingAddressCountry']").val(),
+            "billingAddressPostcode": $("input[name='billingAddressPostcode']").val(),
+            "billingEmailAddress": $("input[name='billingEmailAddress']").val(),
+            "billingPhoneNumber": $("input[name='billingPhoneNumber']").val(),
+            "billingAddressCountryCode": "",
+            "shippingAddressSameAsBilling": $("input[name='shippingAddressUseBilling']").val(),
+            "shippingAddressCompanyName": $("input[name='shippingAddressCompanyName']").val(),
+            "shippingAddressFullName": $("input[name='shippingAddressFullName']").val(),
+            "shippingAddress1": $("input[name='shippingAddress1']").val(),
+            "shippingAddress2": $("input[name='shippingAddress2']").val(),
+            "shippingAddress3": $("input[name='shippingAddress3']").val(),
+            "shippingAddressCity": $("input[name='shippingAddressCity']").val(),
+            "shippingAddressCounty": $("input[name='shippingAddressCounty']").val(),
+            "shippingAddressCountry": $("input[name='shippingAddressCountry']").val(),
+            "shippingAddressPostcode": $("input[name='shippingAddressPostcode']").val(),
+            "shippingEmailAddress": $("input[name='shippingEmailAddress']").val(),
+            "shippingPhoneNumber": $("input[name='shippingPhoneNumber']").val(),
+            "shippingAddressCountryCode": "",
+            "buyerMessage":$('#buyer-message').val(),
+            "alert": $("input[name='orderAlertText']").val()
+        });
     };
 
     ManualOrder.prototype.mapNotes = function (notes) {
