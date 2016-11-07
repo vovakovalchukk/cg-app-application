b135701f64145b7e57405da6f6062c677e997936
Aaron Baker <aaronb@channelgrabber.com>
2016-11-07 15:14:01 +0000

    CGIV-7608: corrected countries supported by Royal Mail Internation Standard w/Signature

--- config/autoload/global/netdespatch.php
+++ config/autoload/global/netdespatch.php
@@ -130,19 +130,19 @@ return [
                                 [
                                     'title' => 'Signature',
                                     'countries' => [
-                                        'AF', 'AX', 'AL', 'DZ', 'AD', 'AO', 'AI', 'AG', 'AR', 'AM', 'AW', 'AC', 'AU', 'AT',
-                                        'AZ', 'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BQ', 'BA',
-                                        'BW', 'BR', 'IO', 'BN', 'BF', 'BI', 'CM', 'CV', 'CF', 'EA', 'TD', 'CL', 'CN', 'CX',
-                                        'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'CU', 'CW', 'DJ', 'DM', 'DO', 'EG', 'SV', 'GQ',
-                                        'ER', 'EE', 'ET', 'FK', 'FO', 'FJ', 'GF', 'PF', 'TF', 'GA', 'GM', 'GE', 'GH', 'GL',
-                                        'GD', 'GP', 'GT', 'GN', 'GW', 'GY', 'HT', 'HN', 'IN', 'IR', 'IQ', 'IM', 'IT', 'JM',
-                                        'JO', 'KZ', 'KE', 'KI', 'KO', 'KW', 'KG', 'LA', 'LB', 'LS', 'LR', 'LY', 'MO', 'MK',
-                                        'MG', 'MW', 'MV', 'ML', 'MQ', 'MR', 'MU', 'MX', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM',
-                                        'NA', 'NR', 'NP', 'NC', 'NI', 'NE', 'NG', 'NU', 'KP', 'NO', 'OM', 'PK', 'PW', 'PA',
-                                        'PG', 'PY', 'PE', 'PH', 'PN', 'PR', 'QA', 'RE', 'RU', 'RW', 'ST', 'SA', 'SN', 'SC',
-                                        'SL', 'SB', 'ZA', 'SS', 'LK', 'XE', 'SH', 'SD', 'SR', 'SZ', 'SY', 'TW', 'TJ', 'TZ',
-                                        'TL', 'TG', 'TK', 'TO', 'TA', 'TN', 'TM', 'TC', 'TV', 'UG', 'UA', 'UY', 'UZ', 'VU',
-                                        'VE', 'VN', 'WF', 'EH', 'YE', 'ZM', 'ZW'
+                                        'AF', 'AX', 'AL', 'DZ', 'AO', 'AI', 'AG', 'AM', 'AW', 'AC', 'AU', 'AZ', 'BS', 'BH',
+                                        'BD', 'BB', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BQ', 'BA', 'BW', 'BR', 'IO', 'BN', 'BF',
+                                        'BI', 'CM', 'CV', 'CF', 'EA', 'TD', 'CL', 'CN', 'CX', 'CO', 'KM', 'CG', 'CD', 'CK',
+                                        'CR', 'CU', 'CW', 'DJ', 'DM', 'DO', 'EG', 'SV', 'GQ', 'ER', 'EE', 'ET', 'FK', 'FO',
+                                        'FJ', 'GF', 'PF', 'TF', 'GA', 'GM', 'GE', 'GH', 'GL', 'GD', 'GP', 'GT', 'GN', 'GW',
+                                        'GY', 'HT', 'HN', 'IN', 'IR', 'IQ', 'IM', 'IT', 'JM', 'JO', 'KZ', 'KE', 'KI', 'KO',
+                                        'KW', 'KG', 'LA', 'LB', 'LS', 'LR', 'LY', 'MO', 'MK', 'MG', 'MW', 'MV', 'ML', 'MQ',
+                                        'MR', 'MU', 'MX', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP', 'NC', 'NI',
+                                        'NE', 'NG', 'NU', 'KP', 'NO', 'OM', 'PK', 'PW', 'PA', 'PG', 'PY', 'PE', 'PH', 'PN',
+                                        'PR', 'QA', 'RE', 'RU', 'RW', 'ST', 'SA', 'SN', 'SC', 'SL', 'SB', 'ZA', 'SS', 'LK',
+                                        'XE', 'SH', 'SD', 'SR', 'SZ', 'SY', 'TW', 'TJ', 'TZ', 'TL', 'TG', 'TK', 'TO', 'TA',
+                                        'TN', 'TM', 'TC', 'TV', 'UG', 'UA', 'UY', 'UZ', 'VU', 'VE', 'VN', 'WF', 'EH', 'YE',
+                                        'ZM', 'ZW'
                                     ]
                                 ],
                                 // Standard only allows for Compensation when its with Signature
