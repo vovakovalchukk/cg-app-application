11dc9104c1178c06fac722be35613c59ea79389d
Jack Monk <jack.monk@channelgrabber.com>
2016-12-05 09:58:29 +0000

    CGIV-7700 Implemented the order aware interface so the bulk action can be used on the order detail page

--- module/Orders/src/Orders/Order/BulkActions/Action/Courier.php
+++ module/Orders/src/Orders/Order/BulkActions/Action/Courier.php
@@ -4,14 +4,20 @@ namespace Orders\Order\BulkActions\Action;
 use CG_UI\View\BulkActions\Action;
 use Orders\Module;
 use Orders\Controller\CourierController;
+use CG\Channel\Action\Order\Service as ActionDecider;
+use CG\Channel\Action\Order\MapInterface as ActionDeciderMap;
+use Orders\Order\BulkActions\OrderAwareInterface;
+use CG\Order\Shared\Entity as Order;
 use Zend\View\Model\ViewModel;
 use SplObjectStorage;
 
-class Courier extends Action
+class Courier extends Action implements OrderAwareInterface
 {
     protected $urlView;
+    protected $actionDecider;
 
     public function __construct(
+        ActionDecider $actionDecider,
         ViewModel $urlView,
         array $elementData = [],
         ViewModel $javascript = null,
@@ -19,6 +25,7 @@ class Courier extends Action
     ) {
         parent::__construct('sprite-courier-22-black', 'Courier', 'courier', $elementData, $javascript, $subActions);
         $this->setUrlView($urlView)->configure();
+        $this->setActionDecider($actionDecider);
     }
 
     public function setUrlView(ViewModel $urlView)
@@ -41,9 +48,21 @@ class Courier extends Action
         return $this->urlView;
     }
 
+    public function setActionDecider($actionDecider)
+    {
+        $this->actionDecider = $actionDecider;
+        return $this;
+    }
+
     protected function configure()
     {
         $this->addElementView($this->getUrlView());
         return $this;
+    }
+
+    public function setOrder(Order $order)
+    {
+        $actions = array_fill_keys($this->actionDecider->getAvailableActionsForOrder($order), true);
+        $this->setEnabled(isset($actions[ActionDeciderMap::DISPATCH]));
     }
 } 
