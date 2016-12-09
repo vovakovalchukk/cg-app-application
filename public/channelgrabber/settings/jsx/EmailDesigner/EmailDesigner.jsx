a15841f96cb22ff3ba9277d7aae52e0416c84d0d
Jack Monk <jack.monk@channelgrabber.com>
2016-12-09 09:57:10 +0000

    CGIV-7768 Removed the document ready as we want to load our JS straight away regardless of DOM and also added the EmailDesigner main JS file

---
+++ public/channelgrabber/settings/jsx/EmailDesigner/EmailDesigner.jsx
@@ -0,0 +1,14 @@
+define([
+    'react',
+    'react-dom',
+    'EmailDesigner/Components/Root'
+], function(
+    React,
+    ReactDOM,
+    RootComponent
+) {
+    var EmailDesigner = function(mountingNodes) {
+        ReactDOM.render(<RootComponent />, mountingNodes.emailDesignerRoot);
+    };
+    return EmailDesigner;
+});
