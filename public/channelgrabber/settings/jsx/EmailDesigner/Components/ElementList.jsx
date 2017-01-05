f0ee879118b75f7e991631f8921746cec044fb1a
Jack Monk <jack.monk@channelgrabber.com>
2017-01-05 09:57:09 +0000

    CGIV-7768 Added default data generator in the elements list class

--- public/channelgrabber/settings/jsx/EmailDesigner/Components/ElementList.jsx
+++ public/channelgrabber/settings/jsx/EmailDesigner/Components/ElementList.jsx
@@ -12,11 +12,34 @@ define([
             'Text': function (elementData) {
                 return (<Text id={elementData.id} text={elementData.text} initialPosition={{x: elementData.x, y: elementData.y}}/>);
             }
+        };
+
+        this.defaults = {
+            'Text': function () {
+                return {
+                    type: 'text',
+                    text: "Enter text...",
+                    id: String(IdGenerator.generate()),
+                    width: 200,
+                    height: 50,
+                    style: {
+                        left: 0,
+                        top: 0,
+                        fontColour: "black",
+                        fontFamily: "Arial",
+                        fontSize: 12,
+                    }
+                };
+            }
         }
     };
 
     ElementList.prototype.createElement = function (elementData) {
         return this.elements[elementData.type](elementData);
+    };
+
+    ElementList.prototype.getDefaultData = function (elementType) {
+        return this.defaults[elementType]();
     };
 
     return new ElementList;
