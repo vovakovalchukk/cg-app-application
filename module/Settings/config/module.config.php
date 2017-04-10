6c3db6702e6b9ac562d694103427158211579d86
Jack Monk <jack.monk@channelgrabber.com>
2017-04-10 16:08:15 +0100

    CGIV-7903 Removed the assignedInvoice column from the tradingCompany table

--- module/Settings/config/module.config.php
+++ module/Settings/config/module.config.php
@@ -910,7 +910,6 @@ return [
                     ],
                     'addColumn' => [
                         ['column' => 'InvoiceTradingCompanyColumn'],
-                        ['column' => 'InvoiceAssignedInvoiceColumn'],
                         ['column' => 'InvoiceSendFromAddressColumn'],
                     ],
                     'setVariable' => [
--- module/Settings/src/Settings/Invoice/Mapper.php
+++ module/Settings/src/Settings/Invoice/Mapper.php
@@ -19,7 +19,6 @@ class Mapper
         $data = [
             'organisationUnit' => $tradingCompany->getAddressCompanyName(),
             'organisationUnitId' => $tradingCompany->getId(),
-            'assignedInvoice' => [],
             'class' => 'invoiceTradingCompaniesCustomSelect',
             'name' => 'invoiceTradingCompaniesCustomSelect_' . $tradingCompany->getId(),
             'id' => 'invoiceTradingCompaniesCustomSelect_' . $tradingCompany->getId(),
@@ -37,16 +36,6 @@ class Mapper
 
         $emailVerifiedStatus = isset($tradingCompanySettings['emailVerificationStatus']) ? $tradingCompanySettings['emailVerificationStatus'] : null;
         $data['sendFromAddress']['emailVerificationStatus'] = $this->getEmailVerificationStatusMessage($emailVerifiedStatus);
-
-        $selected = isset($tradingCompanySettings['assignedInvoice']) ? $tradingCompanySettings['assignedInvoice'] : false;
-
-        foreach ($invoices as $invoice) {
-            $data['options'][] = [
-                'title' => $invoice->getName(),
-                'value' => $invoice->getId(),
-                'selected' => $invoice->getId() == $selected
-            ];
-        }
         return $data;
     }
 
