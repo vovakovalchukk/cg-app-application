0683a93a69dd63530315c919f664244eac7767f0
Jack Monk <jack.monk@channelgrabber.com>
2017-04-19 11:45:11 +0100

    CGIV-8253 Replaced the hover text with a list of possible texts based on the status of the listing

--- public/channelgrabber/products/jsx/Product/Components/ListingsRow.jsx
+++ public/channelgrabber/products/jsx/Product/Components/ListingsRow.jsx
@@ -12,6 +12,17 @@ define([
                 listings: {}
             };
         },
+        getHoverText: function (status) {
+            var hoverText = {
+                'active': 'This is an active listing with available stock',
+                'pending': 'We have recently sent a stock update to this listing, and are currently waiting for X (X= ebay, amazon, whatever it is) to confirm they have received and processed that stock update',
+                'paused': 'Listing is paused due to no stock being available for sale',
+                'error': 'We received an error when sending a stock update for this listing and so we are not currently able to manage the stock for this listing. Click here for the full error message, please contact support if you need assistance.',
+                'inactive': 'You do not currently have this product listed in this location',
+                'unimported': 'This listing has not yet been imported or is not available'
+            };
+            return hoverText[status];
+        },
         getValues: function() {
             var values = [];
 
@@ -20,9 +31,9 @@ define([
 
                 if (this.props.listings[accountId]) {
                     var status = $.trim(this.props.listings[accountId].status);
-                    values.push(<td title={status}><span className={"status " + status}>{status}</span></td>);
+                    values.push(<td title={this.getHoverText(status)}><span className={"status " + status}>{status}</span></td>);
                 } else {
-                    values.push(<td title="Unimported"><span className={"status not_started"}>Unimported</span></td>);
+                    values.push(<td title={this.getHoverText('unimported')}><span className={"status not_started"}>Unimported</span></td>);
                 }
             }
 
