ea3ae15afbd9d41cf2af05ac2f2334e9d073d256
Jack Monk <jack.monk@channelgrabber.com>
2017-05-11 13:00:45 +0100

    CGIV-8312 Changed the product list so we can have the body scrollable, set a maximum height so we do not go past the fold

--- public/channelgrabber/products/jsx/PurchaseOrders/Components/List.jsx
+++ public/channelgrabber/products/jsx/PurchaseOrders/Components/List.jsx
@@ -1,7 +1,9 @@
 define([
-    'react'
+    'react',
+    'Common/Components/Select'
 ], function(
-    React
+    React,
+    Select
 ) {
     "use strict";
 
@@ -18,19 +20,21 @@ define([
         {
             return (
                 <div className="purchase-orders-list">
-                    <div className="grid-table">
-                        <div className="grid-table-header-row">
-                            <div className="grid-table-col">Status</div>
-                            <div className="grid-table-col">Date</div>
-                            <div className="grid-table-col">Number</div>
+                    <div className="head">
+                        <div className="row">
+                            <div className="cell">Status</div>
+                            <div className="cell">Date</div>
+                            <div className="cell">Number</div>
                         </div>
+                    </div>
+                    <div className="body">
                         {this.props.purchaseOrders.filter(this.filterPurchaseOrders).map(function (purchaseOrder) {
                             var statusClass = purchaseOrder.status.replace(" ", "_").toLowerCase();
                             return (
-                                <div className="grid-table-row hoverable">
-                                    <div className="grid-table-col"><span className={"status " + statusClass}>{purchaseOrder.status}</span></div>
-                                    <div className="grid-table-col">{purchaseOrder.date}</div>
-                                    <div className="grid-table-col">{purchaseOrder.number}</div>
+                                <div className="row hoverable">
+                                    <div className="cell"><span className={"status " + statusClass}>{purchaseOrder.status}</span></div>
+                                    <div className="cell">{purchaseOrder.date}</div>
+                                    <div className="cell">{purchaseOrder.number}</div>
                                 </div>
                             );
 
