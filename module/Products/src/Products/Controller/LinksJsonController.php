4fbd008d4ac7ebc9adfc822ef3919705c029acd0
Jack Monk <jack.monk@channelgrabber.com>
2017-05-17 14:58:26 +0100

    CGIV-8270 Extract the variations, find any matching productLinks then map them back for the view

--- module/Products/src/Products/Controller/LinksJsonController.php
+++ module/Products/src/Products/Controller/LinksJsonController.php
@@ -6,33 +6,64 @@ use CG\Stdlib\Exception\Runtime\NotFound;
 use Zend\Mvc\Controller\AbstractActionController;
 use CG_UI\View\Prototyper\JsonModelFactory;
 
+use CG\Product\Link\Filter as ProductLinkFilter;
+use CG\Product\Link\Service as ProductLinkService;
+use CG\Product\Link\Mapper as ProductLinkMapper;
+
 class LinksJsonController extends AbstractActionController
 {
     const ROUTE_AJAX = 'Links AJAX';
 
     protected $jsonModelFactory;
+    protected $productLinkService;
 
     public function __construct(
-        JsonModelFactory $jsonModelFactory
+        JsonModelFactory $jsonModelFactory,
+        ProductLinkService $productLinkService
     ) {
         $this->jsonModelFactory = $jsonModelFactory;
+        $this->productLinkService = $productLinkService;
     }
 
     public function ajaxAction()
     {
-        try {
-            $sku = $this->params()->fromPost('sku');
+        $products = json_decode($this->params()->fromPost('products'), true);
 
-            /**
-             * retrieve product links
-             */
-            $linkedProducts = [];
+        $allVariationsBySkus = [];
+        foreach ($products as $parentProductId => $variations) {
+            foreach ($variations as $variation) {
+                $allVariationsBySkus[$variation['sku']] = $variation;
+            }
+        }
+
+        try {
+            $filter = (new ProductLinkFilter('all', 1))
+                ->setProductSku(array_keys($allVariationsBySkus));
+            $linkedProducts = $this->productLinkService->fetchCollectionByFilter($filter);
         } catch(NotFound $e) {
-            //noop
+            $linkedProducts = [];
+        }
+
+        $linkedProductsByProductId = [];
+        foreach ($allVariationsBySkus as $sku => $variation) {
+            $linkedProduct = $linkedProducts->getById($variation['organisationUnitId'].'-'.$sku);
+            if ($linkedProduct) {
+                foreach ($linkedProduct->getStockSkuMap() as $stockSku => $stockQty) {
+                    $imageUrl = "";
+                    if (isset($allVariationsBySkus[$stockSku]) && isset($allVariationsBySkus[$stockSku]['images'][0])) {
+                        $imageUrl = $allVariationsBySkus[$stockSku]['images'][0]['url'];
+                    }
+                    $linkedProductsByProductId[$variation['parentProductId']][$variation['id']][] = [
+                        'sku' => $stockSku,
+                        'quantity' => $stockQty,
+                        'imageUrl' => $imageUrl,
+                    ];
+                }
+            }
         }
 
         return $this->jsonModelFactory->newInstance([
-            'linkedProducts' => $linkedProducts
+            'linkedProducts' => $linkedProductsByProductId
         ]);
     }
 }
