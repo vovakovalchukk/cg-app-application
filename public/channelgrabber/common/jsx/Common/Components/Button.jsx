36c6ba2de9b00ed5a71afa01d4cb3940e802703d
Jack Monk <jack.monk@channelgrabber.com>
2017-05-23 15:29:57 +0100

    CGIV-8312 Extended the button component to be able to be disabled and unclickable when needed

--- public/channelgrabber/common/jsx/Common/Components/Button.jsx
+++ public/channelgrabber/common/jsx/Common/Components/Button.jsx
@@ -6,9 +6,15 @@ define([
     "use strict";
 
     var ButtonComponent = React.createClass({
+        onClick: function (e) {
+            if (this.props.disabled) {
+                return;
+            }
+            this.props.onClick(e);
+        },
         render: function () {
             return (
-                <div className="button" onClick={this.props.onClick}>
+                <div className={"button" + (this.props.disabled ? " disabled" : "")} onClick={this.onClick}>
                     {this.props.sprite ? <span className={"sprite-sprite "+this.props.sprite}>&nbsp;</span> : ''}
                     <span className="button-text">{this.props.text}</span>
                 </div>
