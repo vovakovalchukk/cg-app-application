0965f3a727e15e95eb8308bc657d287b2a4baa09
Aaron Goulden <aaron.goulden@channelgrabber.com>
2017-05-23 09:13:17 +0100

    CGIV-8414: Added additional fields to stock import table

---
+++ phinx/migrations/20170523080449_stock_import_additional_fields.php
@@ -0,0 +1,27 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class StockImportAdditionalFields extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+     */
+    public function change()
+    {
+
+        $table = $this->table('stockImportFiles');
+
+        $table
+            ->addColumn('organisationUnitId', 'string')
+            ->addColumn('userId', 'string')
+            ->update();
+    }
+
+}
