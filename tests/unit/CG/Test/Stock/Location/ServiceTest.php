9e93fb4e71f1176572bfbdb908826853f2f1c68b
Eric Mugerwa <eric.mugerwa@channelgrabber.com>
2017-07-04 15:17:04 +0100

    CGIV-8585 - Tests passing for Stock\Location\Service::excludeFromCollectionByLocationIds

--- tests/unit/CG/Test/Stock/Location/ServiceTest.php
+++ tests/unit/CG/Test/Stock/Location/ServiceTest.php
@@ -9,6 +9,9 @@ class StockLocationTest extends PHPUnit_Framework_TestCase
     protected $stockLocationService;
     protected static $di;
 
+    /** @var array Must exist in testing environment */
+    const EXCLUDE_LOCATION_IDS = [2];
+
     public static function setUpBeforeClass()
     {
         require_once TEST_PROJECT_ROOT . DIRECTORY_SEPARATOR . 'tests/unit/unit-bootstrap.php';
@@ -32,8 +35,23 @@ class StockLocationTest extends PHPUnit_Framework_TestCase
         $this->assertInstanceOf(StockLocationService::class, $this->stockLocationService);
     }
 
-    public function testProcess()
+    public function testRemovesStockLocationsByLocationId()
     {
+        $stockLocationCollection = $this->exampleStockLocationCollection();
+        $stockLocationCollection->rewind();
+        echo sprintf('There are %d stockLocations in the collection' . "\n", $stockLocationCollection->count());
+        echo sprintf('The stockLocation IDs in the collection are: %s' . "\n", json_encode($stockLocationCollection->getIds(), true));
+        echo sprintf('Attempting to remove stockLocations with Location IDs matching: %s' . "\n", json_encode(static::EXCLUDE_LOCATION_IDS, true));
+
         $stockLocationService = clone $this->stockLocationService;
+        $newStockLocationCollection = $stockLocationService->excludeFromCollectionByLocationIds($stockLocationCollection, static::EXCLUDE_LOCATION_IDS);
+
+        echo sprintf('There is now only %d stockLocations in the collection' . "\n", $newStockLocationCollection->count());
+        echo sprintf('The new stockLocation IDs in the collection are: %s' . "\n", json_encode($newStockLocationCollection->getIds(), true));
+    }
+
+    public function exampleStockLocationCollection()
+    {
+        return $this->stockLocationService->fetchCollectionByPaginationAndFilters('all', 1, [], []);
     }
 }
