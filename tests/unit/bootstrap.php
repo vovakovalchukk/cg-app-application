86fcc5379fdc35bf8009164fbe75edb5f59e36ce
Eric Mugerwa <eric.mugerwa@channelgrabber.com>
2017-07-04 16:38:44 +0100

    CGIV-8585 - Revert fixing unit tests - until can fix properly in bitbucket pipelines

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
-define('TEST_PROJECT_ROOT', dirname(dirname(__DIR__)));
+define('PROJECT_ROOT', dirname(dirname(__DIR__)));
 define('TEST_ROOT', __DIR__);
-require_once TEST_PROJECT_ROOT.'/vendor/autoload.php';
+
+require_once PROJECT_ROOT.'/vendor/autoload.php';
