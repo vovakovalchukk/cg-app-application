8edf223d79c18f3fd1a0b10e9312d0bb083c8c83
Eric Mugerwa <eric.mugerwa@channelgrabber.com>
2017-07-18 12:58:04 +0100

    CGIV-8668 - Remove status column on up and add on down as change does not handle dropping of columns

--- phinx/migrations/20170531152508_update_stock_import_fields.php
+++ phinx/migrations/20170531152508_update_stock_import_fields.php
@@ -4,22 +4,21 @@ use Phinx\Migration\AbstractMigration;
 
 class UpdateStockImportFields extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * More information on this method is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
-     *
-     * Uncomment this method if you would like to use it.
-     */
-    public function change()
-    {
-        $table = $this->table('stockImportFiles');
+    const TABLE = 'stockImportFiles';
 
-        $table
-            ->removeColumn('status')
+    public function up()
+    {
+        $this->table(static::TABLE)
             ->renameColumn('type', 'updateOption')
+            ->removeColumn('status')
             ->update();
     }
 
+    public function down()
+    {
+        $this->table(static::TABLE)
+            ->renameColumn('updateOption', 'type')
+            ->addColumn('status', 'string')
+            ->update();
+    }
 }
