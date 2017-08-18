433e410151bdc56346f1f10211d0cf3f88d27e9f
Eric Mugerwa <eric.mugerwa@channelgrabber.com>
2017-08-18 16:24:26 +0100

    CGIV-8035 - Throw exception if no sendFrom

--- module/Orders/src/Orders/Controller/BulkActionsController.php
+++ module/Orders/src/Orders/Controller/BulkActionsController.php
@@ -431,8 +431,8 @@ class BulkActionsController extends AbstractActionController implements LoggerAw
 
         if (!$sendFrom) {
             $this->logDebug(static::LOG_MSG_EMAIL_INVOICES_NO_VERIFIED_EMAIL_ADDRESS_SKIP, ["ou" => $ou, "rootOu" => $rootOuId], [static::LOG_CODE, static::LOG_CODE_EMAIL_INVOICES]);
+            throw new \Exception('Please <a href="' . $this->url()->fromRoute(SettingsModule::ROUTE) . '">add a verified email address</a> to send emails from ChannelGrabber');
         }
-        throw new \Exception('Please <a href="' . $this->url()->fromRoute(SettingsModule::ROUTE) . '">add a verified email address</a> to send emails from ChannelGrabber');
 
         $invoiceService = $this->getInvoiceService();
         if ($this->params()->fromPost('validate', false)) {
