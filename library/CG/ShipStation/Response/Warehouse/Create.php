c8590ac73c01815d618a18aaa25de62428b49dee
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2017-12-15 15:25:56 +0000

    CGIV-9089: various fixes for Account saving

--- library/CG/ShipStation/Response/Warehouse/Create.php
+++ library/CG/ShipStation/Response/Warehouse/Create.php
@@ -4,6 +4,7 @@ namespace CG\ShipStation\Response\Warehouse;
 use CG\ShipStation\Messages\Address;
 use CG\ShipStation\Messages\Timestamp;
 use CG\ShipStation\ResponseAbstract;
+use CG\Stdlib\DateTime;
 
 class Create extends ResponseAbstract
 {
@@ -28,12 +29,12 @@ class Create extends ResponseAbstract
             $decodedJson->origin_address->phone,
             $decodedJson->origin_address->address_line1,
             $decodedJson->origin_address->city_locality,
-            $decodedJson->origin_address->province,
+            $decodedJson->origin_address->state_province,
             $decodedJson->origin_address->postal_code,
             $decodedJson->origin_address->country_code,
             $decodedJson->origin_address->address_line2
         );
-        $timestamp = new Timestamp($decodedJson->create_date);
+        $timestamp = new Timestamp(new DateTime($decodedJson->created_at));
         return new static($address, $timestamp, $decodedJson->warehouse_id);
     }
 
