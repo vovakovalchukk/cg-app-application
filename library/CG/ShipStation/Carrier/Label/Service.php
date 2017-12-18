2282a6dedf0e46cda4d8643564bba2e6786cbe92
Richard Heelin <richardh@channelgrabber.com>
2017-12-18 11:33:24 +0000

    CGIV-9091: Allow for cancelling shipstation labels

--- library/CG/ShipStation/Carrier/Label/Service.php
+++ library/CG/ShipStation/Carrier/Label/Service.php
@@ -3,14 +3,16 @@ namespace CG\ShipStation\Carrier\Label;
 
 use CG\Account\Client\Service as AccountService;
 use CG\Account\Shared\Entity as Account;
+use CG\Channel\Shipping\Provider\Service\CancelInterface as ShippingProviderCancelInterface;
 use CG\Channel\Shipping\Provider\ServiceInterface as ShippingProviderServiceInterface;
 use CG\Order\Shared\Collection as OrderCollection;
 use CG\Order\Shared\Label\Collection as OrderLabelCollection;
+use CG\Order\Shared\ShippableInterface as Order;
 use CG\OrganisationUnit\Entity as OrganisationUnit;
 use CG\ShipStation\Carrier\Service as CarrierService;
 use CG\User\Entity as User;
 
-class Service implements ShippingProviderServiceInterface
+class Service implements ShippingProviderServiceInterface, ShippingProviderCancelInterface
 {
     /** @var CarrierService */
     protected $carrierServive;
@@ -18,15 +20,19 @@ class Service implements ShippingProviderServiceInterface
     protected $accountService;
     /** @var Creator */
     protected $labelCreator;
+    /** @var Canceller */
+    protected $labelCanceller;
 
     public function __construct(
         CarrierService $carrierServive,
         AccountService $accountService,
-        Creator $labelCreator
+        Creator $labelCreator,
+        Canceller $labelCanceller
     ) {
         $this->carrierServive = $carrierServive;
         $this->accountService = $accountService;
         $this->labelCreator = $labelCreator;
+        $this->labelCanceller = $labelCanceller;
     }
 
     /**
@@ -82,6 +88,21 @@ class Service implements ShippingProviderServiceInterface
             $user,
             $shipStationAccount
         );
+    }
+
+    public function isCancellationAllowedForOrder(Account $account, Order $order)
+    {
+        // Until you try to cancel a label we don't know if you can or not
+        return true;
+    }
+
+    public function cancelOrderLabels(
+        OrderLabelCollection $orderLabels,
+        OrderCollection $orders,
+        Account $shippingAccount
+    ) {
+        $shipStationAccount = $this->getShipStationAccountForShippingAccount($shippingAccount);
+        $this->labelCanceller->cancelOrderLabels($orderLabels, $orders, $shippingAccount, $shipStationAccount);
     }
 
     protected function getShipStationAccountForShippingAccount(Account $shippingAccount): Account
