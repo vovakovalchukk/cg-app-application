e8140921eb2d262194d4d216ab5bc86162a4de71
Aaron Baker <aaron.baker@channelgrabber.com>
2018-01-04 11:23:36 +0000

    CGIV-9014: the Product CSV Importer now creates a gearman job to do the importing

--- module/Products/src/Products/Product/Csv/Importer.php
+++ module/Products/src/Products/Product/Csv/Importer.php
@@ -1,7 +1,9 @@
 <?php
 namespace Products\Product\Csv;
 
+use CG\Product\Gearman\Workload\ProductImport as Workload;
 use CG\User\ActiveUserInterface;
+use GearmanClient;
 use Products\Product\Csv\Entity;
 use Products\Product\Csv\Mapper;
 use Products\Product\Csv\StorageInterface;
@@ -14,27 +16,37 @@ class Importer
     protected $storage;
     /** @var Mapper */
     protected $mapper;
+    /** @var GearmanClient */
+    protected $gearmanClient;
 
     public function __construct(
         ActiveUserInterface $activeUserContainer,
         StorageInterface $storage,
-        Mapper $mapper
+        Mapper $mapper,
+        GearmanClient $gearmanClient
     ) {
         $this->activeUserContainer = $activeUserContainer;
         $this->storage = $storage;
         $this->mapper = $mapper;
+        $this->gearmanClient = $gearmanClient;
     }
 
     public function importFromCsv(string $fileContents, int $accountId)
     {
         $rootOuId = $this->activeUserContainer->getActiveUserRootOrganisationUnitId();
         $this->saveCsv($fileContents, $accountId, $rootOuId);
-        // TODO: actually do the import by creating a gearman job
+        $this->createImportJob($accountId, $rootOuId);
     }
 
     protected function saveCsv(string $fileContents, int $accountId, int $rootOuId): Entity
     {
         $entity = $this->mapper->fromFileAndMetadata($fileContents, ['accountId' => $accountId, 'rootOuId' => $rootOuId]);
         return $this->storage->save($entity);
+    }
+
+    protected function createImportJob(int $accountId, int $rootOuId)
+    {
+        $workload = new Workload($accountId, $rootOuId);
+        $this->gearmanClient->doBackground(Workload::FUNCTION_NAME, serialize($workload));
     }
 }
