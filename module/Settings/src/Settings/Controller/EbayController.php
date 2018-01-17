a310a2d648bfcc47cb8e2ab3cfce81a36ac50cab
Aaron Baker <aaron.baker@channelgrabber.com>
2018-01-17 12:10:48 +0000

    LIS-22: added the remaining fields to the new section on the account details page for editing Listing Default Settings. W.I.P.

--- module/Settings/src/Settings/Controller/EbayController.php
+++ module/Settings/src/Settings/Controller/EbayController.php
@@ -3,12 +3,20 @@ namespace Settings\Controller;
 
 use CG\Account\Shared\Entity as Account;
 use CG\Channel\Type as ChannelType;
+use CG\Ebay\CodeType\ListingDuration;
+use CG\Ebay\CodeType\PaymentMethod;
 use CG\Listing\Client\Service as ListingService;
+use CG\Locale\CurrencyCode;
 use Settings\Module;
 use Zend\View\Model\ViewModel;
 
 class EbayController extends ChannelControllerAbstract implements AddChannelSpecificVariablesToViewInterface
 {
+    const DEFAULT_CURRENCY = 'GBP';
+    const DEFAULT_DURATION = ListingDuration::GTC;
+    const DEFAULT_DISPATCH_DAYS = 3;
+    const DEFAULT_PAYMENT_METHODS = [PaymentMethod::PAY_PAL];
+
     public function saveAction()
     {
         $accountEntity = $this->getAccountCreationService()->connectAccount(
@@ -28,6 +36,80 @@ class EbayController extends ChannelControllerAbstract implements AddChannelSpec
             ListingService::FEATURE_FLAG_CREATE_LISTINGS,
             $this->getActiveUserRootOrganisationUnit()
         );
-        $view->setVariable('shouldShowListingDefaults', $shouldShowListingDefaults);
+        $view->setVariable('shouldShowListingDefaults', $shouldShowListingDefaults)
+            ->setVariable('defaultListingDispatchDays', static::DEFAULT_DISPATCH_DAYS)
+            ->addChild($this->getCurrencySelectView($account->getExternalDataByKey('listingCurrency')), 'currencySelect')
+            ->addChild($this->getListingDurationView($account->getExternalDataByKey('listingDuration')), 'listingDurationSelect')
+            ->addChild($this->getPaymentMethodView($account->getExternalDataByKey('listingPaymentMethods')), 'paymentMethodsSelect');
+    }
+
+    protected function getCurrencySelectView(?string $selected = null): ViewModel
+    {
+        $selected = $selected ?? static::DEFAULT_CURRENCY;
+        $options = [];
+        foreach (CurrencyCode::getCurrencyCodes() as $currencyCode) {
+            $options[] = ['title' => $currencyCode, 'value' => $currencyCode, 'selected' => $currencyCode == $selected];
+        }
+        $selectView = $this->getSelectView(
+            'ebayAccountListingDefaultCurrency',
+            'listingCurrency',
+            $options,
+            $selected
+        );
+        $selectView->setVariable('searchField', true);
+        return $selectView;
+    }
+
+    protected function getListingDurationView(?string $selected = null): ViewModel
+    {
+        $selected = $selected ?? static::DEFAULT_DURATION;
+        $options = [];
+        foreach (ListingDuration::getValidValues() as $code => $text) {
+            $options[] = ['title' => $text, 'value' => $code, 'selected' => $code == $selected];
+        }
+        return $this->getSelectView(
+            'ebayAccountListingDefaultDuration',
+            'listingDuration',
+            $options,
+            $selected
+        );
+    }
+
+    protected function getPaymentMethodView(array $selected = []): ViewModel
+    {
+        $selected = (!empty($selected) ? $selected : static::DEFAULT_PAYMENT_METHODS);
+        $options = [];
+        foreach (PaymentMethod::getStandardValues() as $code => $text) {
+            $options[] = ['title' => $text, 'value' => $code, 'selected' => in_array($code, $selected)];
+        }
+        return $this->getMultiSelectView(
+            'ebayAccountListingPaymentMethod',
+            'listingPaymentMethods',
+            $options,
+            $selected
+        );
+    }
+
+    protected function getSelectView(string $id, string $name, array $options): ViewModel
+    {
+        $select = $this->getBaseSelectView($id, $name, $options);
+        $select->setTemplate('elements/custom-select.mustache');
+        return $select;
+    }
+
+    protected function getMultiSelectView(string $id, string $name, array $options): ViewModel
+    {
+        $select = $this->getBaseSelectView($id, $name, $options);
+        $select->setTemplate('elements/custom-select-group.mustache');
+        return $select;
+    }
+
+    protected function getBaseSelectView(string $id, string $name, array $options): ViewModel
+    {
+        return $this->viewModelFactory->newInstance([
+            'id' => $id,
+            'name' => $name,
+            'options' => $options
+        ]);
     }
 }
