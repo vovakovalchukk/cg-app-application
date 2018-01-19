d9323e91f7315f60c0a874ff22b4b7fd2921c6ae
Aaron Baker <aaron.baker@channelgrabber.com>
2018-01-19 16:37:50 +0000

    LIS-34: added first-pass at a new React component for showing messages within a popup such as for errors

---
+++ public/channelgrabber/common/jsx/Common/Components/Popup/Message.jsx
@@ -0,0 +1,50 @@
+define([
+    'react'
+], function(
+    React
+) {
+    "use strict";
+
+    var MessageComponent = React.createClass({
+        getDefaultProps: function () {
+            return {
+                initiallyActive: false,
+                headerText: "",
+                className: "",
+                closeButtonText: "Close"
+            };
+        },
+        getInitialState: function () {
+            return {
+                active: this.props.initiallyActive
+            };
+        },
+        componentWillReceiveProps: function (nextProps) {
+            this.setState({
+                active: nextProps.initiallyActive
+            });
+        },
+        closeButtonPressed: function()
+        {
+            this.setState({
+                active: false
+            });
+        },
+        render: function () {
+            if (!this.state.active) {
+                return;
+            }
+            return (
+                <div className={"react-popup-message " + this.props.className }>
+                    <div className="react-popup-message-header">{this.props.headerText}</div>
+                    <div className="react-popup-message-content">{this.props.children}</div>
+                    <div className="react-popup-message-buttons">
+                        <div className="button react-popup-btn close" onClick={this.closeButtonPressed}>{this.props.closeButtonText}</div>
+                    </div>
+                </div>
+            );
+        }
+    });
+
+    return MessageComponent;
+});
