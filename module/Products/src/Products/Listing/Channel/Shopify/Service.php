bf80a734fdfdec26e27b9bde4acd106b39bf8549
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2018-01-29 11:25:53 +0000

    LIS-48: Fetch shopify categories by filtering to the accountId

--- module/Products/src/Products/Listing/Channel/Shopify/Service.php
+++ module/Products/src/Products/Listing/Channel/Shopify/Service.php
@@ -2,6 +2,9 @@
 namespace Products\Listing\Channel\Shopify;
 
 use CG\Account\Shared\Entity as Account;
+use CG\Product\Category\Entity as Category;
+use CG\Product\Category\Filter as CategoryFilter;
+use CG\Product\Category\Service as CategoryService;
 use Products\Listing\Channel\CategoriesRefreshInterface;
 use Products\Listing\Channel\ChannelSpecificValuesInterface;
 
@@ -9,12 +12,18 @@ class Service implements
     ChannelSpecificValuesInterface,
     CategoriesRefreshInterface
 {
+    /** @var CategoryService */
+    protected $categoryService;
+
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
+
     public function getChannelSpecificFieldValues(Account $account): array
     {
         return [
-            "categories" => [
-                123  => "category name"
-            ]
+            'categories' => $this->fetchCategoriesForAccount($account)
         ];
     }
 
@@ -23,5 +32,22 @@ class Service implements
         return [
             456  => "updated category name"
         ];
+    }
+
+    protected function fetchCategoriesForAccount(Account $account)
+    {
+        $categories = $this->categoryService->fetchCollectionByFilter(
+            (new CategoryFilter())
+                ->setLimit('all')
+                ->setPage(1)
+                ->setChannel(['shopify'])
+                ->setAccountId([$account->getId()])
+        );
+        $result = [];
+        /** @var Category $category */
+        foreach ($categories as $category) {
+            $result[$category->getExternalId()] = $category->getTitle();
+        }
+        return $result;
     }
 }
