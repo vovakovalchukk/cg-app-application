6ae54e33546aef4961270c79c3eb597ac13266a2
Richard Heelin <richardh@channelgrabber.com>
2018-01-30 11:24:21 +0000

    CGIV-9205: Added a new command for clearing session from a cron

--- console/app.php
+++ console/app.php
@@ -3,21 +3,27 @@ chdir(dirname(__DIR__));
 
 use CG\Cilex\GenericCommand;
 use CG\Cilex\ServiceProvider\Bootstrap;
+use CG_Sessions\GCCommand as SessionGarbageCollection;
 use Cilex\Application as Cilex;
+use Zend\Di\Config as DiConfig;
 use Zend\Di\Di;
 use Zend\Mvc\Application as Zend;
 
 require_once 'application/bootstrap.php';
 require_once 'init_autoloader.php';
 
-$di = Zend::init(require 'config/console.config.php')->getServiceManager()->get(Di::class);
+$serviceManager = Zend::init(require 'config/console.config.php')->getServiceManager();
+$di = $serviceManager->get(Di::class);
 $commands = require_once 'config/console/commands.php';
 
-/**
- * @var Cilex $app
- */
+foreach (['CG_Sessions'] as $modules) {
+    (new DiConfig($serviceManager->get('ModuleManager')->loadModule($modules)->getConfig()['di'] ?? []))->configure($di);
+}
+
+/** @var Cilex $app */
 $app = $di->get(Cilex::class);
 $app->register($di->get(Bootstrap::class));
+$app->command($di->get(SessionGarbageCollection::class));
 foreach ($commands as $commandName => $command) {
     $app->command($di->newInstance(GenericCommand::class, ['commandName' => $commandName, 'command' => $command]));
 }
