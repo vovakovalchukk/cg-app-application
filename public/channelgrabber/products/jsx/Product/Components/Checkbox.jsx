348016b629a5c2bebe38234cebb75db97c268143
James Moorhouse <jamesm@channelgrabber.com>
2018-02-16 13:09:39 +0000

    LIS-101 checkboxes on variation picker are working

--- public/channelgrabber/products/jsx/Product/Components/Checkbox.jsx
+++ public/channelgrabber/products/jsx/Product/Components/Checkbox.jsx
@@ -6,13 +6,25 @@ define([
     "use strict";
 
     var CheckboxComponent = React.createClass({
+        getDefaultProps: function() {
+            return {
+                onClick: null,
+                isChecked: null
+            }
+        },
         render: function()
         {
             return (
                 <div className="checkbox-container">
                     <div className="checkbox-holder bulk-action-checkbox">
                         <a className="std-checkbox">
-                            <input type="checkbox" id={"product-checkbox-input-"+this.props.id} name="" />
+                            <input
+                                type="checkbox"
+                                id={"product-checkbox-input-"+this.props.id}
+                                name=""
+                                onClick={this.props.onClick}
+                                checked={this.props.isChecked}
+                            />
                             <label htmlFor={"product-checkbox-input-"+this.props.id}>
                                 <span className="checkbox_label"></span>
                             </label>
--- public/channelgrabber/products/jsx/Product/Components/CreateListing/Form/Shared/VariationPicker.jsx
+++ public/channelgrabber/products/jsx/Product/Components/CreateListing/Form/Shared/VariationPicker.jsx
@@ -1,7 +1,9 @@
 define([
-    'react'
+    'react',
+    'Product/Components/Checkbox'
 ], function(
-    React
+    React,
+    Checkbox
 ) {
     "use strict";
 
@@ -11,21 +13,79 @@ define([
                 variations: []
             }
         },
+        getInitialState: function() {
+            return {
+                variationsFormState: {},
+                allChecked: true
+            }
+        },
+        componentDidMount: function(props, a, b) {
+            this.createVariationFormStateFromProps(this.props);
+        },
+        componentWillReceiveProps: function(newProps) {
+            this.createVariationFormStateFromProps(newProps);
+        },
+        createVariationFormStateFromProps: function(newProps) {
+            var variationsFormState = {};
+            for (var variationIndex in newProps.variations) {
+                var currentVariation = newProps.variations[variationIndex];
+                variationsFormState[currentVariation.id] = {checked: true};
+            }
+
+            this.setState({
+                variationsFormState: variationsFormState
+            })
+        },
+        onCheckBoxClick: function(variationId) {
+            var newVariationsState = Object.assign({}, this.state.variationsFormState);
+            newVariationsState[variationId].checked = ! newVariationsState[variationId].checked;
+
+            var allChecked = true;
+            for (var variationId in newVariationsState) {
+                if (newVariationsState[variationId].checked == false) {
+                    allChecked = false;
+                    break;
+                }
+            }
+            this.setState({
+                variationsFormState: newVariationsState,
+                allChecked: allChecked
+            })
+        },
         renderVariationRows: function () {
             return this.props.variations.map(function(variation) {
                 return <tr>
-                    <td>checkbox</td>
+                    <td>
+                        <Checkbox
+                            id={variation.id}
+                            onClick={this.onCheckBoxClick.bind(this, variation.id)}
+                            isChecked={this.state.variationsFormState[variation.id] ? this.state.variationsFormState[variation.id].checked : true}
+                        />
+                    </td>
                     <td>{variation.sku}</td>
                     <td>{variation.price}</td>
                 </tr>
-            });
+            }.bind(this));
+        },
+        onCheckAll: function() {
+            var newVariationsState = Object.assign({}, this.state.variationsFormState);
+
+            var newCheckedState = this.state.allChecked ? false : true;
+            for (var variationId in newVariationsState) {
+                newVariationsState[variationId].checked = newCheckedState;
+            }
+
+            this.setState({
+                variationsFormState: newVariationsState,
+                allChecked: ! this.state.allChecked
+            })
         },
         render: function() {
             return (
                 <div>
                     <table>
                         <tr>
-                            <td>checkbox</td>
+                            <td><Checkbox onClick={this.onCheckAll} isChecked={this.state.allChecked} /></td>
                             <td>sku</td>
                             <td>price</td>
                         </tr>
