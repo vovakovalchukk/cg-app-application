71dd839b2e73c566b94b4474f934564bfeae2537
Richard Heelin <richardh@channelgrabber.com>
2018-03-02 09:21:40 +0000

    TAC-19: Added a new factory class to help with creating the provider classes

--- library/CG/CourierExport/ShippingService.php
+++ library/CG/CourierExport/ShippingService.php
@@ -4,26 +4,23 @@ namespace CG\CourierExport;
 use CG\Account\Shared\Entity as Account;
 use CG\Channel\Shipping\ServicesInterface;
 use CG\Order\Shared\ShippableInterface as Order;
-use CG\Stdlib\Exception\Runtime\NotFound;
-use function CG\Stdlib\hyphenToClassname;
 
 class ShippingService implements ServicesInterface
 {
+    /** @var Factory */
+    protected $factory;
     /** @var Account */
     protected $account;
 
-    public function __construct(Account $account)
+    public function __construct(Factory $factory, Account $account)
     {
+        $this->factory = $factory;
         $this->account = $account;
     }
 
     protected function getShippingService(): ServicesInterface
     {
-        $class = __NAMESPACE__ . '\\' . hyphenToClassname($this->account->getChannel()) . '\\ShippingService';
-        if (!class_exists($class) || !is_a($class, ServicesInterface::class, true)) {
-            throw new NotFound(sprintf('Unsupported account "%s"', $this->account->getChannel()));
-        }
-        return new $class();
+        return $this->factory->getShippingServiceForAccount($this->account);
     }
 
     public function getShippingServices()
