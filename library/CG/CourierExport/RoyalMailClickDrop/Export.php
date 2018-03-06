7ace51234c0ae8153907a6dfebd469f8f9f991ef
Richard Heelin <richardh@channelgrabber.com>
2018-03-06 16:36:46 +0000

    TAC-19: We want to compare the keys not the data

--- library/CG/CourierExport/RoyalMailClickDrop/Export.php
+++ library/CG/CourierExport/RoyalMailClickDrop/Export.php
@@ -64,7 +64,7 @@ class Export implements ExportDocumentInterface
     protected function getRow(array $row): string
     {
         $fields = array_fill_keys(array_values($this->headers), '');
-        $rowData = array_merge($fields, array_intersect_assoc($row, $fields));
+        $rowData = array_merge($fields, array_intersect_key($row, $fields));
         return str_putcsv($rowData);
     }
 }
