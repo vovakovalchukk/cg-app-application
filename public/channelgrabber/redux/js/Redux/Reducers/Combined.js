c053a70134a56cc8f468531cde02c6b39be06ec6
Aaron Baker <aaron.baker@channelgrabber.com>
2018-03-07 14:08:55 +0000

    LIS-125: removed accidentally committed, generated JS files

--- public/channelgrabber/redux/js/Redux/Reducers/Combined.js
+++
@@ -1,7 +0,0 @@
-define(['redux', 'Redux/Reducers/Todo', 'Redux/Reducers/FilterLink'], function (Redux, TodoReducer, FilterLinkReducer) {
-    var combined = Redux.combineReducers({
-        todos: TodoReducer,
-        visibilityFilter: FilterLinkReducer
-    });
-    return combined;
-});
