b8271ecdfa88393ecdb94bf1f804c6615480558b
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2018-03-28 13:59:58 +0100

    LIS-119: add comment on the debug code

--- public/channelgrabber/products/jsx/CategoryMapper/Components/Root.jsx
+++ public/channelgrabber/products/jsx/CategoryMapper/Components/Root.jsx
@@ -9,6 +9,10 @@ define([
 
     var RootComponent = React.createClass({
         submitCategoryMap: function(values) {
+            /**
+             *  @TODO: this will be handled by LIS-121, but I'll leave this debug code in here,
+             *  @TODO: so that we know what are the form values when pressing the Save button
+             * */
             console.log(values);
         },
         render: function()
