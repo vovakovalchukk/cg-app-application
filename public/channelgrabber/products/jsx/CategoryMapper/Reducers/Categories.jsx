be09ca6195ddf2df396076b35bfbf663ad446974
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2018-04-05 16:37:20 +0100

    LIS-120: WIP - fix issues with setting the state after refreshing the categories

--- public/channelgrabber/products/jsx/CategoryMapper/Reducers/Categories.jsx
+++ public/channelgrabber/products/jsx/CategoryMapper/Reducers/Categories.jsx
@@ -36,52 +36,10 @@ define([
 
             return newState;
         },
-        "REFRESH_CATEGORIES": function (state, action) {
-            var newState = state.slice(0);
-            var accountId = action.payload.accountId;
-
-            for (var i = 0; i < newState.length; i++) {
-                var newCategoryMap = Object.assign({}, newState[i].categoryMap);
-                newCategoryMap[accountId] = Object.assign({}, newCategoryMap[accountId], {
-                    categories: [{0: {tile: ''}}],
-                    refreshing: true
-                });
-                newState[i].categoryMap = newCategoryMap;
-            }
-
-            return newState;
-        },
         "REFRESH_CATEGORIES_FETCHED": function (state, action) {
-            var newState = state.slice(0);
-            var accountId = action.payload.accountId;
-
-            for (var i = 0; i < newState.length; i++) {
-                var newCategoryMap = Object.assign({}, newState[i].categoryMap);
-                newCategoryMap[accountId] = Object.assign({}, newCategoryMap[accountId], {
-                    categories: [action.payload.categories],
-                    refreshing: false
-                });
-                newState[i].categoryMap = newCategoryMap;
-            }
-
-            return newState;
-        },
-        "REMOVE_ROOT_CATEGORY": function (state, action) {
-            var newState = state.slice(0);
-            var accountId = action.payload.accountId;
-            var categoryMapIndex = action.payload.categoryMapIndex;
-
-            var newCategoryMap = Object.assign({}, newState[categoryMapIndex].categoryMap);
-
-            var newCategoriesArray = newCategoryMap[accountId].categories.slice(0);
-            newCategoriesArray.splice(1);
-
-            newCategoryMap[accountId] = Object.assign({}, newCategoryMap[accountId], {
-                categories: newCategoriesArray,
-                resetSelection: true
+            var newState = Object.assign({}, state, {
+                [action.payload.accountId]: Object.assign({}, action.payload.categories)
             });
-
-            newState[categoryMapIndex].categoryMap = newCategoryMap
 
             return newState;
         },
