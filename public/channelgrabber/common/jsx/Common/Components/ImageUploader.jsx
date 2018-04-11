89998c5f0c881241d4216635e51f23b07cd6561f
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2018-04-11 08:51:38 +0100

    LIS-189 - Created ImageUploaderComponent component wrapper in React

---
+++ public/channelgrabber/common/jsx/Common/Components/ImageUploader.jsx
@@ -0,0 +1,23 @@
+define([
+    'react'
+], function (
+    React
+) {
+    "use strict";
+    
+    var ImageUploaderComponent = React.createClass({
+        getDefaultProps: function () {
+            return {};
+        },
+        getInitialState: function () {
+            return {};
+        },
+        render: function () {
+            return (
+            
+            );
+        }
+    });
+    
+    return ImageUploaderComponent;
+});
