3db6b7eb01d3f792b3628b2a2c7c057af6d731d2
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2018-04-15 10:34:04 +0100

    LIS-121: Refactor the root component into better named methods

--- public/channelgrabber/products/jsx/CategoryMapper/Components/Root.jsx
+++ public/channelgrabber/products/jsx/CategoryMapper/Components/Root.jsx
@@ -3,18 +3,40 @@ define([
     'redux-form',
     'CategoryMapper/Components/CategoryMaps',
     'CategoryMapper/Actions/ResponseActions',
+    'CategoryMapper/Actions/ApiHelper',
 ], function(
     React,
     ReduxForm,
     CategoryMaps,
-    Actions
+    Actions,
+    ApiHelper
 ) {
     "use strict";
 
     var SubmissionError = ReduxForm.SubmissionError;
 
-    var saveCategoryMap = function(values) {
-        var extractCategoryIds = function(categories) {
+    var RootComponent = React.createClass({
+        validateForm: function(mapId, values) {
+            if (!values.name || values.name.length < 2) {
+                throw new SubmissionError({name: 'The name is too short.'});
+            }
+        },
+        checkResponseForErrors: function(response) {
+            if (response.error) {
+                if (response.error.code == 'existing name') {
+                    throw new SubmissionError({name: response.error.message});
+                }
+                if (response.error.code == 'existing category') {
+                    // This doesn't work as expected as it triggers the whole array of accounts to fail, but it's a start
+                    throw new SubmissionError({
+                        categories: {
+                            _error: response.error.message
+                        }
+                    });
+                }
+            }
+        },
+        extractCategoryIdsFromFormValues: function(categories) {
             var categoryIds = [];
             categories.forEach(function(categoryId, accountId) {
                 if (categoryId) {
@@ -22,64 +44,41 @@ define([
                 }
             });
             return categoryIds;
-        }
-        return new Promise(function(resolve) {
-            return $.post(
-                '/settings/category/templates/save',
-                {
-                    name: values.name,
-                    etag: values.etag,
-                    categoryIds: extractCategoryIds(values.categories)
-                }
-            ).success(
-                function (response) {
+        },
+        saveCategoryMap: function(values) {
+            return new Promise(function(resolve) {
+                return $.post(
+                    ApiHelper.buildSaveCategoryMapUrl(),
+                    {
+                        name: values.name,
+                        etag: values.etag,
+                        categoryIds: this.extractCategoryIdsFromFormValues(values.categories)
+                    }
+                ).success(function(response) {
                     resolve(response);
-                }
-            );
-        });
-    };
-
-    var RootComponent = React.createClass({
-        validateForm: function(mapId, values) {
-            if (!values.name || values.name.length < 3) {
-                throw new SubmissionError({name: 'The name is too short.'});
-            }
+                }).error(function() {
+                    n.error('There was an error while saving the category map. Please try again or contact support if the problem persists.');
+                })
+            }.bind(this));
         },
         submitCategoryMap: function(values, dispatch, state) {
             var mapId = state.mapId;
             this.validateForm(mapId, values);
-            /**
-             *  @TODO: this will be handled by LIS-121, but I'll leave this debug code in here,
-             *  @TODO: so that we know what are the form values when pressing the Save button
-             * */
-            console.log({
-                dispatch: dispatch,
-                values: values,
-                state: state
-            });
 
-            return saveCategoryMap(values).then(function(response) {
-                if (response.error) {
-                    if (response.error.code == 'existing name') {
-                        throw new SubmissionError({name: response.error.message});
-                    }
-                    if (response.error.code == 'existing category') {
-                        // This doesn't work as expected as it triggers the whole array of accounts to fail, but it's a start
-                        throw new SubmissionError({
-                            categories: {
-                                _error: response.error.message
-                            }
-                        });
-                    }
-                }
+            console.log(mapId, values);
+
+            return this.saveCategoryMap(values).then(function(response) {
+                this.checkResponseForErrors(response);
 
                 if (mapId == 0) {
+                    n.success('The new category map <b>' + values.name + '</b> has been saved successfully');
                     dispatch(Actions.addCategoryMap(response.id, response.etag, values));
                     return;
                 }
 
+                n.success('The category map <b>' +  values.name + '</b> has been successfully updated.');
                 dispatch(Actions.updateCategoryMap(mapId, response.etag, values));
-            });
+            }.bind(this));
         },
         render: function()
         {
