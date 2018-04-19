549065046a2a042a7d8fc4c20a5d1f33e2432a06
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2018-04-19 12:19:20 +0100

    LIS-164: remove the dummy data code in the progress action

--- module/Products/src/Products/Controller/CreateMultipleListings/JsonController.php
+++ module/Products/src/Products/Controller/CreateMultipleListings/JsonController.php
@@ -51,28 +51,10 @@ class JsonController extends AbstractJsonController
             }
             $response[$accountId]['categories'][$categoryId] = $status;
         }
+
         return $this->buildResponse([
             'accounts' => $response
         ]);
 
-        // Dummy Data
-        $statuses = ['started', 'complete', 'error'];
-        $warnings = ['This is a warning', 'This is a different warning', 'This is also a warning'];
-        $errors = ['This is an error', 'This is a different error', 'ERROR!!!!'];
-        $dummyData = [];
-        foreach ($this->params()->fromPost('accountId', []) as $accountId) {
-            $dummyData[$accountId] = ['categories' => []];
-            foreach ($this->params()->fromPost('categoryTemplateId', []) as $categoryTemplateId) {
-                $status = $statuses[rand(0, count($statuses) - 1)];
-                shuffle($warnings);
-                shuffle($errors);
-                $dummyData[$accountId]['categories'][$categoryTemplateId] = [
-                    'status' => $status,
-                    'warnings' => array_slice($warnings, 0, rand(0, count($warnings) - 1)),
-                    'errors' => $status == 'error' ? array_slice($errors, 0, rand(1, count($errors) - 1)) : [],
-                ];
-            }
-        }
-        return $this->buildResponse($dummyData);
     }
 }
