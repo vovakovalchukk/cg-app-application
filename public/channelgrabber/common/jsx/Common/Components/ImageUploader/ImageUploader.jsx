253240978a0815868137c5e4311f827b4dd930d0
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2018-04-20 10:12:41 +0100

    LIS-190 - resolved merge conflict on ImageUploader

--- public/channelgrabber/common/jsx/Common/Components/ImageUploader/ImageUploader.jsx
+++ public/channelgrabber/common/jsx/Common/Components/ImageUploader/ImageUploader.jsx
@@ -8,12 +8,8 @@ define([
     var ImageUploaderComponent = React.createClass({
         getDefaultProps: function() {
             return {
-<<<<<<< HEAD
                 uploadImageHandler: null,
                 className: ''
-=======
-                uploadImageHandler: null
->>>>>>> LIS-189-image-upload-on-product-creation-form
             };
         },
         onUploadButtonClick: function(e) {
@@ -23,11 +19,6 @@ define([
             return (
                 <div>
 
-<<<<<<< HEAD
-                    <input type="file" onChange={this.props.uploadImageHandler} onClick={this.onUploadButtonClick}
-                           name="fileupload" className={this.props.className}
-                           id="fileupload"/>
-=======
                     <input
                         type="file"
                         onChange={this.props.uploadImageHandler}
@@ -35,8 +26,6 @@ define([
                         name="fileupload"
                         id="fileupload"
                     />
->>>>>>> LIS-189-image-upload-on-product-creation-form
-
                 </div>
             );
         }
