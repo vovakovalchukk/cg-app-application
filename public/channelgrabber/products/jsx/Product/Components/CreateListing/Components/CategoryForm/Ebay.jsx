c0bc9b6f4b7cf942131be268099d34175c71d9aa
Aaron Baker <aaron.baker@channelgrabber.com>
2018-05-01 15:05:08 +0100

    LIS-158: multi-listing creation - added Listing Duration for eBay categories

--- public/channelgrabber/products/jsx/Product/Components/CreateListing/Components/CategoryForm/Ebay.jsx
+++ public/channelgrabber/products/jsx/Product/Components/CreateListing/Components/CategoryForm/Ebay.jsx
@@ -1,8 +1,10 @@
 define([
     'react',
+    './Ebay/ListingDuration',
     './Ebay/ItemSpecifics'
 ], function(
     React,
+    ListingDuration,
     ItemSpecifics
 ) {
     "use strict";
@@ -11,12 +13,14 @@ define([
         getDefaultProps: function() {
             return {
                 categoryId: null,
+                listingDuration: {},
                 itemSpecifics: {}
             };
         },
         render: function() {
             return (
                 <div className="ebay-category-form-container">
+                    <ListingDuration listingDurations={this.props.listingDuration} />
                     <ItemSpecifics categoryId={this.props.categoryId} itemSpecifics={this.props.itemSpecifics} />
                 </div>
             );
---
+++ public/channelgrabber/products/jsx/Product/Components/CreateListing/Components/CategoryForm/Ebay/ListingDuration.jsx
@@ -0,0 +1,63 @@
+define([
+    'react',
+    'redux-form',
+    'Common/Components/Select'
+], function(
+    React,
+    ReduxForm,
+    Select
+) {
+    "use strict";
+
+    var Field = ReduxForm.Field;
+
+    var EbayListingDuration = React.createClass({
+        getDefaultProps: function() {
+            return {
+                listingDurations: {}
+            };
+        },
+        renderSelect: function(field) {
+            var options = this.buildListingDurationOptions(this.props.listingDurations);
+            var selectedOption = this.findSelectedOptionFromValue(field.input.value, options);
+            return <label>
+                <span className={"inputbox-label"}>Listing Duration</span>
+                <div className={"order-inputbox-holder"}>
+                    <Select
+                        name="listingDuration"
+                        options={options}
+                        autoSelectFirst={false}
+                        title="Listing Duration"
+                        onOptionChange={this.onOptionChange.bind(this, field.input)}
+                        selectedOption={selectedOption}
+                    />
+                </div>
+            </label>;
+        },
+        buildListingDurationOptions: function(listingDurations) {
+            var options = [];
+            for (var value in listingDurations) {
+                options.push({
+                    "name": listingDurations[value],
+                    "value": value
+                });
+            }
+            return options;
+        },
+        findSelectedOptionFromValue: function(selectedValue, options) {
+            for (var key in options) {
+                if (options[key].value == selectedValue) {
+                    return options[key];
+                }
+            }
+            return null;
+        },
+        onOptionChange: function(input, selectedOption) {
+            input.onChange(selectedOption.value);
+        },
+        render: function() {
+            return <Field name="listingDuration" component={this.renderSelect} />;
+        }
+    });
+    return EbayListingDuration;
+});
