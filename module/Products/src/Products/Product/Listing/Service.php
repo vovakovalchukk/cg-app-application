01f4a19405fd38213be0b9b0d02dd851cb142f3a
Aaron Baker <aaron.baker@channelgrabber.com>
2018-05-16 15:59:22 +0100

    LIS-192: commercialise listings - improved check for permission to create listings by caching it in the session.
    We only do this if the user *does* have permission. This means that when they add the Listings package we don't have to invalidate the cache.
    If they remove the package the permission status will still be cached but we've set it to expire after 24 hours.
    
    Also added some logging around this.

--- module/Products/src/Products/Product/Listing/Service.php
+++ module/Products/src/Products/Product/Listing/Service.php
@@ -4,38 +4,80 @@ namespace Products\Product\Listing;
 use CG\Billing\Licence\Entity as Licence;
 use CG\Billing\Subscription\Service as SubscriptionService;
 use CG\Stdlib\Exception\Runtime\NotFound;
+use CG\Stdlib\Log\LoggerAwareInterface;
+use CG\Stdlib\Log\LogTrait;
 use CG\Stdlib\Sites;
 use CG\User\ActiveUserInterface;
+use Zend\Session\SessionManager;
 
-class Service
+class Service implements LoggerAwareInterface
 {
+    use LogTrait;
+
+    const LOG_CODE = 'ProductListingService';
+    const CACHE_TTL = 86400; // 24 hours
+
     /** @var ActiveUserInterface */
     protected $activeUserContainer;
     /** @var SubscriptionService */
     protected $subscriptionService;
     /** @var Sites */
     protected $sites;
+    /** @var SessionManager */
+    protected $sessionManager;
 
     public function __construct(
         ActiveUserInterface $activeUserContainer,
         SubscriptionService $subscriptionService,
-        Sites $sites
+        Sites $sites,
+        SessionManager $sessionManager
     ) {
         $this->activeUserContainer = $activeUserContainer;
         $this->subscriptionService = $subscriptionService;
         $this->sites = $sites;
+        $this->sessionManager = $sessionManager;
     }
 
     public function isListingCreationAllowed(): bool
     {
+        $rootOuId = $this->activeUserContainer->getActiveUserRootOrganisationUnitId();
+        $cachedStatus = $this->getCachedListingCreationAllowed();
+        if ($cachedStatus !== null) {
+            $this->logDebug('Got Listing Creation Allowed status for OU %d from cache: %s', ['ou' => $rootOuId, $cachedStatus ? 'allowed' : 'not allowed'], [static::LOG_CODE, 'ListingCreation', 'Cached'], ['rootOu' => $rootOuId]);
+            return $cachedStatus;
+        }
+
         try {
-            $this->subscriptionService->getCurrentPackageForOuId(
-                $this->activeUserContainer->getActiveUserRootOrganisationUnitId(), Licence::TYPE_LISTING
-            );
+            $this->subscriptionService->getCurrentPackageForOuId($rootOuId, Licence::TYPE_LISTING);
+            $this->setCachedListingCreationAllowed(true);
+            $this->logDebug('Listing creation is allowed for OU %d', ['ou' => $rootOuId], [static::LOG_CODE, 'ListingCreation', 'Allowed'], ['rootOu' => $rootOuId]);
             return true;
         } catch (NotFound $e) {
+            $this->logDebug('Listing creation is NOT allowed for OU %d', ['ou' => $rootOuId], [static::LOG_CODE, 'ListingCreation', 'NotAllowed'], ['rootOu' => $rootOuId]);
+            // Don't cache when false so that when users add Listings we don't have to invalidate any cache
             return false;
         }
+    }
+
+    protected function getCachedListingCreationAllowed(): ?bool
+    {
+        $session = $this->sessionManager->getStorage();
+        if (!isset($session['ListingCreation'], $session['ListingCreation']['allowed'], $session['ListingCreation']['timestamp']) ||
+            $session['ListingCreation']['timestamp'] + static::CACHE_TTL < time()) {
+            return null;
+        }
+        return $session['ListingCreation']['allowed'];
+    }
+
+    protected function setCachedListingCreationAllowed(bool $allowed)
+    {
+        $session = $this->sessionManager->getStorage();
+        if (!isset($session['ListingCreation'])) {
+            $session['ListingCreation'] = [];
+        }
+        $session['ListingCreation']['allowed'] = $allowed;
+        $session['ListingCreation']['timestamp'] = time();
+        return $this;
     }
 
     public function getManagePackageUrl(): string
