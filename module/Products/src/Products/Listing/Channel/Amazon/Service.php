48ea023096f7853a4ed9cc225c8aa3199506cc06
Aaron Baker <aaron.baker@channelgrabber.com>
2018-05-23 16:33:42 +0100

    LIS-236: amazon listings - added dummy data for variationThemes in the category-template-dependent-field-values endpoint if the relevant feature flag is on.

--- module/Products/src/Products/Listing/Channel/Amazon/Service.php
+++ module/Products/src/Products/Listing/Channel/Amazon/Service.php
@@ -2,6 +2,9 @@
 namespace Products\Listing\Channel\Amazon;
 
 use CG\Account\Shared\Entity as Account;
+use CG\FeatureFlags\Service as FeatureFlagsService;
+use CG\Listing\Client\Service as ListingService;
+use CG\OrganisationUnit\Service as OrganisationUnitService;
 use CG\Stdlib\Exception\Runtime\NotFound;
 use Products\Listing\Category\Service as CategoryService;
 use Products\Listing\Channel\CategoryChildrenInterface;
@@ -15,10 +18,19 @@ class Service implements
 {
     /** @var CategoryService */
     protected $categoryService;
+    /** @var FeatureFlagsService */
+    protected $featureFlagsService;
+    /** @var OrganisationUnitService */
+    protected $organisationUnitService;
 
-    public function __construct(CategoryService $categoryService)
-    {
+    public function __construct(
+        CategoryService $categoryService,
+        FeatureFlagsService $featureFlagsService,
+        OrganisationUnitService $organisationUnitService
+    ) {
         $this->categoryService = $categoryService;
+        $this->featureFlagsService = $featureFlagsService;
+        $this->organisationUnitService = $organisationUnitService;
     }
 
     public function getChannelSpecificFieldValues(Account $account): array
@@ -39,10 +51,15 @@ class Service implements
 
     public function getCategoryDependentValues(?Account $account, int $categoryId): array
     {
-        return [
+        $values = [
             'itemSpecifics' => $this->getItemSpecifics(),
-            'rootCategories' => $this->categoryService->fetchRootCategoriesForAccount($account, true, null, false)
+            'rootCategories' => $this->categoryService->fetchRootCategoriesForAccount($account, true, null, false),
         ];
+        $rootOu = $this->organisationUnitService->getRootOuFromOuId($account->getOrganisationUnitId());
+        if ($this->featureFlagsService->isActive(ListingService::FEATURE_FLAG_CREATE_LISTINGS_VARIATIONS_AMAZON, $rootOu)) {
+            $values['variationThemes'] = $this->getVariationThemes();
+        }
+        return $values;
     }
 
     protected function getItemSpecifics(): array
@@ -97,6 +114,47 @@ class Service implements
         return [
             'required' => $required,
             'optional' => $optional
+        ];
+    }
+
+    protected function getVariationThemes(): array
+    {
+        // To be replaced by LIS-237
+        return [
+            [
+                'name' => 'SizeColor',
+                'validValues' => [
+                    [
+                        'name' => "Size",
+                        'options' => [
+                            'small' => 'small',
+                            'medium' => 'medium',
+                            'large' => 'large',
+                        ]
+                    ],
+                    [
+                        'name' => "Color",
+                        'options' => [
+                            'red' => 'red',
+                            'green' => 'green',
+                            'blue' => 'blue',
+                        ]
+                    ],
+                ]
+            ],
+            [
+                'name' => 'Color',
+                'validValues' => [
+                    [
+                        'name' => "Color",
+                        'options' => [
+                            'red' => 'red',
+                            'green' => 'green',
+                            'blue' => 'blue',
+                        ]
+                    ],
+                ]
+            ],
         ];
     }
 }
