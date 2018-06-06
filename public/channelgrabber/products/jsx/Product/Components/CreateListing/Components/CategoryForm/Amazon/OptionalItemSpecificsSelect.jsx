d380347132a1b407ebe38ef271ec69cef9ea9dee
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2018-06-06 11:29:10 +0100

    LIS-257: Remove an optional item specifics from the select dropdown when selected

--- public/channelgrabber/products/jsx/Product/Components/CreateListing/Components/CategoryForm/Amazon/OptionalItemSpecificsSelect.jsx
+++ public/channelgrabber/products/jsx/Product/Components/CreateListing/Components/CategoryForm/Amazon/OptionalItemSpecificsSelect.jsx
@@ -10,23 +10,51 @@ define([
     var OptionalItemSpecificsSelect = React.createClass({
         getDefaultProps: function () {
             return {
-                options: {},
+                options: [],
                 displayTitle: '',
                 input: null
             };
         },
+        getInitialState: function () {
+            return {
+                options: []
+            };
+        },
+        componentDidMount: function() {
+            this.setState({
+                options: this.props.options
+            });
+        },
         onOptionalItemSpecificSelected: function (selected) {
+            this.removeSelectedOptionFromOptions(selected);
             this.props.input.fields.push({
                 fieldName: selected.value
             });
         },
+        removeSelectedOptionFromOptions: function (selected) {
+            var selectedOptionIndex = this.state.options.findIndex(option => {
+                return option.value === selected.value;
+            });
+            if (selectedOptionIndex === -1) {
+                return;
+            }
+
+            var newOptions = this.state.options.slice();
+            newOptions.splice(selectedOptionIndex, 1);
+            this.setState({
+                options: newOptions
+            });
+        },
         render: function () {
+            if (this.state.options.length === 0) {
+                return null;
+            }
             return <label>
                 <span className={"inputbox-label"}>{this.props.displayTitle}</span>
                 <div className={"order-inputbox-holder"}>
                     <Select
                         name="optionalItemSpecifics"
-                        options={this.props.options}
+                        options={this.state.options}
                         autoSelectFirst={false}
                         onOptionChange={this.onOptionalItemSpecificSelected}
                         filterable={true}
