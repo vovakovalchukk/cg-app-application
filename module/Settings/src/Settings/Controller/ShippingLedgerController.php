da1b72ab375230baece23a0b082e251a8a37c32d
Mark Duffield <mark.duffield@channelgrabber.com>
2018-07-03 12:43:22 +0100

    TAC-121: Update to shippingledgercontroller to remove dummy data and add saving of shippingLedger using post data

--- module/Settings/src/Settings/Controller/ShippingLedgerController.php
+++ module/Settings/src/Settings/Controller/ShippingLedgerController.php
@@ -1,8 +1,11 @@
 <?php
 namespace Settings\Controller;
 
+use CG\Http\Exception\Exception3xx\NotModified;
 use CG_UI\View\Prototyper\JsonModelFactory;
 use Zend\Mvc\Controller\AbstractActionController;
+Use CG\Billing\Shipping\Ledger\Service as ShippingLedgerService;
+use Settings\Channel\Service as ChannelService;
 
 class ShippingLedgerController extends AbstractActionController
 {
@@ -12,10 +15,16 @@ class ShippingLedgerController extends AbstractActionController
 
     /** @var JsonModelFactory */
     protected $jsonModelFactory;
+    /** @var ShippingLedgerService */
+    protected $shippingLedgerService;
+    /** @var ChannelService */
+    protected $channelService;
 
-    public function __construct(JsonModelFactory $jsonModelFactory)
+    public function __construct(JsonModelFactory $jsonModelFactory, ShippingLedgerService $shippingLedgerService, ChannelService $channelService)
     {
         $this->jsonModelFactory = $jsonModelFactory;
+        $this->shippingLedgerService = $shippingLedgerService;
+        $this->channelService = $channelService;
     }
 
     public function topupAction()
@@ -31,8 +40,18 @@ class ShippingLedgerController extends AbstractActionController
 
     public function saveAction()
     {
-        $accountId = $this->params()->fromRoute('account');
+        $account = $this->channelService->getAccount($this->params()->fromRoute('account'));
+        $shippingLedger = $this->shippingLedgerService->fetch($account->getId());
+
         $input = $this->params()->fromPost();
+        $shippingLedger->setAutoTopUp($input['autoTopUp']);
+
+        try {
+            $this->shippingLedgerService->save($shippingLedger);
+        } catch (NotModified $exception) {
+            // Nothing to see here
+        }
+
         // Dummy data to be replaced in TAC-121
         return $this->jsonModelFactory->newInstance([
             'success' => true,
