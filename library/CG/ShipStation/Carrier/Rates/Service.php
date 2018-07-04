6ec1eb5f489c7fbc7d637c36c3fc356f5bbe8fb3
Aaron Baker <aaron.baker@channelgrabber.com>
2018-07-04 16:52:34 +0100

    TAC-125: ShipStation - updated code for fetching rates to actually fetch the rates from ShipEngine

--- library/CG/ShipStation/Carrier/Rates/Service.php
+++ library/CG/ShipStation/Carrier/Rates/Service.php
@@ -5,13 +5,33 @@ use CG\Account\Shared\Entity as Account;
 use CG\Channel\Shipping\Provider\Service\ShippingRate\OrderRates\Collection as ShippingRateCollection;
 use CG\Channel\Shipping\Provider\Service\ShippingRate\OrderRates as OrderShippingRates;
 use CG\Order\Shared\Collection as OrderCollection;
+use CG\Order\Shared\Entity as Order;
+use CG\Order\Shared\Courier\Label\OrderData;
 use CG\Order\Shared\Courier\Label\OrderData\Collection as OrderDataCollection;
 use CG\Order\Shared\Courier\Label\OrderItemsData\Collection as OrderItemsDataCollection;
+use CG\Order\Shared\Courier\Label\OrderParcelsData;
 use CG\Order\Shared\Courier\Label\OrderParcelsData\Collection as OrderParcelsDataCollection;
 use CG\OrganisationUnit\Entity as OrganisationUnit;
+use CG\ShipStation\Client;
+use CG\ShipStation\Messages\Rate as ShipStationRate;
+use CG\ShipStation\Messages\Shipment;
+use CG\ShipStation\Request\Shipping\Rates as RatesRequest;
+use CG\ShipStation\Response\Shipping\Rates as RatesResponse;
+use CG\ShipStation\ShipStation\Service as ShipStationService;
 
 class Service
 {
+    /** @var ShipStationService */
+    protected $shipStationService;
+    /** @var Client */
+    protected $client;
+
+    public function __construct(ShipStationService $shipStationService, Client $client)
+    {
+        $this->shipStationService = $shipStationService;
+        $this->client = $client;
+    }
+
     public function fetchRatesForOrders(
         OrderCollection $orders,
         OrderDataCollection $ordersData,
@@ -20,36 +40,54 @@ class Service
         OrganisationUnit $rootOu,
         Account $shippingAccount
     ): ShippingRateCollection {
-        // Dummy data to be replaced by TAC-125
-        $dummyData = [
-            [
-                'rate_id' => 'se-12345',
-                'shipping_amount' => [
-                    'currency' => 'usd',
-                    'amount' => 9.37
-                ],
-                'service_type' => 'USPS Test 1',
-                'service_code' => 'usps_test_1',
-            ],
-            [
-                'rate_id' => 'se-67890',
-                'shipping_amount' => [
-                    'currency' => 'usd',
-                    'amount' => 10.50
-                ],
-                'service_type' => 'USPS Test 2',
-                'service_code' => 'usps_test_2',
-            ],
-        ];
+        $shipStationAccount = $this->shipStationService->getShipStationAccountForShippingAccount($shippingAccount);
         $rates = new ShippingRateCollection();
         foreach ($orders as $order) {
-            $orderRates = new OrderShippingRates($order->getId());
-            foreach ($dummyData as $dummyRate) {
-                $rate = Entity::fromShipEngineRateData($dummyRate);
-                $orderRates->attach($rate);
-            }
+            $shipStationRates = $this->fetchRatesForOrderFromShipStation(
+                $order,
+                $ordersData->getById($order->getId()),
+                $ordersParcelsData->getById($order->getId()),
+                $shipStationAccount,
+                $shippingAccount,
+                $rootOu
+            );
+            $orderRates = $this->mapShipstationRatesToShippingRates($order->getId(), $shipStationRates);
             $rates->attach($orderRates);
         }
         return $rates;
+    }
+
+    protected function fetchRatesForOrderFromShipStation(
+        Order $order,
+        OrderData $orderData,
+        OrderParcelsData $parcelsData,
+        Account $shipStationAccount,
+        Account $shippingAccount,
+        OrganisationUnit $rootOu
+    ): array
+    {
+        $shipment = Shipment::createFromOrderAndData(
+            $order,
+            $orderData,
+            $parcelsData,
+            $shipStationAccount,
+            $shippingAccount,
+            $rootOu
+        );
+        $request = new RatesRequest($shipment, [$shippingAccount->getExternalId()]);
+        /** @var RatesResponse $response */
+        $response = $this->client->sendRequest($request, $shipStationAccount);
+        return $response->getRates();
+    }
+
+    protected function mapShipstationRatesToOrderShippingRates(int $orderId, array $shipStationRates): ShippingRateCollection
+    {
+        $orderRates = new OrderShippingRates($orderId);
+        /** @var ShipStationRate $shipStationRate */
+        foreach ($shipStationRates as $shipStationRate) {
+            $shippingRate = Entity::fromShipEngineRate($shipStationRate);
+            $orderRates->attach($shippingRate);
+        }
+        return $orderRates;
     }
 }
