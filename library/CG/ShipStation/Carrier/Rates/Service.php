11e92d2f226ce7a2e7a02fa89f451339c1a293ef
Aaron Baker <aaron.baker@channelgrabber.com>
2018-07-18 12:13:05 +0100

    TAC-125: USPS - improved rate fetching to handle errors and convert them to a format that can be shown to the user

--- library/CG/ShipStation/Carrier/Rates/Service.php
+++ library/CG/ShipStation/Carrier/Rates/Service.php
@@ -4,6 +4,7 @@ namespace CG\ShipStation\Carrier\Rates;
 use CG\Account\Shared\Entity as Account;
 use CG\Channel\Shipping\Provider\Service\ShippingRate\OrderRates\Collection as ShippingRateCollection;
 use CG\Channel\Shipping\Provider\Service\ShippingRate\OrderRates as OrderShippingRates;
+use CG\Http\StatusCode;
 use CG\Order\Shared\Collection as OrderCollection;
 use CG\Order\Shared\Entity as Order;
 use CG\Order\Shared\Courier\Label\OrderData;
@@ -20,9 +21,13 @@ use CG\ShipStation\Messages\Shipment;
 use CG\ShipStation\Request\Shipping\Rates as RatesRequest;
 use CG\ShipStation\Response\Shipping\Rates as RatesResponse;
 use CG\ShipStation\ShipStation\Service as ShipStationService;
+use CG\Stdlib\Exception\Runtime\ValidationException;
+use CG\Stdlib\Exception\Runtime\ValidationMessagesException;
 
 class Service
 {
+    const DEFAULT_RATE_ERROR = 'No rates found. Please check the data you entered and try again.';
+
     /** @var ShipStationService */
     protected $shipStationService;
     /** @var AccountDeciderFactory */
@@ -54,17 +59,25 @@ class Service
         $shippingAccountToUse = $accountDecider->getShippingAccountForRequests($shippingAccount);
 
         $rates = new ShippingRateCollection();
+        $exception = new ValidationMessagesException(StatusCode::BAD_REQUEST);
         foreach ($orders as $order) {
-            $shipStationRates = $this->fetchRatesForOrderFromShipStation(
-                $order,
-                $ordersData->getById($order->getId()),
-                $ordersParcelsData->getById($order->getId()),
-                $shipStationAccountToUse,
-                $shippingAccountToUse,
-                $rootOu
-            );
-            $orderRates = $this->mapShipstationRatesToOrderShippingRates($order->getId(), $shipStationRates);
-            $rates->attach($orderRates);
+            try {
+                $shipStationRates = $this->fetchRatesForOrderFromShipStation(
+                    $order,
+                    $ordersData->getById($order->getId()),
+                    $ordersParcelsData->getById($order->getId()),
+                    $shipStationAccountToUse,
+                    $shippingAccountToUse,
+                    $rootOu
+                );
+                $orderRates = $this->mapShipstationRatesToOrderShippingRates($order->getId(), $shipStationRates);
+                $rates->attach($orderRates);
+            } catch (ValidationException $e) {
+                $exception->addErrorWithField($order->getExternalId(), $e->getMessage());
+            }
+        }
+        if (!empty($exception->getErrors())) {
+            throw $exception;
         }
         return $rates;
     }
@@ -86,9 +99,16 @@ class Service
             $rootOu
         );
         $request = new RatesRequest($shipment, [$shippingAccount->getExternalId()]);
-        /** @var RatesResponse $response */
-        $response = $this->client->sendRequest($request, $shipStationAccount);
-        return $response->getRates();
+        try {
+            /** @var RatesResponse $response */
+            $response = $this->client->sendRequest($request, $shipStationAccount);
+        } catch (\Exception $e) {
+            throw new ValidationException(static::DEFAULT_RATE_ERROR, $e->getCode(), $e);
+        }
+        if (!empty($response->getRates())) {
+            return $response->getRates();
+        }
+        return $this->handleInvalidRatesResponse($response, $order);
     }
 
     protected function mapShipstationRatesToOrderShippingRates(string $orderId, array $shipStationRates): OrderShippingRates
@@ -100,5 +120,24 @@ class Service
             $orderRates->attach($shippingRate);
         }
         return $orderRates;
+    }
+
+    protected function handleInvalidRatesResponse(RatesResponse $response, Order $order): void
+    {
+        if (empty($response->getInvalidRates())) {
+            // No rates and no invalid rates, nothing to go off, show generic error
+            throw new ValidationException(static::DEFAULT_RATE_ERROR);
+        }
+        /** @var ShipStationRate $invalidRate */
+        foreach ($response->getInvalidRates() as $invalidRate) {
+            if (empty($invalidRate->getErrorMessages())) {
+                continue;
+            }
+            // Each potential rate is returned with a reason why it can't be used. The error is usually the same for each
+            // e.g. the weight is too high. Rather than show them all to the user we just show the first set of errors.
+            throw new ValidationException(implode('; ', $invalidRate->getErrorMessages()));
+        }
+        // No exception thrown yet, throw a generic one
+        throw new ValidationException(static::DEFAULT_RATE_ERROR);
     }
 }
