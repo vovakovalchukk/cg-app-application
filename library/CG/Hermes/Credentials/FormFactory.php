820c0c26368c33b94f21c67519b9bb28c76d120c
Aaron Baker <aaron.baker@channelgrabber.com>
2018-07-24 16:17:25 +0100

    TAC-170: Hermes - added credentials fields to represent whether the entered credentials are test or live and also to indicate if the test pack has been approved.
    We'll use the former to determine whether to connect to Hermes' test or live API.
    We'll use the latter to determine whether to offer the test pack for generation or not.

--- library/CG/Hermes/Credentials/FormFactory.php
+++ library/CG/Hermes/Credentials/FormFactory.php
@@ -23,11 +23,22 @@ class FormFactory
         $form->add((new Password("password", [
             "label" => "Password"
         ]))->setAttribute('required', true));
-//        $form->add(new Checkbox("live", [
-//            "label" => "Are these live credentials?",
-//            "checked_value" => 1,
-//            'unchecked_value' => 0
-//        ]));
+        $form->add(new Checkbox("liveCredentials", [
+            "label" => "Are these live credentials?",
+            "checked_value" => 1,
+            'unchecked_value' => 0,
+            'label_attributes' => [
+                'title' => 'Hermes will first give you test credentials then, after approval, live credentials. Please specify which these are.'
+            ]
+        ]));
+        $form->add(new Checkbox("testPackApproved", [
+            "label" => "Has your live test pack been approved by Hermes?",
+            "checked_value" => 1,
+            'unchecked_value' => 0,
+            'label_attributes' => [
+                'title' => 'Only check this after your test pack generated with your LIVE credentials has been approved by Hermes. This fully activates your Hermes account.'
+            ]
+        ]));
         return $form;
     }
 }
