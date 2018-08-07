6214ba75049e4b1a64e160a99e3b75eb4820ab07
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2018-08-07 15:42:06 +0100

    TAC-174 - attempting to pull back variations

--- public/channelgrabber/products/jsx/Product/Components/ProductList/ActionCreators.jsx
+++ public/channelgrabber/products/jsx/Product/Components/ProductList/ActionCreators.jsx
@@ -1,10 +1,26 @@
 define([
     'Product/Storage/Ajax',
+    'Product/Filter/Entity'
 ], function(
-    AjaxHandler
+    AjaxHandler,
+    ProductFilter
 ) {
     "use strict";
-    
+    // fetchVariations: function(filter) {
+    //     $('#products-loading-message').show();
+    //
+    //     function onSuccess(data) {
+    //         var variationsByParent = this.sortVariationsByParentId(data.products, filter.getParentProductId());
+    //         this.setState({
+    //             variations: variationsByParent
+    //         }, function() {
+    //             this.fetchLinkedProducts();
+    //             $('#products-loading-message').hide()
+    //         }.bind(this));
+    //     }
+    //
+    //     AjaxHandler.fetchByFilter(filter, onSuccess.bind(this));
+    // },
     return {
         initialSimpleAndParentProductsLoad: (products) => {
             return {
@@ -22,15 +38,27 @@ define([
             //     }
             // }
             return function(dispatch, getState) {
+                console.log('in epxandProduct with productRowIdToExpand: ' , productRowIdToExpand);
+                var filter = new ProductFilter(null, event.detail.productId);
+    
+    
                 dispatch({
                     type: 'PRODUCT_EXPAND_REQUEST'
                 });
                 // let filter =
-                let callback = function(dispatch){
+                
+    
+                let callback = function(data){
+                    console.log('in callback with data ' , data, ' and dispatch : ', dispatch);
+    
+                    var variationsByParent = sortVariationsByParentId(data.products, filter.getParentProductId());
+                    console.log('variationsByParent in callback: ', variationsByParent);
+                    
                     dispatch({
-                        type: 'PRODUCT_EXPAND_SUCCESS'
+                        type: 'PRODUCT_EXPAND_SUCCESS',
+                        payload:data
                     })
-                }
+                };
                 AjaxHandler.fetchByFilter(filter,callback.bind(dispatch));
             }
     
@@ -44,4 +72,20 @@ define([
             }
         }
     };
+    function sortVariationsByParentId(newVariations, parentProductId) {
+        var variationsByParent = {};
+        // if (parentProductId) {
+        //     variationsByParent = this.state.variations;
+        //     variationsByParent[parentProductId] = newVariations;
+        //     return variationsByParent;
+        // }
+        for (var index in newVariations) {
+            var variation = newVariations[index];
+            if (!variationsByParent[variation.parentProductId]) {
+                variationsByParent[variation.parentProductId] = [];
+            }
+            variationsByParent[variation.parentProductId].push(variation);
+        }
+        return variationsByParent;
+    }
 });
