034b1ea9305ed8efd0b3748621024e033597ee98
Aaron Baker <aaron.baker@channelgrabber.com>
2018-08-10 15:35:16 +0100

    TAC-194: ShipStation - updated webhook registration to handle conflicts from ShipEngine.
    A 409 just means we've already registered that webhook and dont need to do it again.

--- module/ShipStation/src/ShipStation/Webhook/Service.php
+++ module/ShipStation/src/ShipStation/Webhook/Service.php
@@ -2,11 +2,13 @@
 namespace ShipStation\Webhook;
 
 use CG\Account\Shared\Entity as Account;
+use CG\Http\StatusCode as HttpStatusCode;
 use CG\ShipStation\Client;
 use CG\ShipStation\Request\Webhook\Create as WebhookRequest;
 use CG\ShipStation\Webhook\Notification\Service as NotificationService;
 use CG\User\ActiveUserInterface;
 use CG_UI\View\Helper\RemoteUrl;
+use Guzzle\Http\Exception\ClientErrorResponseException;
 
 class Service
 {
@@ -34,7 +36,7 @@ class Service
     public function registerForCarrierConnectedWithActiveUser(Account $account, Account $shipStationAccount): void
     {
         $request = $this->buildWebhookRequest($shipStationAccount->getRootOrganisationUnitId());
-        $this->client->sendRequest($request, $shipStationAccount);
+        $this->sendWebhookRegistrationRequest($request, $shipStationAccount);
         $this->saveActiveUserForNotificationOfConnection($account);
     }
 
@@ -44,6 +46,20 @@ class Service
             WebhookRequest::EVENT_CARRIER_CONNECTED,
             ($this->remoteUrl)('/shipstation/carrierConnected/' . $organisationUnitId, 'hook')
         );
+    }
+
+    protected function sendWebhookRegistrationRequest(WebhookRequest $request, Account $shipStationAccount): void
+    {
+        try {
+            $this->client->sendRequest($request, $shipStationAccount);
+        } catch (ClientErrorResponseException $e) {
+            if ($e->getResponse()->getStatusCode() == HttpStatusCode::CONFLICT) {
+                // No-op. ShipEngine doesn't allow the same hook to be registered twice,
+                // so this just means its already set up for this OU
+                return;
+            }
+            throw $e;
+        }
     }
 
     protected function saveActiveUserForNotificationOfConnection(AccountEntity $account): void
