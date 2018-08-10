c8f2e42a04d8dc158e1a43c0f3dd2e2b3d9b0e5d
Aaron Baker <aaron.baker@channelgrabber.com>
2018-08-10 12:22:04 +0100

    TAC-194: ShipStation - added request and response classes for creating webhooks

---
+++ library/CG/ShipStation/Request/Webhook/Create.php
@@ -0,0 +1,44 @@
+<?php
+namespace CG\ShipStation\Request\Webhook;
+
+use CG\ShipStation\RequestAbstract;
+use CG\ShipStation\Response\Webhook\Create as Response;
+
+class Create extends RequestAbstract
+{
+    const EVENT_CARRIER_CONNECTED = 'API_CARRIER_CONNECTED';
+
+    /** @var string */
+    protected $event;
+    /** @var string */
+    protected $url;
+
+    public function __construct(string $event, string $url)
+    {
+        $this->event = $event;
+        $this->url = $url;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'event' => $this->getEvent(),
+            'url' => $this->getUrl(),
+        ];
+    }
+
+    public function getResponseClass(): string
+    {
+        return Response::class;
+    }
+
+    public function getEvent(): string
+    {
+        return $this->event;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+}
