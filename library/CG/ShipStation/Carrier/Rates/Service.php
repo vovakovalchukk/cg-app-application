866e478f82a1278de8702d7a2f0b14c5785bc5d3
markduffield <duffield.mark@gmail.com>
2018-08-21 14:59:39 +0100

    TAC-158: Fix to prevent user being unable to change service after attempting to fetch rates that have nothing suitable for package type

--- library/CG/ShipStation/Carrier/Rates/Service.php
+++ library/CG/ShipStation/Carrier/Rates/Service.php
@@ -150,6 +150,9 @@ class Service
                  unset($shipStationRates[$key]);
             }
         }
+        if (count($shipStationRate === 0)) {
+            throw new ValidationException('No rates found for the selected package type. Please select another package type and try again.');
+        }
         return $shipStationRates;
     }
 }
