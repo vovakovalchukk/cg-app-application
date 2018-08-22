29b1c26108ca64661adf3456f76d6b866a967c34
markduffield <duffield.mark@gmail.com>
2018-08-22 14:04:05 +0100

    TAC-122: Update to use the first order for the current courier to determine if the rates are fetchable

--- module/Orders/src/Orders/Controller/CourierController.php
+++ module/Orders/src/Orders/Controller/CourierController.php
@@ -258,7 +258,10 @@ class CourierController extends AbstractActionController
             ->setVariable('subHeaderHide', true);
 
         $provider = $this->carrierProviderServiceRepository->getProviderForAccount($selectedCourier);
-        $order = $this->orderService->fetch($courierOrders[$courierIds[0]][0]);
+
+        // For now the first order will suffice as USPS only cares about the account.
+        // If this needs to be specific to the individual orders in the future, please update this as needed.
+        $order = $this->orderService->fetch($courierOrders[$selectedCourier->getId()][0]);
         if ($provider instanceof FetchRatesInterface && $provider->isFetchRatesAllowedForOrder($selectedCourier, $order)) {
             $view->addChild(
                 $this->getShippingLedgerBalanceSection($this->shippingLedgerService->fetch($selectedCourier->getRootOrganisationUnitId())),
