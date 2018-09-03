58d306e425443b78a7ffe9f4ae78f044594793a7
markduffield <duffield.mark@gmail.com>
2018-09-03 15:31:29 +0100

    Adds in notification to advise the user their selected service us unavailable when the rates are returned and we cannot pre-select

--- public/channelgrabber/orders/js-vanilla/Courier/Specifics/Columns/Cost.js
+++ public/channelgrabber/orders/js-vanilla/Courier/Specifics/Columns/Cost.js
@@ -25,6 +25,7 @@ define(['./ServiceDependantOptionsAbstract.js', '../Storage.js'], function(Servi
         $(document).on('change', ServiceDependantOptionsAbstract.SELECTOR_SERVICE_SELECT, function(event, element, value)
         {
             if (value === undefined || value === "") {
+                self.displayNoServiceWarning();
                 return;
             }
                 var orderId = $(element).data('elementName').match(/^orderData\[(.+?)\]/)[1];
@@ -57,7 +58,12 @@ define(['./ServiceDependantOptionsAbstract.js', '../Storage.js'], function(Servi
            totalLabelCost += Number($(this).val());
         });
         $(Cost.SELECTOR_TOTAL_ORDER_LABEL_COST).text(totalLabelCost.toFixed(2));
-    }
+    };
+
+    Cost.prototype.displayNoServiceWarning = function()
+    {
+        n.notice('The service you requested is unavailable, please select an alternative');
+    };
 
     return Cost;
 });
