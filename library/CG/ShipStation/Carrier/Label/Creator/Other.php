dd838e523b2f06581ccb5e4fe9b28b9ccca27c1f
Aaron Baker <aaron.baker@channelgrabber.com>
2018-09-06 12:38:38 +0100

    TAC-218: ShipStation - changed the logic to determine if we can use test labels from a blacklist to a whitelist as it seems only USPS supports it

--- library/CG/ShipStation/Carrier/Label/Creator/Other.php
+++ library/CG/ShipStation/Carrier/Label/Creator/Other.php
@@ -61,7 +61,7 @@ class Other implements CreatorInterface, LoggerAwareInterface
     /** @var ShippingServiceFactory */
     protected $shippingServiceFactory;
 
-    protected $testLabelBlacklist = ['fedex-ss', 'ups-ss', 'royal-mail-ss'];
+    protected $testLabelWhitelist = ['usps-ss'];
 
     public function __construct(
         ShipStationClient $shipStationClient,
@@ -232,10 +232,10 @@ class Other implements CreatorInterface, LoggerAwareInterface
         if (ENVIRONMENT == 'live') {
             return false;
         }
-        if (in_array($shippingAccount->getChannel(), $this->testLabelBlacklist)) {
-            return false;
+        if (in_array($shippingAccount->getChannel(), $this->testLabelWhitelist)) {
+            return true;
         }
-        return true;
+        return false;
     }
 
     protected function getErrorsForUnsuccessfulLabels(array $labelResponses): array
