7b64774fd8e738262d2285f5bef6a7d0d27c0b3d
Aaron Baker <aaron.baker@channelgrabber.com>
2018-09-06 09:50:03 +0100

    TAC-218: ShipStation - added config required for setting up FedEx UK accounts

--- config/autoload/global/shipstation.php
+++ config/autoload/global/shipstation.php
@@ -357,6 +357,89 @@ return [
                                 'packageType' => 'packageType',
                             ],
                         ],
+                        'fedex-uk' => [
+                            'channelName' => 'fedex-uk-ss',
+                            'displayName' => 'FedEx (UK)',
+                            'allowsCancellation' => true,
+                            'allowsManifesting' => false,
+                            'allowsRates' => false,
+                            'fields' => [
+                                'nickname' => [
+                                    'name' => 'nickname',
+                                    'label' => 'Nickname for account',
+                                    'required' => true,
+                                ],
+                                'account_number' => [
+                                    'name' => 'account_number',
+                                    'label' => 'Account Number',
+                                    'required' => true,
+                                ],
+                                'first_name' => [
+                                    'name' => 'first_name',
+                                    'label' => 'First Name',
+                                    'required' => true,
+                                ],
+                                'last_name' => [
+                                    'name' => 'last_name',
+                                    'label' => 'Last Name',
+                                    'required' => true,
+                                ],
+                                'company' => [
+                                    'name' => 'company',
+                                    'label' => 'Company',
+                                    'required' => false,
+                                ],
+                                'address1' => [
+                                    'name' => 'address1',
+                                    'label' => 'Address Line 1',
+                                    'required' => true,
+                                ],
+                                'address2' => [
+                                    'name' => 'address2',
+                                    'label' => 'Address Line 2',
+                                    'required' => false,
+                                ],
+                                'city' => [
+                                    'name' => 'city',
+                                    'label' => 'City',
+                                    'required' => true,
+                                ],
+                                'state_province' => [
+                                    'name' => 'state_province',
+                                    'label' => 'County',
+                                    'required' => true,
+                                ],
+                                'postal_code' => [
+                                    'name' => 'postal_code',
+                                    'label' => 'Post code',
+                                    'required' => true,
+                                ],
+                                'country_code' => [
+                                    'name' => 'country_code',
+                                    'label' => 'Country',
+                                    'required' => true,
+                                    'inputType' => 'country',
+                                ],
+                                'email' => [
+                                    'name' => 'email',
+                                    'label' => 'Email',
+                                    'required' => true,
+                                    'inputType' => 'email',
+                                ],
+                                'phone' => [
+                                    'name' => 'phone',
+                                    'label' => 'Telephone',
+                                    'required' => true,
+                                    'inputType' => 'number',
+                                ],
+                                'agree_to_eula' => [
+                                    'name' => 'agree_to_eula',
+                                    'label' => 'Do you agree to <a href="https://app.shipengine.com/content/integration/FedEx/EULA.pdf" target="_blank">FedEx\'s EULA</a>?',
+                                    'required' => true,
+                                    'inputType' => 'checkbox',
+                                ],
+                            ]
+                        ],
                     ],
                     'defaultBookingOptions' => [
                         'parcels' => 'parcels',
