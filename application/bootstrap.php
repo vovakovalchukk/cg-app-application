d3cf35cd0040e6d1dd51cea88d3645726e0ad32e
Richard Heelin <richardh@channelgrabber.com>
2018-09-24 14:45:41 +0100

    CGIV-9908: Moved the inbuilt php server handler back to index.php as thats the only official entry into the system for web requests

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -9,8 +9,3 @@ chdir(PROJECT_ROOT);
 
 require 'config/env.local.php';
 require 'init_autoloader.php';
-
-// Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
-    return false;
-}
--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
 require_once __DIR__.'/../application/bootstrap.php';
 
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
+
 // Run the application!
 $app = Zend\Mvc\Application::init(require 'config/application.config.php');
 $app->run();
