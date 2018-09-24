767ec38b1b8d1828b37aeebcbab01cb3c4759d27
Richard Heelin <richardh@channelgrabber.com>
2018-09-24 13:48:54 +0100

    CGIV-9908: Updated the status checker to use cached results for faster load times

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -7,3 +7,10 @@ define('PROJECT_ROOT', dirname(__DIR__));
 define('DS', DIRECTORY_SEPARATOR);
 chdir(PROJECT_ROOT);
 
+require 'config/env.local.php';
+require 'init_autoloader.php';
+
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
--- public/index.php
+++ public/index.php
@@ -1,14 +1,6 @@
 <?php
 require_once __DIR__.'/../application/bootstrap.php';
 
-// Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
-    return false;
-}
-
-// Setup autoloading
-require 'init_autoloader.php';
-
 // Run the application!
 $app = Zend\Mvc\Application::init(require 'config/application.config.php');
 $app->run();
