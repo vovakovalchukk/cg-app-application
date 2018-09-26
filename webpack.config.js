476ed1d9cd1b8674d0d47fc020b3209561d029fe
Aaron Baker <aaron.baker@channelgrabber.com>
2018-09-26 12:16:49 +0100

    TAC-227: added support for ES6 stage 2 features.
    These are not officially part of JavaScript yet but will be soon.
    This allows us to use static properties.
    Webpack and babel will handle transpiling these down for us.

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
                 test: /\.jsx$/,
                 loader: 'babel-loader',
                 options: {
-                    presets: ['es2015', 'react']
+                    presets: ['es2015', 'react', 'stage-2']
                 }
             },
             { test: /thenBy/, use: 'exports-loader?firstBy' },
