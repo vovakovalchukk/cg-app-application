282f8a0c1befb05563ff2ff7f02f8d7ff844361a
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2018-09-27 12:58:57 +0100

    TAC-232 - testing react lskeleton loader lib

--- public/channelgrabber/products/jsx/Product/Components/ProductList/Cell/Link.jsx
+++ public/channelgrabber/products/jsx/Product/Components/ProductList/Cell/Link.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Link from 'Product/Components/Link';
 import stateUtility from 'Product/Components/ProductList/stateUtility';
-import styled from 'styled-components';
+import styled from 'styled-components'
+import Skeleton from 'react-skeleton-loader';
 
 // define([], function() {
 //     "use strict";
     
     const StyledLink = styled(Link)([]);
 
+    const LINK_ICON_DIMENSIONS = {
+        width:40,
+        height:40
+    };
+    
     StyledLink.container = styled.div`
            display: flex;
            justify-content: center;
@@ -25,17 +31,24 @@ import styled from 'styled-components';
             //         <div>sdfsdf</div>
             // );
             return (
-                    <StyledLink.container className={this.props.className} {...this.props}>
-                        {!isParentProduct ?
-                            <StyledLink
-                                sku={rowData.sku}
-                                productLinks={productLinks}
-                                linkStatus={rowData.linkStatus}
-                            />
-                            : ''
-                        }
-                    </ StyledLink.container>
+                    <Skeleton
+                        width={LINK_ICON_DIMENSIONS.height+'px'}
+                        height={LINK_ICON_DIMENSIONS.height+'px'}
+                        borderRadius={(LINK_ICON_DIMENSIONS.height / 2)+'px'}
+                    />
             );
+            // return (
+            //         <StyledLink.container className={this.props.className} {...this.props}>
+            //             {!isParentProduct ?
+            //                 <StyledLink
+            //                     sku={rowData.sku}
+            //                     productLinks={productLinks}
+            //                     linkStatus={rowData.linkStatus}
+            //                 />
+            //                 : ''
+            //             }
+            //         </ StyledLink.container>
+            // );
         }
     };
     
