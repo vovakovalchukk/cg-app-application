b8ad34dd9bb4dce778d91ef36c7c15e7fc447551
markduffield <duffield.mark@gmail.com>
2018-10-04 16:19:34 +0100

    CGIV-9606: Moves code from CourierAbstract into storage

--- library/CG/CourierAdapter/Provider/Implementation/Storage/Redis.php
+++ library/CG/CourierAdapter/Provider/Implementation/Storage/Redis.php
@@ -2,12 +2,26 @@
 namespace CG\CourierAdapter\Provider\Implementation\Storage;
 
 use CG\CourierAdapter\StorageInterface;
+use CG\Stdlib\Log\LoggerAwareInterface;
+use CG\Stdlib\Log\LoggerInterface;
+use CG\Stdlib\Log\LogTrait;
 use Predis\Client as PredisClient;
 use CG\Predis\Command\Setnxex;
+use CG\CourierAdapter\ShipmentInterface as Shipment;
+use CG\CourierAdapter\Exception\UserError;
 
-class Redis implements StorageInterface
+class Redis implements StorageInterface, LoggerAwareInterface
 {
+    use LogTrait;
+
+    const LOG_CODE = 'CourierAdapterRedis';
     const KEY_PREFIX = 'CourierAdapter:';
+
+    const CACHE_PARCEL_NUMBER_LOCK_KEY_PREFIX = 'ParcelNumberLock';
+
+    const LOCK_EXPIRY_SECONDS = 60;
+    const LOCK_RETRY_WAIT_SECONDS = 2;
+    const LOCK_MAX_RETRIES = 5;
 
     /** @var PredisClient */
     protected $predisClient;
@@ -54,5 +68,50 @@ class Redis implements StorageInterface
     public function setnxex($key, $time, $expiry)
     {
         return $this->predisClient->setnxex(static::KEY_PREFIX . $key, $time, $expiry);
+    }
+
+    public function lockParcelNumber(Shipment $shipment, string $parcelNumberKey): void
+    {
+        $key = $this->getParcelNumberLockKey($parcelNumberKey);
+        $result = $this->setnxex($key, static::LOCK_EXPIRY_SECONDS, time());
+
+        if ($result) {
+            $this->logDebug('Locked parcelNumber for shipping account %s', [$shipment->getAccount()->getId()], static::LOG_CODE);
+            return;
+        }
+
+        $count = 0;
+        while (!$result) {
+
+            if ($count >= static::LOCK_MAX_RETRIES) {
+                $this->logDebug('Unable to lock parcelNumber for shipping account %s after %d tries. Throwing UserError', [$shipment->getAccount()->getId(), $count], static::LOG_CODE);
+                throw new UserError('Unable to generate new parcel number, please try again.');
+            }
+
+            $count++;
+            $this->logWarning('Unable to lock parcelNumber for shipping account %s, sleeping for %d seconds, attempt %d of %d', [$shipment->getAccount()->getId(), static::LOCK_RETRY_WAIT_SECONDS, $count, static::LOCK_MAX_RETRIES], static::LOG_CODE);
+            sleep(static::LOCK_RETRY_WAIT_SECONDS);
+            $result = $this->setnxex($key, static::LOCK_EXPIRY_SECONDS, time());
+        }
+
+        $this->logDebug('Locked parcelNumber for shipping account %s after %d attempts', [$shipment->getAccount()->getId(), $count], static::LOG_CODE);
+        return;
+    }
+
+    public function unlockParcelNumber(Shipment $shipment, string $parcelNumberKey): void
+    {
+        $this->remove($parcelNumberKey);
+        $this->logDebug('Unlocked parcelNumber for shipping account %d', [$shipment->getAccount()->getId()], static::LOG_CODE);
+    }
+
+    protected function getParcelNumberLockKey(string $parcelNumberKey): string
+    {
+        return static::CACHE_PARCEL_NUMBER_LOCK_KEY_PREFIX . '-' . $parcelNumberKey;
+    }
+
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+        return $this;
     }
 }
