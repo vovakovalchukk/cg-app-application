f3437d9ed28f3fa7fc914029696a10eb12930afb
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2018-10-04 09:23:34 +0100

    TAC-235 - review prep - formatting

--- public/channelgrabber/products/jsx/Product/Components/ProductList/Components/Navbar/Button.jsx
+++ public/channelgrabber/products/jsx/Product/Components/ProductList/Components/Navbar/Button.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Button = (props) => {
     return (
         <span className={"navbar-strip__button u-margin-left-small"} onClick={props.onClick}>
-                <span className={props.iconClass + " left icon icon--medium navbar-strip__button__icon"}>&nbsp;</span>
-                <span className="navbar-strip__button__text">{props.buttonLabel}</span>
-            </span>
+            <span className={props.iconClass + " left icon icon--medium navbar-strip__button__icon"}>&nbsp;</span>
+            <span className="navbar-strip__button__text">{props.buttonLabel}</span>
+        </span>
     );
 };
 
