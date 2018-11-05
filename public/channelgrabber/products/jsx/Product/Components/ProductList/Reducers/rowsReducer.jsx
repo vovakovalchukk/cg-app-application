e486db37b4461d000366328ca9c687b8dac42c80
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2018-11-05 11:23:17 +0000

    TAC-238 - fixed issue where the hader row was scrolling out of view

--- public/channelgrabber/products/jsx/Product/Components/ProductList/Reducers/rowsReducer.jsx
+++ public/channelgrabber/products/jsx/Product/Components/ProductList/Reducers/rowsReducer.jsx
@@ -9,84 +9,29 @@ var initialState = {
 
 var rowsReducer = reducerCreator(initialState, {
     "MODIFY_ZINDEX_OF_ROWS": function(state, action) {
-        let allRows = document.querySelectorAll('.js-row');
-
-        let orderArray = [];
-
-        for (let i = 0; i < allRows.length; i++) {
-            orderArray[i] = allRows.length-i-1;
-        }
-
-        let rowsContainer, parentRow;
-
-
-        for(let j=0; j<allRows.length; j++){
-            let rowIndex = utility.getRowIndexFromRow(allRows[j]);
-            parentRow = allRows[j].parentNode;
-            if (j === 0) {
-                rowsContainer = parentRow.parentNode;
-            }
-             console.log('in loop',{
-                j, rowIndex
-            });
-
-             parentRow.style.zIndex = (allRows.length*2)-rowIndex;
-        }
-
-
-//        for (let i = 0; i < allRows.length; i++) {
-//
-//            let rowIndex = utility.getRowIndexFromRow(allRows[i]);
-//            parentRow = allRows[i].parentNode;
-//
-//            if (i === 0) {
-//                rowsContainer = parentRow.parentNode;
-//            }
-//
-//            console.log('in loop',{
-//                i, rowIndex, parentRow,
-//                'allRows[i]': allRows[i]
-//            });
-//
-//            //todo invert this so it stacks from bottom to top
-//            if(rowIndex===i){
-//                console.log('appending child');
-//
-//
-//                rowsContainer.appendChild(parentRow)
-//            }
-//            break;
-////            parentRows.push(allRows[i].parentNode);
-//        }
-
-        console.log('rowsContainer after order', rowsContainer);
-
-
-
-//
-//
-//        for (let i = 0; i < newOrder.length; i++) {
-//
-//            for (let j = 0; j < newOrder.length; j++) {
-//
-//                if (i === newOrders[j]) {
-//
-//                }
-//
-//            }
-//        }
-//
-//        console.log('parentRows: ', parentRows);
-//
-//        let rowsContainer = parentRows[0].parentNode;
-//
-//        console.log('rowsContainer: ', rowsContainer);
-//
-//        parentRows.forEach(function(row) {
-//            rowsContainer.appendChild(row);
-//        });
+        modifyZIndexOfScrollableRows();
+        modifyZIndexOfHeader();
         return state;
     }
 });
 
 export default rowsReducer
+
+
+function modifyZIndexOfScrollableRows() {
+    let allRows = document.querySelectorAll('.js-row');
+    let rowsContainer, parentRow;
+
+    for (let j = 0; j < allRows.length; j++) {
+        let rowIndex = utility.getRowIndexFromRow(allRows[j]);
+        parentRow = allRows[j].parentNode;
+        if (j === 0) {
+            rowsContainer = parentRow.parentNode;
+        }
+        parentRow.style.zIndex = (allRows.length * 2) - rowIndex;
+    }
+}
+function modifyZIndexOfHeader() {
+    let headerParent = document.querySelector('.public_fixedDataTable_header').parentNode;
+    headerParent.style.zIndex = 50;
+}
