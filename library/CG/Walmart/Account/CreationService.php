250c71a8ea419de82dcbb67d5226b2bc3ed5246d
Aaron Baker <aaron.baker@channelgrabber.com>
2018-11-27 11:11:44 +0000

    TAC-260: Walmart - account creation now tests the connection to Walmart

--- library/CG/Walmart/Account/CreationService.php
+++ library/CG/Walmart/Account/CreationService.php
@@ -2,12 +2,34 @@
 namespace CG\Walmart\Account;
 
 use CG\Account\Client\Entity as AccountEntity;
+use CG\Account\Client\Mapper as AccountMapper;
+use CG\Account\Client\Service as AccountService;
 use CG\Account\CreationServiceAbstract;
+use CG\Account\Credentials\Cryptor;
+use CG\Channel\AccountInterface;
+use CG\Scraper\Client as ScraperClient;
+use CG\Stdlib\Exception\Storage as StorageException;
+use CG\Walmart\Client\Factory as ClientFactory;
 use CG\Walmart\Credentials;
 
 class CreationService extends CreationServiceAbstract
 {
     const CHANNEL = 'walmart';
+
+    /** @var ClientFactory */
+    protected $clientFactory;
+
+    public function __construct(
+        AccountService $accountService,
+        Cryptor $cryptor,
+        AccountMapper $accountMapper,
+        ScraperClient $scraperClient,
+        ClientFactory $clientFactory,
+        AccountInterface $channelAccount = null
+    ) {
+        parent::__construct($accountService, $cryptor, $accountMapper, $scraperClient, $channelAccount);
+        $this->clientFactory = $clientFactory;
+    }
 
     public function getChannelName()
     {
@@ -21,8 +43,20 @@ class CreationService extends CreationServiceAbstract
 
     public function configureAccount(AccountEntity $account, array $params)
     {
-        return $account->setCredentials(
+        $account->setCredentials(
             $this->cryptor->encrypt(new Credentials($params['clientId'], $params['clientSecret']))
         );
+        $this->testAccount($account);
+        return $account;
+    }
+
+    protected function testAccount(AccountEntity $account): void
+    {
+        try {
+            // Requesting a Client will request a token which will test the connection
+            ($this->clientFactory)($account);
+        } catch (StorageException $e) {
+            throw new \InvalidArgumentException('There was a problem connecting up that account. Please check your credentials and try again');
+        }
     }
 }
