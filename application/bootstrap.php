c78d1d5f34300e895f31782ccef1744cdd148939
markduffield <duffield.mark@gmail.com>
2018-12-04 11:11:53 +0000

    CGIV-9873: Adds profiling service to bootstrap

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,4 +1,7 @@
 <?php
+
+use CG\Profiling\Service as ProfilingService;
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
@@ -9,3 +12,13 @@ chdir(PROJECT_ROOT);
 
 require 'config/env.local.php';
 require 'init_autoloader.php';
+
+ProfilingService::startProfiling(
+    'app',
+    [],
+    [
+        ProfilingService::MODE_BASIC => 70,
+        ProfilingService::MODE_PROFILE => 29,
+        ProfilingService::MODE_TRACE => 1
+    ]
+);
