2bf501ba728096edfcfe7754332b9c4dd09726fb
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2019-02-01 15:55:13 +0000

    TAC-325: Render the safe input boxes on the low stock column - no UI functionality on them yet

--- public/channelgrabber/products/jsx/Product/Components/ProductList/Cell/LowStock.jsx
+++ public/channelgrabber/products/jsx/Product/Components/ProductList/Cell/LowStock.jsx
@@ -4,49 +4,72 @@ import LowStockInputs from "../Components/LowStockInputs";
 import portalSettingsFactory from "../Portal/settingsFactory";
 import elementTypes from "../Portal/elementTypes";
 import {StockModeCellContainer, StyledSafeSubmits} from "./StockMode";
+import portalFactory from "../Portal/portalFactory";
 
 class LowStockCell extends React.Component {
     static defaultProps = {};
 
-    getLowStockThresholdDefaults() {
-        return {
-            toggle: this.props.userSettings.lowStockThresholdToggle,
-            value: this.props.userSettings.lowStockThresholdValue
-        };
-    };
-
     render() {
-        const {
-            products,
-            rowIndex,
-            distanceFromLeftSideOfTableToStartOfCell,
-            width
-        } = this.props;
-
-        let portalSettingsForDropdown = portalSettingsFactory.createPortalSettings({
-            elemType: elementTypes.LOW_STOCK_SELECT_DROPDOWN,
-            rowIndex,
-            distanceFromLeftSideOfTableToStartOfCell,
-            width,
-            allRows: this.props.rows.allIds
-        });
-
-        const product = stateUtility.getRowData(products, rowIndex);
+        const product = stateUtility.getRowData(this.props.products, this.props.rowIndex);
 
         if (stateUtility.isParentProduct(product) || !product.stock) {
             return null;
         }
 
         return <StockModeCellContainer>
-            <LowStockInputs
-                product={product}
-                portalSettingsForDropdown={portalSettingsForDropdown}
-                lowStockThreshold={stateUtility.getLowStockThresholdForProduct(product, this.props.stock)}
-                default={this.getLowStockThresholdDefaults()}
-                actions={this.props.actions}
-            />
+            {this.renderLowStockInputs(product, this.getPortalSettingsForDropdown())}
+            {this.renderSubmits()}
         </StockModeCellContainer>
     }
+
+    renderLowStockInputs = (product, portalSettingsForDropdown) => {
+        return <LowStockInputs
+            product={product}
+            portalSettingsForDropdown={portalSettingsForDropdown}
+            lowStockThreshold={stateUtility.getLowStockThresholdForProduct(product, this.props.stock)}
+            default={this.getLowStockThresholdDefaults()}
+            actions={this.props.actions}
+        />
+    };
+
+    renderSubmits = () => {
+        let portalSettings = this.getPortalSettingsForSubmits();
+
+        if (!portalSettings) {
+            return null;
+        }
+
+        return portalFactory.createPortal({
+            portalSettings: portalSettings,
+            Component: StyledSafeSubmits,
+            componentProps: {}
+        });
+    };
+
+    getLowStockThresholdDefaults = () =>  {
+        return {
+            toggle: this.props.userSettings.lowStockThresholdToggle,
+            value: this.props.userSettings.lowStockThresholdValue
+        };
+    };
+
+    getPortalSettingsForDropdown = () => {
+        return this.getPortalSettingsForType(elementTypes.LOW_STOCK_SELECT_DROPDOWN);
+    };
+
+    getPortalSettingsForSubmits = () => {
+        return this.getPortalSettingsForType(elementTypes.INPUT_SAFE_SUBMITS);
+    };
+
+    getPortalSettingsForType = (type) => {
+        return portalSettingsFactory.createPortalSettings({
+            elemType: type,
+            rowIndex: this.props.rowIndex,
+            distanceFromLeftSideOfTableToStartOfCell: this.props.distanceFromLeftSideOfTableToStartOfCell,
+            width: this.props.width,
+            allRows: this.props.rows.allIds
+        });
+    };
 }
 
 export default LowStockCell;
