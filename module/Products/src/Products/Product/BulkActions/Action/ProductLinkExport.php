d4262d00cc8707126d9dc751bd9f71c7b36962f6
markduffield <duffield.mark@gmail.com>
2019-02-07 13:23:59 +0000

    TAC-324: Updates ProductLinkExport to implement interface and add method to determine if the user has the feature flag enabled

--- module/Products/src/Products/Product/BulkActions/Action/ProductLinkExport.php
+++ module/Products/src/Products/Product/BulkActions/Action/ProductLinkExport.php
@@ -1,15 +1,30 @@
 <?php
 namespace Products\Product\BulkActions\Action;
 
+use CG\OrganisationUnit\Service as OrganisationUnitService;
+use CG\User\ActiveUserInterface;
 use CG_UI\View\BulkActions\Action;
+use CG_UI\View\BulkActions\FeatureFlagRequiredInterface;
 use Zend\View\Model\ViewModel;
 use SplObjectStorage;
+use CG\FeatureFlags\Service as FeatureFlagsService;
 
-class ProductLinkExport extends Action
+class ProductLinkExport extends Action implements FeatureFlagRequiredInterface
 {
     const ICON = 'sprite-csv-download-22-black';
+    const FEATURE_FLAG = 'Product Link Export';
+
+    /** @var FeatureFlagsService */
+    protected $featureFlagService;
+    /** @var ActiveUserInterface */
+    protected $activeUserContainer;
+    /** @var OrganisationUnitService */
+    protected $ouService;
 
     public function __construct(
+        FeatureFlagsService $featureFlagService,
+        ActiveUserInterface $activeUserContainer,
+        OrganisationUnitService $ouService,
         ViewModel $urlView,
         array $elementData = [],
         ViewModel $javascript = null,
@@ -17,6 +32,9 @@ class ProductLinkExport extends Action
     ) {
         parent::__construct(static::ICON, "Product Link Export", "productLinkExport", $elementData, $javascript, $subActions);
         $this->setUrlView($urlView)->configure();
+        $this->featureFlagService = $featureFlagService;
+        $this->activeUserContainer = $activeUserContainer;
+        $this->ouService = $ouService;
     }
 
     /**
@@ -46,5 +64,15 @@ class ProductLinkExport extends Action
     {
         $this->urlView = $urlView;
         return $this;
+    }
+
+    public function isFeatureEnabled(): bool
+    {
+        return $this->featureFlagService->isActive(
+            static::FEATURE_FLAG,
+            $this->ouService->getRootOuFromOuId(
+                $this->activeUserContainer->getActiveUserRootOrganisationUnitId()
+            )
+        );
     }
 }
