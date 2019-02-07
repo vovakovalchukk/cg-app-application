101ae95f0bf871fa3e7751349f3e6b0013b4b096
Oliver Stephenson <oliver@channelgrabber.com>
2019-02-07 11:55:18 +0000

    TAC-330: added di config so product link csv imports uses the right bucket

---
+++ config/autoload/global/productLink.php
@@ -0,0 +1,24 @@
+<?php
+
+use CG\Product\Csv\Link\Service as ProductCsvLinkService;
+use CG\FileStorage\S3\Adapter as ProductLinkCsvS3Storage;
+
+return [
+    'di' => [
+        'instance' => [
+            'aliases' => [
+                'ProductLinkS3FileImportAdapter' => ProductLinkCsvS3Storage::class,
+            ],
+            'ProductLinkS3FileImportAdapter' => [
+                'parameters' => [
+                    'location' => 'channelgrabber-reports',
+                ],
+            ],
+            ProductCsvLinkService::class => [
+                'parameters' => [
+                    'exportFileStorage' => 'ProductLinkS3FileImportAdapter',
+                ],
+            ],
+        ],
+    ],
+];
