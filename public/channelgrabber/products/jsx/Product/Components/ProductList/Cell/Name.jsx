2c5e8fc6373188a6826fdf7e39180a8781b359a4
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2019-02-21 10:58:22 +0000

    CGIV-10394 - progress with making name editable

--- public/channelgrabber/products/jsx/Product/Components/ProductList/Cell/Name.jsx
+++ public/channelgrabber/products/jsx/Product/Components/ProductList/Cell/Name.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import Clipboard from 'Clipboard';
 import stateUtility from 'Product/Components/ProductList/stateUtility';
 import styled from 'styled-components';
-import LinesEllipsis from 'react-lines-ellipsis'
 import layoutSettings from 'Product/Components/ProductList/Config/layoutSettings';
-import SafeInputStateless from 'Common/Components/SafeInputStateless';
-
 
 let NameContainer = styled.div`
     display:flex;
@@ -14,10 +11,45 @@ let NameContainer = styled.div`
     padding-left: ${layoutSettings.columnPadding};
     padding-right: ${layoutSettings.columnPadding};
 `;
+let TextAreaContainer = styled.div`
+  width:100%;
+  height:100%;
+  display:flex;
+  align-items: center;
+`;
+let TextArea = styled.textarea`
+    background:none;
+    resize: none;
+    box-sizing: border-box;
+    overflow: hidden;
+    border:none;
+    &:hover{
+        outline-color:rgb(97, 180, 224);
+        outline-offset:-1.81818px;
+        outline-style: solid;
+        outline-width: 0.909091px;
+        border:none;
+    }
+    &:focus{
+        outline-color:rgb(97, 180, 224);
+        outline-offset:-1.81818px;
+        outline-style: solid;
+        outline-width: 0.909091px;
+        outline: 1px solid #61b4e0;
+        border:none;
+    }
+`;
+
+//border: ${props => props.active ? '3px dashed grey' : 'inherit'};
+
+const COLS = 32;
 
 class NameCell extends React.Component {
     static defaultProps = {};
-    state = {};
+    state = {
+        origVal: 'Please write an essay about your favorite DOM element something else is in here somethingsomething else is in here somethingsomething else is in here something',
+        value: 'Please write an essay about your favorite DOM element something else is in here somethingsomething else is in here somethingsomething else is in here something'
+    };
 
     getVariationAttributeArray = (row) => {
         return Object.keys(row.attributeValues).map((key) => {
@@ -40,6 +72,17 @@ class NameCell extends React.Component {
     componentDidMount = () => {
         new Clipboard('div.' + this.getUniqueClassName(), [], 'data-copy');
     };
+    onChange = (e) => {
+        console.log('on change');
+        
+        
+        this.setState({
+            value: e.target.value
+        });
+    };
+    isActive(){
+        return this.state.value !== this.state.origVal;
+    }
     render() {
         const {products, rowIndex} = this.props;
         const row = stateUtility.getRowData(products, rowIndex);
@@ -47,11 +90,39 @@ class NameCell extends React.Component {
 
         let name = this.getVariationName(row, isVariation);
 
-        return (
-            <SafeInputStateless
-                borderless={true}
+        //todo - remove this hack....
 
-            />
+//        return (
+//            <SafeInputStateless
+//                borderless={true}
+//                width={200}
+//            />
+//        )
+        // this is to dummy when a value has been saved
+        if(this.state.value==='something'){
+                    return (
+                        <NameContainer>
+                            <LinesEllipsis
+                                text={this.state.value}
+                                maxLine='2'
+                                ellipsis='...'
+                                trimRight
+                                basedOn='letters'
+                                title={name}
+                            />
+                        </NameContainer>
+                    );
+        }
+        return (
+            <TextAreaContainer>
+                <TextArea
+                    cols={COLS}
+                    rows={2}
+                    value={this.state.value}
+                    onChange={this.onChange}
+                    active={this.isActive()}
+                />
+            </TextAreaContainer>
         )
 //        return (
 //            <NameContainer>
