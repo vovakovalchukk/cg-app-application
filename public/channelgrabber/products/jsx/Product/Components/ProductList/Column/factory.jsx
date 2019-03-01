120f07188ed858bff66b2b8b8e8ee100ce59a34f
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2019-03-01 11:15:42 +0000

    CGIV-10395 - fulfilled ticket requirements

--- public/channelgrabber/products/jsx/Product/Components/ProductList/Column/factory.jsx
+++ public/channelgrabber/products/jsx/Product/Components/ProductList/Column/factory.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
-import FixedDataTable from 'fixed-data-table-2';
 import CellFactory from 'Product/Components/ProductList/Cell/factory';
 import columnKeys from 'Product/Components/ProductList/Column/columnKeys'
 import CellWrapper from 'Product/Components/ProductList/Cell/Wrapper';
+import FixedDataTable from 'fixed-data-table-2';
+import styleVars from 'Product/Components/ProductList/styleVars';
 
 "use strict";
 
-const Column = FixedDataTable.Column;
+const {Column, Cell} = FixedDataTable;
+
+let HeaderCell = styled(Cell)`
+    .public_fixedDataTableCell_cellContent{
+      max-height: ${styleVars.heights.headerHeight}px;  
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+    }
+`;
 
 const columnKeysMetricPropertyMap = {
     [columnKeys.weight]: 'massUnit',
@@ -92,7 +102,9 @@ function getHeaderCellAlignment(column) {
 
 function getHeaderText(column, userSettings) {
     if (!columnKeysMetricPropertyMap[column.key]) {
-        return column.headerText;
+        return <HeaderCell title={column.headerText}>
+            {column.headerText}
+        </HeaderCell>
     }
     let metricProp = columnKeysMetricPropertyMap[column.key];
     let metricString = '(' + userSettings[metricProp] + ')';
