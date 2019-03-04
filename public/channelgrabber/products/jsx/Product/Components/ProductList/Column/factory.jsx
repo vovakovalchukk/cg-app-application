0b168ab8f47bf52f8d47070d2798e37268a08bba
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2019-03-04 15:14:58 +0000

    CGIV-10397 - changes made in column factory to support headerCell factory

--- public/channelgrabber/products/jsx/Product/Components/ProductList/Column/factory.jsx
+++ public/channelgrabber/products/jsx/Product/Components/ProductList/Column/factory.jsx
@@ -3,27 +3,13 @@ import styled from 'styled-components';
 import CellFactory from 'Product/Components/ProductList/Cell/factory';
 import columnKeys from 'Product/Components/ProductList/Column/columnKeys'
 import CellWrapper from 'Product/Components/ProductList/Cell/Wrapper';
-import BulkSelectHeader from 'Product/Components/ProductList/Cell/Header/BulkSelect';
 import FixedDataTable from 'fixed-data-table-2';
-import styleVars from 'Product/Components/ProductList/styleVars';
+import headerCellFactory from 'Product/Components/ProductList/Cell/Header/factory';
 
 "use strict";
 
-const {Column, Cell} = FixedDataTable;
+const {Column} = FixedDataTable;
 
-let HeaderCell = styled(Cell)`
-    .public_fixedDataTableCell_cellContent{
-      max-height: ${styleVars.heights.headerHeight}px;  
-      display: -webkit-box;
-      -webkit-line-clamp: 2;
-      -webkit-box-orient: vertical;
-    }
-`;
-
-const columnKeysMetricPropertyMap = {
-    [columnKeys.weight]: 'massUnit',
-    [columnKeys.dimensions]: 'lengthUnit'
-};
 const columnSpecificPropsMap = {
     stockMode: ['stock', 'rows', 'userSettings', 'scroll'],
     available: ['rows'],
@@ -50,7 +36,7 @@ let columnCreator = function(column, parentProps) {
     createCellWrapper(column);
 
     let CellToRender = getCell(column);
-    let HeaderCellToRender = getHeaderCell(column, parentProps.userSettings);
+    let HeaderCellToRender = headerCellFactory.createHeaderCellContent(column, parentProps.userSettings);
 
     return (<Column
         pureRendering={true}
@@ -110,28 +96,4 @@ function getJustifyContentProp(column) {
 
 function getHeaderCellAlignment(column) {
     return column.align;
-}
-
-function getHeaderTextWithMetricInfo(column, userSettings) {
-    let metricProp = columnKeysMetricPropertyMap[column.key];
-    let metricString = '(' + userSettings[metricProp] + ')';
-    return column.headerText + ' ' + metricString;
-}
-
-function getHeaderCell(column, userSettings) {
-    if (columnKeysMetricPropertyMap[column.key]) {
-        return getHeaderTextWithMetricInfo(column, userSettings);
-    }
-    if (column.key === 'bulkSelect') {
-        return (
-            <HeaderCell>
-                <BulkSelectHeader {...column}/>
-            </HeaderCell>
-        )
-    }
-    return (
-        <HeaderCell title={column.headerText}>
-            {column.headerText}
-        </HeaderCell>
-    );
 }
