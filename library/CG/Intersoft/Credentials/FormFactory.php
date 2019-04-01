f248cb56a760ea88a409f232b2a08f620dc41695
markduffield <duffield.mark@gmail.com>
2019-04-01 09:28:49 +0100

    TAC-386: Adds posting location number has a field we require when the user adds their account

--- library/CG/Intersoft/Credentials/FormFactory.php
+++ library/CG/Intersoft/Credentials/FormFactory.php
@@ -16,6 +16,9 @@ class FormFactory
         $form->add((new Element("userId", [
             "label" => "User ID"
         ]))->setAttribute('required', true));
+        $form->add((new Element("postingLocationNumber", [
+            "label" => "Posting Location Number"
+        ]))->setAttribute('required', true));
         $form->add((new Password("password", [
             "label" => "Password"
         ]))->setAttribute('required', true));
