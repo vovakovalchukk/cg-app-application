1903393097c4fc04402c590fb5773aa2fa576468
markduffield <duffield.mark@gmail.com>
2019-04-17 11:51:30 +0100

    TAC-410: Fixes sprintf

--- library/CG/CourierAdapter/Command/RetrieveConnectionRequestDetails.php
+++ library/CG/CourierAdapter/Command/RetrieveConnectionRequestDetails.php
@@ -18,7 +18,7 @@ class RetrieveConnectionRequestDetails
     {
         try {
             $storedDetails = json_decode($this->storage->get($this->getStorageKey($ouId, $shippingAccountId)));
-            $output->writeln('Stored details for OU %s with shippingAccount %s are as follows:');
+            $output->writeln(sprintf('Stored details for OU %s with shippingAccount %s are as follows:', $ouId, $shippingAccountId));
             $output->writeln(print_r($storedDetails));
         } catch (\Throwable $exception) {
             $output->writeln(sprintf('Unable to retrieve stored details for OU %s with shippingAccount %s', $ouId, $shippingAccountId));
