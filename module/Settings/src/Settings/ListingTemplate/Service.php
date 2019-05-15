5e8e45f794be9867fc8b80f299ba33dbb717e554
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2019-05-15 14:18:59 +0100

    TAC-434 - removed label from the tags as it was decided this would be better handled by the frontend via a map so that a FE dev can easily change it if needed

--- module/Settings/src/Settings/ListingTemplate/Service.php
+++ module/Settings/src/Settings/ListingTemplate/Service.php
@@ -7,25 +7,25 @@ class Service
 
     public function getListingTemplateTags(){
         return [
-            ['id'=> 1, 'tag' => 'title', 'label' => 'title'],
-            ['id'=> 2, 'tag' => 'description', 'label' => 'description'],
-            ['id'=> 2, 'tag' => 'bin', 'label' => 'bin'],
-            ['id'=> 2, 'tag' => 'brand', 'label' => 'brand'],
-            ['id'=> 2, 'tag' => 'manufacturer', 'label' => 'manufacturer'],
-            ['id'=> 2, 'tag' => 'barcode', 'label' => 'barcode'],
-            ['id'=> 2, 'tag' => 'weight', 'label' => 'weight'],
-            ['id'=> 2, 'tag' => 'tax', 'label' => 'tax'],
-            ['id'=> 2, 'tag' => 'condition', 'label' => 'condition'],
-            ['id'=> 2, 'tag' => 'image1', 'label' => 'image1'],
-            ['id'=> 2, 'tag' => 'image2', 'label' => 'image2'],
-            ['id'=> 2, 'tag' => 'image3', 'label' => 'image3'],
-            ['id'=> 2, 'tag' => 'image4', 'label' => 'image4'],
-            ['id'=> 2, 'tag' => 'image5', 'label' => 'image5'],
-            ['id'=> 2, 'tag' => 'image6', 'label' => 'image6'],
-            ['id'=> 2, 'tag' => 'image7', 'label' => 'image7'],
-            ['id'=> 2, 'tag' => 'image8', 'label' => 'image8'],
-            ['id'=> 2, 'tag' => 'image9', 'label' => 'image9'],
-            ['id'=> 2, 'tag' => 'image10', 'label' => 'image10']
+            ['id'=> 1, 'tag' => 'title'],
+            ['id'=> 2, 'tag' => 'description'],
+            ['id'=> 2, 'tag' => 'bin'],
+            ['id'=> 2, 'tag' => 'brand'],
+            ['id'=> 2, 'tag' => 'manufacturer'],
+            ['id'=> 2, 'tag' => 'barcode'],
+            ['id'=> 2, 'tag' => 'weight'],
+            ['id'=> 2, 'tag' => 'tax'],
+            ['id'=> 2, 'tag' => 'condition'],
+            ['id'=> 2, 'tag' => 'image1'],
+            ['id'=> 2, 'tag' => 'image2'],
+            ['id'=> 2, 'tag' => 'image3'],
+            ['id'=> 2, 'tag' => 'image4'],
+            ['id'=> 2, 'tag' => 'image5'],
+            ['id'=> 2, 'tag' => 'image6'],
+            ['id'=> 2, 'tag' => 'image7'],
+            ['id'=> 2, 'tag' => 'image8'],
+            ['id'=> 2, 'tag' => 'image9'],
+            ['id'=> 2, 'tag' => 'image10']
         ];
     }
 
@@ -35,21 +35,21 @@ class Service
             [
                 'id' => 1,
                 'name' => 'template1',
-                'content' => "<h1>Template 1 Title</h1>
+                'html' => "<h1>Template 1 Title</h1>
                     some content in the template
                 "
             ],
             [
                 'id' => 2,
                 'name' => 'template2',
-                'content' => "<h1>Template 2 Title</h1>
+                'html' => "<h1>Template 2 Title</h1>
                     some content in the template
                 "
             ],
             [
                 'id' => 3,
                 'name' => 'template3',
-                'content' => "<h1>Template 3 Title</h1>
+                'html' => "<h1>Template 3 Title</h1>
                     some content in the template
                 "
             ]
