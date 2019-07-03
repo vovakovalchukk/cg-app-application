1227064e0bd2277e89544b6621391fa0024dab33
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2019-07-03 11:33:48 +0100

    TAC-448 - added initial functionality for the bulk action

--- public/channelgrabber/orders/jsx/Components/TemplateExportBulkAction.jsx
+++ public/channelgrabber/orders/jsx/Components/TemplateExportBulkAction.jsx
@@ -1,14 +1,44 @@
 import React, {useState} from 'react';
+import ButtonSelect from 'Common/Components/ButtonSelect';
+import OrdersBulkActionAbstract from 'Orders/js-vanilla/OrdersBulkActionAbstract';
 
 const TemplateExportBulkAction = ({pdfExportOptions}) => {
+    let OrdersBulkActions = new OrdersBulkActionAbstract();
+
+    //todo -remove this hack
+    pdfExportOptions.forEach((option,index)=>{
+        if(index === 2){
+            option.favourite=true;
+        }
+    });
+    console.log('pdfExportOptions (post hack): ', JSON.stringify(pdfExportOptions,null,1));
 
 
-    console.log('props in TemplateExportBNulk ACs pdfExportOptions: ', pdfExportOptions);
+    let options = prepareOptions(pdfExportOptions);
+
+    console.log('options (after prepared): ', JSON.stringify(options,null,1));
 
 
+    return (<ButtonSelect
+        options={options}
+        buttonTitle={'Template Export'}
+        spriteClass={'sprite-invoice-22-black'}
+        onButtonClick={requestTemplateExport}
+    />);
 
-    return <div>yo in here</div>
+    function requestTemplateExport(){
+        console.log('in requestTemplateExport');
+        let orderIds = OrdersBulkActions.getOrders();
+        console.log('orderIds: ', orderIds);
+    }
 
+    function prepareOptions(pdfExportOptions){
+        let result = pdfExportOptions.sort((a,b) => {
+           return b.favourite - a.favourite;
+        });
+
+        return result;
+    }
 };
 
 export default TemplateExportBulkAction;
