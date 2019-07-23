024040cf127ab718cefde04e3cf0d7508876c9a4
Aaron Baker <aaron.baker@channelgrabber.com>
2019-07-23 09:59:37 +0100

    MIG-11: Listing Templates - built on the work from MIG-10 and replaced the dummy product tags with the real ones

--- module/Settings/src/Settings/ListingTemplate/Service.php
+++ module/Settings/src/Settings/ListingTemplate/Service.php
@@ -6,6 +6,7 @@ use CG\Listing\Template\Entity as ListingTemplate;
 use CG\Listing\Template\Filter as ListingTemplateFilter;
 use CG\Listing\Template\Service as ListingTemplateService;
 use CG\Stdlib\Exception\Runtime\NotFound;
+use CG\Template\TagReplace\Product as ProductTagReplacer;
 use CG\User\ActiveUserInterface;
 
 class Service
@@ -16,36 +17,26 @@ class Service
     protected $listingTemplateService;
     /** @var ActiveUserInterface */
     protected $activeUserContainer;
+    /** @var ProductTagReplacer */
+    protected $productTagReplacer;
 
-    public function __construct(ListingTemplateService $listingTemplateService, ActiveUserInterface $activeUserContainer)
-    {
+    public function __construct(
+        ListingTemplateService $listingTemplateService,
+        ActiveUserInterface $activeUserContainer,
+        ProductTagReplacer $productTagReplacer
+    ) {
         $this->listingTemplateService = $listingTemplateService;
         $this->activeUserContainer = $activeUserContainer;
+        $this->productTagReplacer = $productTagReplacer;
     }
 
     public function getListingTemplateTags(): array
     {
-        return [
-            ['id'=> 1, 'tag' => 'title'],
-            ['id'=> 2, 'tag' => 'description'],
-            ['id'=> 2, 'tag' => 'bin'],
-            ['id'=> 2, 'tag' => 'brand'],
-            ['id'=> 2, 'tag' => 'manufacturer'],
-            ['id'=> 2, 'tag' => 'barcode'],
-            ['id'=> 2, 'tag' => 'weight'],
-            ['id'=> 2, 'tag' => 'tax'],
-            ['id'=> 2, 'tag' => 'condition'],
-            ['id'=> 2, 'tag' => 'image1'],
-            ['id'=> 2, 'tag' => 'image2'],
-            ['id'=> 2, 'tag' => 'image3'],
-            ['id'=> 2, 'tag' => 'image4'],
-            ['id'=> 2, 'tag' => 'image5'],
-            ['id'=> 2, 'tag' => 'image6'],
-            ['id'=> 2, 'tag' => 'image7'],
-            ['id'=> 2, 'tag' => 'image8'],
-            ['id'=> 2, 'tag' => 'image9'],
-            ['id'=> 2, 'tag' => 'image10']
-        ];
+        $tagOptions = [];
+        foreach ($this->productTagReplacer->getAvailableTags() as $name => $value) {
+            $tagOptions[] = ['name' => $name, 'value' => $value];
+        }
+        return $tagOptions;
     }
 
     public function getUsersTemplates(): ListingTemplateCollection
