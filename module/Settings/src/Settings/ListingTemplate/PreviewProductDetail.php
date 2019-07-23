54936a1e7c7235cb7bfc2f6f3b8281e8e32ff7ec
Aaron Baker <aaron.baker@channelgrabber.com>
2019-07-23 15:02:22 +0100

    MIG-11: Listing Templates - added PreviewProduct and PreviewProductDetail with example data for use in rendering the previews of templates

---
+++ module/Settings/src/Settings/ListingTemplate/PreviewProductDetail.php
@@ -0,0 +1,47 @@
+<?php
+namespace Settings\ListingTemplate;
+
+use CG\Product\Detail\Entity as ProductDetail;
+
+class PreviewProductDetail extends ProductDetail
+{
+    const WEIGHT = 42;
+    const WIDTH = 70;
+    const HEIGHT = 74;
+    const LENGTH = 120;
+    const DESCRIPTION = 'This five-piece garden furniture set, including four chairs and a dining table, is the ideal furnishing for any garden or patio. Stylish, high-backed chairs and a generous, comfortable armrests make this a great place to serve dinner, enjoy a coffee or drinks, or simply relax in comfort and make the most of the summer with this fantastic set';
+    const EAN = '5421085046166';
+    const UPC = '89268500100';
+    const ISBN = '9781781101544';
+    const BRAND = 'Woodard';
+    const MPN = '5GARDINSET';
+    const PRICE = 129.99;
+    const COST = 69.99;
+    const CONDITION = 'New';
+
+    public function __construct($organisationUnitId)
+    {
+        parent::__construct(
+            $organisationUnitId,
+            PreviewProduct::SKU,
+            static::WEIGHT,
+            static::WIDTH,
+            static::HEIGHT,
+            static::LENGTH,
+            null,
+            static::DESCRIPTION,
+            static::EAN,
+            static::BRAND,
+            static::MPN,
+            null,
+            static::PRICE,
+            static::COST,
+            static::CONDITION,
+            [],
+            static::UPC,
+            static::ISBN,
+            null,
+            false
+        );
+    }
+}
