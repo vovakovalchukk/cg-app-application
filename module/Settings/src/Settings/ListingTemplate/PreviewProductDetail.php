4f6a7a567699519b31ff766f83c9d24cb07272dd
Aaron Baker <aaron.baker@channelgrabber.com>
2019-07-25 15:22:37 +0100

    MIG-11: Listing Templates - tweak to the `description` copy of the PreviewProductDetail.
    There was a gramatical error.

--- module/Settings/src/Settings/ListingTemplate/PreviewProductDetail.php
+++ module/Settings/src/Settings/ListingTemplate/PreviewProductDetail.php
@@ -9,7 +9,7 @@ class PreviewProductDetail extends ProductDetail
     const WIDTH = 0.7;
     const HEIGHT = 0.74;
     const LENGTH = 1.20;
-    const DESCRIPTION = 'This five-piece garden furniture set, including four chairs and a dining table, is the ideal furnishing for any garden or patio. Stylish, high-backed chairs and a generous, comfortable armrests make this a great place to serve dinner, enjoy a coffee or drinks, or simply relax in comfort and make the most of the summer with this fantastic set';
+    const DESCRIPTION = 'This five-piece garden furniture set, including four chairs and a dining table, is the ideal furnishing for any garden or patio. Stylish, high-backed chairs with generous, comfortable armrests make this a great place to serve dinner, enjoy a coffee or drinks, or simply relax in comfort and make the most of the summer with this fantastic set';
     const EAN = '5421085046166';
     const UPC = '89268500100';
     const ISBN = '9781781101544';
