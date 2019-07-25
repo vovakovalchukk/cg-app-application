22f60aca1aed19117174d59ed8f2fa63985bb303
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2019-07-25 09:21:13 +0100

    TAC-448 - refactoring and formatting for better readability

--- public/channelgrabber/settings/js-vanilla/InvoiceOverview/TemplateComponent.jsx
+++ public/channelgrabber/settings/js-vanilla/InvoiceOverview/TemplateComponent.jsx
@@ -1,44 +1,58 @@
 import React from 'react';
 import TemplateActions from 'InvoiceOverview/TemplateActions/TemplateActions';
 
+const THUMBNAIL_RELEVANT_ACTION_NAMES = ['create', 'edit'];
+
 const Thumbnail = props => {
     let {imageUrl, actions} = props;
     if (!imageUrl) {
         return null;
     }
 
-    //todo - this is no good this shouldnt be read from properties
-    let thumbnailProps = {};
-    thumbnailProps['src'] = imageUrl;
-    for (let actionName in actions) {
-        let currentAction = actions[actionName];
-        if (currentAction.name !== 'create' || currentAction.name !== 'edit') {
-            continue;
-        }
-        thumbnailProps['href'] = currentAction.linkHref;
-        if (currentAction.linkTarget) {
-            thumbnailProps['linkTarget'] = currentAction.linkTarget;
-        }
-    }
+    let linkAttributes = getThumbnailLinkAttributesFromActions();
 
-    return <img {...thumbnailProps}/>
+    return <a {...linkAttributes}>
+        <img src={imageUrl} alt='thumbnail'/>
+    </a>;
+
+    function getThumbnailLinkAttributesFromActions() {
+        let attributes = {};
+        attributes['src'] = imageUrl;
+        for (let actionName in actions) {
+            let currentAction = actions[actionName];
+            attributes['href'] = currentAction.linkHref;
+            if (currentAction.linkTarget) {
+                attributes['linkTarget'] = currentAction.linkTarget;
+            }
+        }
+        return attributes;
+    }
 };
 
-class TemplateComponent extends React.Component {
-    render() {
-        let {id, imageUrl, templateActions} = this.props;
-        return (<div className={"invoice-template-element"}>
-            <div className={'invoice-template-thumb'}>
-                <Thumbnail imageUrl={imageUrl} actions={templateActions}/>
-                <div className={'template-overview-actions-container'}>
-                    <TemplateActions actions={templateActions} templateId={id}/>
-                </div>
+const TemplateComponent = props => {
+    let {id, imageUrl, templateActions} = props;
+
+    let thumbnailActions = getThumbnailActions(templateActions);
+
+    return (<div className={"invoice-template-element"}>
+        <div className={'invoice-template-thumb'}>
+            <Thumbnail imageUrl={imageUrl} actions={thumbnailActions}/>
+            <div className={'template-overview-actions-container'}>
+                <TemplateActions actions={templateActions} templateId={id}/>
             </div>
-            <div className={'invoice-template-name'}>
-                {this.props.name}
-            </div>
-        </div>);
+        </div>
+        <div className={'invoice-template-name'}>
+            {props.name}
+        </div>
+    </div>);
+
+    function getThumbnailActions(actions) {
+        return Object.keys(actions).filter(actionKey => {
+            return THUMBNAIL_RELEVANT_ACTION_NAMES.includes(actions[actionKey].name.toLowerCase());
+        }).map(actionKey => {
+            return actions[actionKey];
+        });
     }
-}
+};
 
 export default TemplateComponent;
