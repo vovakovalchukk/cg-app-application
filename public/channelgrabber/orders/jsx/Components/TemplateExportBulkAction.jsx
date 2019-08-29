17ba36f9872ece6be3d110be05becf40826dddb2
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2019-08-29 08:49:32 +0100

    TAC-468 - removed debug and made the initial notice appear before the check as it should

--- public/channelgrabber/orders/jsx/Components/TemplateExportBulkAction.jsx
+++ public/channelgrabber/orders/jsx/Components/TemplateExportBulkAction.jsx
@@ -13,41 +13,7 @@ const TemplateExportBulkAction = ({pdfExportOptions}) => {
         options = appendDefaultInvoiceOption(options)
     }
 
-    // todo - remove this before ticket finishes. This is to speed up dev process
-    // medium sized request
-//    requestTemplateExport( ["2", "3", "4", "5", "6"], ["7-1000", "7-1172", "7-1324", "7-1623", "7-1795", "7-1833", "7-400", "7-602"]);
-    // larger sized request
-//    requestTemplateExport( [
-//        "2",
-//        "3",
-//        "4",
-//        "5",
-//        "6",
-//        "7",
-//        "8",
-//        "9",
-//        "10",
-//        "11",
-//        "12",
-//        "default-formsPlusFPD-1_OU2",
-//        "default-formsPlusFPS-15_OU2"
-//    ], [
-//        "7-1000",
-//        "7-1172",
-//        "7-1324",
-//        "7-1623",
-//        "7-1795",
-//        "7-1833",
-//        "7-400",
-//        "7-602",
-//        "7-826",
-//        "7-1200",
-//        "7-1359",
-//        "7-151",
-//        "7-394",
-//        "7-591",
-//        "7-628"
-//    ]);
+    const baseMessage = 'Generating templates...';
 
     return (<ButtonMultiSelect
         options={options}
@@ -58,7 +24,6 @@ const TemplateExportBulkAction = ({pdfExportOptions}) => {
 
     async function requestTemplateExport(templateIds, orders) {
         orders = orders || BulkActionService.getSelectedOrders();
-        const baseMessage = 'Generating templates...';
         const maxProgress = templateIds.length * orders.length;
 
         if (!Array.isArray(templateIds) ||
@@ -73,18 +38,20 @@ const TemplateExportBulkAction = ({pdfExportOptions}) => {
             n.error('Templates could not be generated successfully.')
         };
         try {
+            n.notice(baseMessage);
+
             const {guid} = await progressService.callCheck(
                 '/orders/pdf-export/check',
                 templateIds,
                 orders
             );
-            n.notice(baseMessage);
 
             progressService.runProgressCheck({
                 guid,
                 url: "/orders/pdf-export/progress",
                 maxProgress,
                 successCallback: () => {
+                    n.success('Templates generated successfully.');
                 },
                 progressCallback: progressCount => {
                     n.notice(`${baseMessage} ${progressCount} out of ${maxProgress} complete.`, false, false, false);
@@ -103,7 +70,6 @@ const TemplateExportBulkAction = ({pdfExportOptions}) => {
                 if (response.status !== 200) {
                     return handleError();
                 }
-                n.success('Templates generated successfully.');
             });
         } catch (err) {
             handleError();
