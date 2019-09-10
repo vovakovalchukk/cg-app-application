fc705cc317d26ddacfa54217e1a12dda16f9399e
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2019-09-10 13:09:46 +0100

    TAC-511 - storage table updated with columns specified by Dave

--- public/channelgrabber/settings/js-vanilla/InvoiceDesigner/Template/Storage/Table.js
+++ public/channelgrabber/settings/js-vanilla/InvoiceDesigner/Template/Storage/Table.js
@@ -1,10 +1,77 @@
 define([], function() {
-    const TableStorage = (function() {
+    const allColumns = [
+        {
+            "id": "quantityOrdered",
+            default: true,
+            "cellPlaceholder": "2",
+            displayText: "QTY",
+            "optionText": "Quantity Ordered"
+        },
+        {
+            "id": "skuOrdered",
+            default: true,
+            displayText: 'Item #',
+            "optionText": "Sku Ordered",
+            "cellPlaceholder": "BATTERY10pc"
+        },
+        {
+            "id": "unitPriceIncVAT",
+            default: true,
+            "displayText": "Price",
+            "optionText": "Unit Price inc VAT",
+            "cellPlaceholder": "£6"
+        },
+        {
+            "id": "descriptionChannel",
+            default: true,
+            displayText: "Description",
+            "optionText": "Description-Channel",
+            "cellPlaceholder": "10 x Duracell 1.5V Alkaline Batteries Long Life"
+        },
 
+        {
+            "id": "lineTotalIncVAT",
+            default: true,
+            displayText: "Total",
+            "optionText": "Line Total inc VAT",
+            "cellPlaceholder": "£12"
+        },
+        {
+            "id": "descriptionInternal",
+            "optionText": "Description-Internal",
+            "cellPlaceholder": "10 Duracell Batteries"
+        },
+        {
+            "id": "unitPriceExVAT",
+            "optionText": "Unit Price ex VAT",
+            "cellPlaceholder": "£5"
+        },
+        {
+            "id": "lineTotalExVAT",
+            "optionText": "Line Total ex VAT",
+            "cellPlaceholder": "£10"
+        },
+        {
+            "id": "vatRateApplied",
+            "optionText": "VAT Rate Applied",
+            "cellPlaceholder": "20%"
+        },
+        {
+            "id": "discount",
+            "optionText": "Discount",
+            "cellPlaceholder": "£0"
+        },
+        {
+            "id": "pickingLocation",
+            "optionText": "Picking Location",
+            "cellPlaceholder": "Warehouse 1, Aisle 1, Shelf 1"
+        }
+    ];
+
+    const TableStorage = (function() {
         return {
             getColumns,
             getDefaultColumns: function() {
-                // todo - to be completed in TAC-513
                 return getColumns().map(column => {
                     if (!column.default) {
                         return;
@@ -12,37 +79,12 @@ define([], function() {
                     return column;
                 })
             },
-            getTotals: function() {
-
-            },
-            getSortBy: function() {
-
-            }
-        }
+            getTotals: function() {},
+            getSortBy: function() {}
+        };
 
         function getColumns() {
-            return [
-                {
-                    id: 'quantity',
-                    position: 1,
-                    default: true,
-                    headerText: 'Quantity',
-                    cellPlaceholder: '2'
-                }, {
-                    id: 'description',
-                    position: 2,
-                    default: true,
-                    headerText: 'Description',
-                    cellPlaceholder: 'Duracell Battery 10pc'
-                },
-                {
-                    id: 'price',
-                    position: 3,
-                    default: true,
-                    headerText: 'Price',
-                    cellPlaceholder: '£4.00'
-                }
-            ]
+            return allColumns
         }
     }());
 
