4bf6cb8b48dbd48adf8398725ac9c15f94a7b158
Oliver Stephenson <oliver@channelgrabber.com>
2019-10-29 11:23:36 +0000

    TAC-506: replaced usage check for order exporter to use access module

--- module/Settings/src/Settings/Controller/ExportController.php
+++ module/Settings/src/Settings/Controller/ExportController.php
@@ -10,10 +10,9 @@ use CG\Stdlib\Exception\Runtime\NotFound;
 use CG\User\Entity as User;
 use CG\User\OrganisationUnit\Service as UserOUService;
 use CG\Zend\Stdlib\Http\FileResponse;
+use CG_Access\UsageExceeded\Service as AccessUsageExceededService;
 use CG_UI\View\Prototyper\JsonModelFactory;
 use CG_UI\View\Prototyper\ViewModelFactory;
-use CG_Usage\Exception\Exceeded as UsageExceeded;
-use CG_Usage\Service as UsageService;
 use Orders\Order\Csv\Service as OrderCsvService;
 use Products\Product\Csv\Exporter as ProductCsvService;
 use Settings\Module;
@@ -38,8 +37,6 @@ class ExportController extends AdvancedController
     protected $userOUService;
     /** @var OrderCsvService $orderCsvService */
     protected $orderCsvService;
-    /** @var UsageService */
-    protected $usageService;
     /** @var JsonModelFactory */
     protected $jsonModelFactory;
     /** @var ProductCsvService */
@@ -48,25 +45,27 @@ class ExportController extends AdvancedController
     protected $featureFlagsService;
     /** @var AccountService */
     protected $accountService;
+    /** @var AccessUsageExceededService */
+    protected $accessUsageExceededService;
 
     public function __construct(
         ViewModelFactory $viewModelFactory,
         UserOUService $userOUService,
         OrderCsvService $orderCsvService,
-        UsageService $usageService,
         JsonModelFactory $jsonModelFactory,
         ProductCsvService $productCsvService,
         FeatureFlagsService $featureFlagsService,
-        AccountService $accountService
+        AccountService $accountService,
+        AccessUsageExceededService $accessUsageExceededService
     ) {
         $this->viewModelFactory = $viewModelFactory;
         $this->userOUService = $userOUService;
         $this->orderCsvService = $orderCsvService;
-        $this->usageService = $usageService;
         $this->jsonModelFactory = $jsonModelFactory;
         $this->productCsvService = $productCsvService;
         $this->featureFlagsService = $featureFlagsService;
         $this->accountService = $accountService;
+        $this->accessUsageExceededService = $accessUsageExceededService;
     }
 
     public function exportAction()
@@ -170,9 +169,7 @@ class ExportController extends AdvancedController
 
     public function exportCheckAction()
     {
-        if ($this->usageService->hasUsageBeenExceeded()) {
-            throw new UsageExceeded();
-        }
+        $this->accessUsageExceededService->checkUsage();
 
         $guid = uniqid('', true);
         $this->orderCsvService->startProgress($guid);
@@ -215,4 +212,4 @@ class ExportController extends AdvancedController
             ->setLimit('all')
             ->setOrganisationUnitId($this->getActiveUser()->getOuList());
     }
-} 
+}
