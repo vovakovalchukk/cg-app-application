511e18ad0a165929204a6180213ee321c35b4a5f
markduffieldCG <mark.duffield@channelgrabber.com>
2019-10-29 09:51:25 +0000

    CGIV-11021: Corrects bracket

--- library/CG/Intersoft/Request/Shipment/Create.php
+++ library/CG/Intersoft/Request/Shipment/Create.php
@@ -121,7 +121,7 @@ class Create extends PostAbstract
 
         $deliveryAddress = $this->reformatDestinationAddressLines($deliveryAddress);
 
-        $destination->addChild('destinationContactName', $this->sanitiseString($deliveryAddress->getFirstName() . ' ' . $deliveryAddress->getLastName()), 40);
+        $destination->addChild('destinationContactName', $this->sanitiseString($deliveryAddress->getFirstName() . ' ' . $deliveryAddress->getLastName(), 40));
         $destination->addChild('destinationAddressLine1', $this->sanitiseString($deliveryAddress->getLine1()));
         $destination->addChild('destinationAddressLine2', $this->sanitiseString($deliveryAddress->getLine2()));
         $destination->addChild('destinationAddressLine3', $this->sanitiseString($deliveryAddress->getLine3()));
