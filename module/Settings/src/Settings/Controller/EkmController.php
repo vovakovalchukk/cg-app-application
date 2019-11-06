178aa561ffb6e400d688635fc774e347c6d27e86
Aaron Baker <aaron.baker@channelgrabber.com>
2019-11-06 15:27:06 +0000

    TAC-543: EKM - added controller action for the return URL used by EKM once a user has approved our access to the new REST API
    This then finished off the Account connection process on our side.

--- module/Settings/src/Settings/Controller/EkmController.php
+++ module/Settings/src/Settings/Controller/EkmController.php
@@ -1,12 +1,20 @@
 <?php
 namespace Settings\Controller;
 
-use Settings\Module;
+use CG\Account\Shared\Entity as Account;
 use CG\Channel\Type as ChannelType;
+use CG\Ekm\Account\Connector\Rest as RestAccountConnector;
+use CG\Stdlib\Log\LoggerAwareInterface;
+use CG\Stdlib\Log\LogTrait;
+use Settings\Module;
 
-class EkmController extends ChannelControllerAbstract
+class EkmController extends ChannelControllerAbstract implements LoggerAwareInterface
 {
-    const ROUTE_AJAX = 'ajax';
+    use LogTrait;
+
+    public const ROUTE_AJAX = 'ajax';
+    protected const LOG_CODE = 'EkmController';
+    protected const ERROR_DENIED = 'access_denied';
 
     public function indexAction()
     {
@@ -31,5 +39,49 @@ class EkmController extends ChannelControllerAbstract
         $url = $this->plugin('url')->fromRoute($routeName, ["account" => $accountEntity->getId(), "type" => ChannelType::SALES]);
         $view->setVariable('redirectUrl', $url);
         return $view;
+    }
+
+    /**
+     * EKM redirects the user back here after an authentication attempt
+     * either with a `code` or an `error` and always with 'state'
+     */
+    public function connectRestAccountAction(): void
+    {
+        $params = $this->params()->fromQuery();
+        if (isset($params['error']) && $params['error'] != '') {
+            $this->logConnectRestAccountError($params['error']);
+            $this->redirectToSalesAccountsPage();
+            return;
+        }
+        $currentUri = $this->getRequest()->getUri()->setQuery([]);
+        $params['redirectUri'] = (string)$currentUri;
+        $accountId = RestAccountConnector::getAccountIdFromState($params['state'] ?? '');
+        $account = $this->getAccountCreationService()->connectAccount(
+            $this->getActiveUserContainer()->getActiveUser()->getOrganisationUnitId(),
+            $accountId,
+            $params
+        );
+        $this->redirectToAccountPage($account);
+    }
+
+    protected function redirectToSalesAccountsPage(): void
+    {
+        $route = implode('/', [Module::ROUTE, ChannelController::ROUTE, ChannelController::ROUTE_CHANNELS]);
+        $this->redirect()->toRoute($route, ['type' => ChannelType::SALES]);
+    }
+
+    protected function redirectToAccountPage(Account $account): void
+    {
+        $route = implode('/', [Module::ROUTE, ChannelController::ROUTE, ChannelController::ROUTE_CHANNELS, ChannelController::ROUTE_ACCOUNT]);
+        $this->redirect()->toRoute($route, ['type' => ChannelType::SALES, 'account' => $account->getId()]);
+    }
+
+    protected function logConnectRestAccountError(string $error): void
+    {
+        if ($error == static::ERROR_DENIED) {
+            $this->logNotice('EKM auth attempt was denied by the user', [], [static::LOG_CODE, 'Auth', 'Denied']);
+        } else {
+            $this->logWarning('EKM auth attempt returned an error: %s', [$error], [static::LOG_CODE, 'Auth', 'Error']);
+        }
     }
 }
