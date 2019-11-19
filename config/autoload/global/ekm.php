52ab003fd029630df35e76b5fcc74db32bc31070
Aaron Baker <aaron.baker@channelgrabber.com>
2019-11-19 14:33:08 +0000

    TAC-547: brought in cg-ekm library with changes to listing imports
    Updated DI config to support these changes.

--- config/autoload/global/ekm.php
+++ config/autoload/global/ekm.php
@@ -1,6 +1,9 @@
 <?php
 
+use CG\Ekm\Client\Factory as EkmClientFactory;
+use CG\Ekm\Client\Soap as EkmSoapClient;
 use CG\Ekm\Gearman\Proxy\ImportTaxRates;
+use CG\Ekm\Product\Downloader\Soap as EkmSoapProductDownloader;
 
 return [
     'di' => [
@@ -9,7 +12,22 @@ return [
                 'parameters' => [
                     'gearmanClient' => 'ekmGearmanClient'
                 ]
-            ]
+            ],
+            EkmSoapClient::class => [
+                'parameters' => [
+                    'cryptor' => 'ekm_cryptor',
+                ],
+            ],
+            EkmClientFactory::class => [
+                'parameters' => [
+                    'cryptor' => 'ekm_cryptor',
+                ]
+            ],
+            EkmSoapProductDownloader::class => [
+                'parameters' => [
+                    'cryptor' => 'ekm_cryptor',
+                ]
+            ],
         ],
     ],
 ];
