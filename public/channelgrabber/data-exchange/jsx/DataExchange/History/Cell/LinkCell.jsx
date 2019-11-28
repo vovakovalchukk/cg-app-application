68b2323e096db4ac5555552531cf06f7d3b22361
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2019-11-28 11:41:26 +0000

    MIG-43 - styling change - used flex for the dynamic spacing of cell contents

--- public/channelgrabber/data-exchange/jsx/DataExchange/History/Cell/LinkCell.jsx
+++ public/channelgrabber/data-exchange/jsx/DataExchange/History/Cell/LinkCell.jsx
@@ -1,24 +1,38 @@
 import React from 'react';
+import DownloadIcon from 'Common/Components/DownloadIcon';
+import styled from 'styled-components';
+
+const StyledDownloadIcon = styled(DownloadIcon)`
+  font-size: 2em;
+`;
+
+const ContentFlex = styled.div`
+  display: inline-flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const FlexChild = styled.span`
+  margin: 5px;
+`;
 
 const LinkCell = (props) => {
     let {rowData, column} = props;
 
     let value = typeof column.getValue === 'function' ?
-        column.getValue(rowData) : rowData[column.key];
-
-    if (value === null || typeof value === "undefined") {
-        return null;
-    }
-
+        column.getValue(rowData) : rowData[column.key] || null;
+    
     let link = typeof column.getLink === 'function' ?
         column.getLink(rowData) : '';
 
-    return (<div>
-        {value}
-        {link && <a className="u-margin-left-small" href={link}>
-            link
-        </a>}
-    </div>);
+    return (<ContentFlex>
+        {value && <FlexChild>{value}</FlexChild>}
+        {link && <FlexChild>
+            <a href={link}>
+                <StyledDownloadIcon />
+            </a>
+        </FlexChild>}
+    </ContentFlex>);
 };
 
 export default LinkCell;
