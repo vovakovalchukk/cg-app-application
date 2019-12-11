1e0f1a008120a0ba3ee9db8969cc9926dcb0109b
= <johncranston@cloudcommercepro.com>
2019-12-11 11:10:01 +0000

    TAC-567-initialised app.js

---
+++ public/channelgrabber/messages/jsx/App.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+
+const App = (props) => {
+    console.log(props);
+
+    useEffect(() => {
+        console.log('component mounted');
+        
+    }, []);
+
+    return (
+        <div className="u-width-100pc u-display-flex">
+            <div id="Sidebar" className="u-flex-1">sidebar</div>
+            <div id="Main" className="u-flex-5">main</div>
+        </div>
+    );
+
+};
+
+export default App;
