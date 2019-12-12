b326d03a6e601cfdd0667e7dd8c0918c6c0adc41
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2019-12-12 13:52:32 +0000

    MIG-43 - refactored the Table component into cg-common for reuse in other apps

--- public/channelgrabber/data-exchange/jsx/DataExchange/History/App.jsx
+++ public/channelgrabber/data-exchange/jsx/DataExchange/History/App.jsx
@@ -1,11 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import historyFetch from './Service/historyFetch';
-import Paginator from "Common/Components/Pagination/Paginator";
 import allColumns from 'DataExchange/History/Column/allColumns';
 import styled from 'styled-components';
-import Skeleton from 'react-skeleton-loader';
+import Table from 'Common/Components/Table';
 
-const Table = styled.table`
+const StyledTable = styled.table`
     width: calc(100% + 40px);
     margin-left: calc(-20px);
     margin-right: calc(-20px);
@@ -24,89 +23,21 @@ const HistoryApp = (props) => {
         historyFetch(pagination, setData);
     }, []);
 
-    const maxPages = 100;
-    
-    return (<div>
-        <Table className={"u-margin-top-small"}>
-            <thead>
-                <tr>
-                    {renderCells(data ? data[0] : {} , (Cell, column) => (
-                        <Th key={`header-${column.key}`}>
-                            <div>
-                                {column.label}
-                            </div>
-                        </Th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {renderRows(data, (rowData) => (
-                    <tr key={rowData.id}>
-                        {renderCells(rowData, (Cell, column) => (
-                            <td key={`${rowData.id}-${column.key}`}>
-                                <Cell
-                                    rowData={rowData}
-                                    column={column}
-                                    setRowValue={setRowValue}
-                                    // width & height props only used for Skeleton components
-                                    width={`${getRandomNumber(40, 120)}px`}
-                                    height={'100ox'}
-                                />
-                            </td>
-                        ))}
-                    </tr>
-                ))}
-            </tbody>
-        </Table>
-        <div className={"u-inline-block u-margin-top-large"}>
-            <Paginator
-                incrementPage={incrementPage}
-                decrementPage={decrementPage}
-                currentPage={pagination}
-                maxPages={maxPages}
-                displayOfTotalPages={false}
-            />
-        </div>
-    </div>);
-
-    function renderCells(rowData, renderCell) {
-        const columns = [...allColumns];
-
-        const isEmptyDataSet = typeof rowData === 'object' && !Object.keys(rowData).length;
-
-        return columns.map(column => {
-            if (isEmptyDataSet) {
-                return renderCell(Skeleton, column);
-            }
-
-            const Cell = column.cell ? column.cell : <div/>;
-
-            return renderCell(Cell, column);
-        })
-    }
-
-    function renderRows(data, renderRow) {
-        if (!data) {
-           // render single row for the Skeleton components when there is no data.
-           return renderRow({});
-        }
-
-        return data.map(rowData => {
-            return renderRow(rowData);
-        })
-    }
-
-    function incrementPage() {
-        const newPage = incrementToMax(pagination, maxPages);
-        setPagination(newPage);
-        historyFetch(newPage, setData);
-    }
-
-    function decrementPage() {
-        const newPage = decrementTo0(pagination);
-        setPagination(newPage);
-        historyFetch(newPage, setData);
-    }
+    return <Table
+        data={data}
+        pagination={pagination}
+        onPageChange={(newPage)=>{
+            setPagination(newPage);
+            historyFetch(newPage, setData);
+        }}
+        setRowValue={setRowValue}
+        columns={allColumns}
+        maxPages={100}
+        styledComponents={{
+            Table: StyledTable,
+            Th
+        }}
+    />;
 
     function useDataState(initialValue) {
         let [data, setData] = useState(initialValue);
@@ -127,15 +58,3 @@ const HistoryApp = (props) => {
 };
 
 export default HistoryApp;
-
-function decrementTo0(number) {
-    return number -1 >= 0 ? number - 1 : 0;
-}
-
-function incrementToMax(number, max) {
-    return number + 1 <= max ? number + 1 : max;
-}
-
-function getRandomNumber(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-}
