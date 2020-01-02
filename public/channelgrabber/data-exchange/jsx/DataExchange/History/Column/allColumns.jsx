f4e47afc283e7e7bf9092b65c0476976afd39556
Christian Walker-Spiers <christian.walker-spiers@channelgrabber.com>
2020-01-02 11:29:37 +0000

    MIG-43 - typo fix for column headers

--- public/channelgrabber/data-exchange/jsx/DataExchange/History/Column/allColumns.jsx
+++ public/channelgrabber/data-exchange/jsx/DataExchange/History/Column/allColumns.jsx
@@ -9,7 +9,7 @@ export default [
     },
     {
         key: 'type',
-        label: 'iType',
+        label: 'Type',
         cell: LinkCell
     },
     {
@@ -36,7 +36,7 @@ export default [
     },
     {
         key: 'file',
-        label: 'file',
+        label: 'File',
         cell: LinkCell,
         getLink: (data) => (data.fileLink)
     },
