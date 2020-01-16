929751957fe1567b72115448768099e74f9740ca
= <johncranston@cloudcommercepro.com>
2020-01-16 09:53:41 +0000

    TAC-570-fixing textarea lag (and adding back fake reponses temporarily)

--- public/channelgrabber/messages/jsx/Components/ReplyBox.jsx
+++ public/channelgrabber/messages/jsx/Components/ReplyBox.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 import ButtonSelect from 'Common/Components/ButtonSelect';
+import Textarea from 'MessageCentre/Components/Textarea';
 
 const StyledButtonSelect = styled(ButtonSelect)`
     width: 16rem;
     margin-bottom: 6rem;
 `
 
-const TextArea = styled.textarea`
+const LaggyTextArea = styled.textarea`
+    width: 100%;
+    height: 20rem;
+    resize: vertical;
+    box-sizing: border-box;
+    padding: 1rem;
+`;
+
+const StyledTextarea = styled(Textarea)`
     width: 100%;
     height: 20rem;
     resize: vertical;
@@ -18,7 +27,7 @@ const TextArea = styled.textarea`
 const ReplyBox = (props) => {
     const {actions, thread, reply} = props;
 
-    let options = [
+    const options = [
         {
             name: 'Send and Resolve',
             id: 'send-and-resolve',
@@ -33,11 +42,19 @@ const ReplyBox = (props) => {
 
     return (
         <div>
-            <TextArea
-                onChange={actions.replyInputType}
+
+            <StyledTextarea
                 className={`u-margin-top-med`}
                 placeholder={'Compose your reply here'}
+                id={`composedReply`}
             />
+
+            {/*<LaggyTextArea*/}
+            {/*    onChange={actions.replyInputType}*/}
+            {/*    className={`u-margin-top-med`}*/}
+            {/*    placeholder={'Compose your reply here'}*/}
+            {/*/>*/}
+
             <div className={`u-clear-both u-margin-top-med`}>
                 {thread.status === 'resolved' &&
                     <button
