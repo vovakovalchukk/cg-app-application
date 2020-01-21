83c98468761f64cf83bcbd590950c6d04de2e2ba
Aaron Baker <aaron.baker@channelgrabber.com>
2020-01-21 14:48:10 +0000

    TAC-582: Orders - added new Partial Refund button

--- module/Orders/src/Orders/Order/BulkActions/Action/PartialRefund.php
+++ module/Orders/src/Orders/Order/BulkActions/Action/PartialRefund.php
@@ -1,61 +1,69 @@
 <?php
 namespace Orders\Order\BulkActions\Action;
 
-use CG_UI\View\BulkActions\Action;
-use Zend\View\Model\ViewModel;
-use SplObjectStorage;
-use CG\Order\Shared\Cancel\Reasons;
-use CG\Channel\Action\Order\Service as ActionDecider;
 use CG\Channel\Action\Order\MapInterface as ActionDeciderMap;
-use Orders\Order\BulkActions\OrderAwareInterface;
-use CG\Order\Shared\Entity as Order;
+use CG\Channel\Action\Order\Service as ActionDecider;
+use CG\Order\Shared\Cancel\Reasons;
 use CG\Order\Shared\Cancel\Value as CancelValue;
-use Orders\Module;
-use CG_UI\Module as CG_UI;
+use CG\Order\Shared\Entity as Order;
+use CG\Order\Shared\Item\Collection as ItemCollection;
+use CG\Order\Shared\Item\Entity as Item;
+use CG\Order\Shared\Item\Refund\Collection as ItemRefundCollection;
+use CG\Order\Shared\Item\Refund\Entity as ItemRefund;
+use CG\Order\Shared\Item\Refund\Filter as ItemRefundFilter;
+use CG\Order\Shared\Item\Refund\Service as ItemRefundService;
+use CG\Stdlib\Exception\Runtime\NotFound;
+use CG_UI\View\BulkActions\Action;
+use Orders\Order\BulkActions\OrderAwareInterface;
+use SplObjectStorage;
+use Zend\View\Model\ViewModel;
 
 class PartialRefund extends Action implements OrderAwareInterface
 {
     const ICON = 'sprite-accounting-22-black';
-    const TYPE = CancelValue::REFUND_TYPE;
     const ALLOWED_ACTION = ActionDeciderMap::PARTIAL_REFUND;
 
     /** @var ActionDecider */
     protected $actionDecider;
     /** @var ViewModel */
     protected $urlView;
+    /** @var ItemRefundService */
+    protected $itemRefundService;
 
     public function __construct(
         ActionDecider $actionDecider,
         ViewModel $urlView,
+        ItemRefundService $itemRefundService,
         array $elementData = [],
         ViewModel $javascript = null,
         SplObjectStorage $subActions = null
     ) {
-        parent::__construct(static::ICON, ucwords(static::TYPE), static::TYPE, $elementData, $javascript, $subActions);
+        parent::__construct(static::ICON, 'Partial Refund', 'partialRefund', $elementData, $javascript, $subActions);
         $this->actionDecider = $actionDecider;
         $this->urlView = $urlView;
-        $this->configure();
+        $this->itemRefundService = $itemRefundService;
     }
 
-    protected function configure()
+    public function setOrder(Order $order)
+    {
+        $actions = array_fill_keys($this->actionDecider->getAvailableActionsForOrder($order), true);
+        $this->setEnabled(isset($actions[static::ALLOWED_ACTION]));
+        $this->setEnabled(true);//temp
+        $this->configure($order);
+    }
+
+    protected function configure(Order $order)
     {
         $this->addElementView($this->getUrlView());
         $this->getJavascript()->setVariables(
             [
-                'cancellationReasons' => json_encode($this->getReasons()),
-                'type' => static::TYPE,
-                'templateMap' => [
-                    'popup' => Module::PUBLIC_FOLDER . 'template/popups/cancelOptions.html',
-                    'select' => CG_UI::PUBLIC_FOLDER . 'templates/elements/custom-select.mustache',
-                ],
+                'refundReasons' => json_encode(Reasons::getAllRefundReasons()),
+                'items' => $this->getRefundableItemsData($order),
             ]
         );
         return $this;
     }
 
-    /**
-     * @return ViewModel
-     */
     public function getUrlView()
     {
         $this->urlView->setVariables(
@@ -67,14 +75,43 @@ class PartialRefund extends Action implements OrderAwareInterface
         return $this->urlView;
     }
 
-    protected function getReasons()
+    protected function getRefundableItemsData(Order $order): array
     {
-        return Reasons::getAllRefundReasons();
+        $data = [];
+        $items = $order->getItems();
+        $itemRefunds = $this->fetchItemRefundsForItems($items);
+        foreach ($items as $item) {
+            $data[] = [
+                'id' => $item->getId(),
+                'sku' => $item->getItemSku(),
+                'name' => $item->getItemName(),
+                'amount' => $this->calculateRefundableAmountForItem($item, $itemRefunds->getBy('itemId', $item->getId())),
+            ];
+        }
+        return $data;
     }
 
-    public function setOrder(Order $order)
+    protected function fetchItemRefundsForItems(ItemCollection $items): ItemRefundCollection
     {
-        $actions = array_fill_keys($this->getActionDecider()->getAvailableActionsForOrder($order), true);
-        $this->setEnabled(isset($actions[static::ALLOWED_ACTION]));
+        try {
+            $filter = (new ItemRefundFilter())
+                ->setLimit('all')
+                ->setPage(1)
+                ->setItemId($items->getIds());
+            return $this->itemRefundService->fetchCollectionByFilter($filter);
+        } catch (NotFound $e) {
+            return new ItemRefundCollection(ItemRefund::class, 'empty');
+        }
+    }
+
+    protected function calculateRefundableAmountForItem(Item $item, ItemRefundCollection $itemRefunds): float
+    {
+        $amount = $item->getLineTotal();
+        /** @var ItemRefund $itemRefund */
+        foreach ($itemRefunds as $itemRefund) {
+            $amount -= $itemRefund->getAmount();
+        }
+        $amount = round($amount, 2);
+        return ($amount >= 0 ? $amount : 0);
     }
 }
