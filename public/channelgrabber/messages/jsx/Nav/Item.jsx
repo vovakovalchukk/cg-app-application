176f3d0d8a1a37cceb3e0bfec41066cb9d5cf143
John Cranston <johncranston@cloudcommercepro.com>
2020-02-17 14:30:15 +0000

    TAC-576-css tweak for templates sidebar item

--- public/channelgrabber/messages/jsx/Nav/Item.jsx
+++ public/channelgrabber/messages/jsx/Nav/Item.jsx
@@ -11,9 +11,7 @@ const Item = (props) => {
         <div>
             <NavLink
                 to={to}
-                activeStyle={{
-                    color: 'green'
-                }}
+                activeClassName="sidebar-item-selected"
             >
                 <span>{displayText}</span>
             </NavLink>
