a75485302196d313e66b1d9a5cebcf0753932c99
John Cranston <johncranston@cloudcommercepro.com>
2020-02-21 10:12:33 +0000

    TAC-566-some jsx syntax tweaks for the LoadingSpinner component

--- public/channelgrabber/messages/jsx/Components/LoadingSpinner.jsx
+++ public/channelgrabber/messages/jsx/Components/LoadingSpinner.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const colorBlue = '#5fafda';
+
 const Container = styled.div`
     clear: both;
-    border: 2px solid #5fafda;
+    border: 2px solid ${colorBlue};
     border-radius: 5px;
     background-clip: padding-box;
     position: fixed;
-    color: #5fafda;
+    color: ${colorBlue};
     background-color: #ffffff;
     padding: 20px 15px 15px 15px;
     text-align: left;
@@ -30,14 +32,14 @@ const LoadingSpinner = () => {
                 <svg viewBox="0 0 38 38" className="loading-indicator-element" xmlns="http://www.w3.org/2000/svg">
                     <defs>
                         <linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="indicator-gradient">
-                            <stop stop-color="#5fafda" stop-opacity="0" offset="0%"></stop>
-                            <stop stop-color="#5fafda" stop-opacity=".631" offset="63.146%"></stop>
-                            <stop stop-color="#5fafda" offset="100%"></stop>
+                            <stop stopColor={colorBlue} stopOpacity="0" offset="0%"></stop>
+                            <stop stopColor={colorBlue} stopOpacity=".631" offset="63.146%"></stop>
+                            <stop stopColor={colorBlue} offset="100%"></stop>
                         </linearGradient>
                     </defs>
-                    <g fill="none" fill-rule="evenodd">
+                    <g fill="none" fillRule="evenodd">
                         <g transform="translate(1 1)">
-                            <path d="M36 18c0-9.94-8.06-18-18-18" id="Oval-2" stroke-width="2" stroke="url(#indicator-gradient)" transform="rotate(198.457 18 18)">
+                            <path d="M36 18c0-9.94-8.06-18-18-18" id="Oval-2" strokeWidth="2" stroke="url(#indicator-gradient)" transform="rotate(198.457 18 18)">
                                 <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite"></animateTransform>
                             </path>
                             <circle cx="36" cy="18" r="1" transform="rotate(198.457 18 18)">
