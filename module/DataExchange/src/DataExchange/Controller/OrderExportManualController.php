8a53becace2c69f74e719563710b8c4a653d770e
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2020-03-18 14:45:53 +0000

    CGIV-11264: Remove setting the root OU id on the order filter when exporting orders as it's already set by the filter entity; Handle possible errors when doing order exports by showing a relevant error message to the users as well

--- module/DataExchange/src/DataExchange/Controller/OrderExportManualController.php
+++ module/DataExchange/src/DataExchange/Controller/OrderExportManualController.php
@@ -1,26 +1,33 @@
 <?php
 namespace DataExchange\Controller;
 
-use CG_UI\View\Prototyper\JsonModelFactory;
+use Application\Controller\AbstractJsonController;
 use CG\Stdlib\DateTime as CGDateTime;
+use CG\Stdlib\Exception\Runtime\NotFound;
+use CG\Stdlib\Log\LoggerAwareInterface;
+use CG\Stdlib\Log\LogTrait;
 use CG\Zend\Stdlib\Http\FileResponse;
+use CG_UI\View\Prototyper\JsonModelFactory;
 use DataExchange\Manual\OrderExporter;
-use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\JsonModel;
 
-class OrderExportManualController extends AbstractActionController
+class OrderExportManualController extends AbstractJsonController implements LoggerAwareInterface
 {
+    use LogTrait;
+
     public const ROUTE_DOWNLOAD = 'Download';
+
+    protected const LOG_CODE = 'OrderExportManualController';
+    protected const LOG_MESSAGE_NO_ORDERS_FOUND = 'No orders found for filter %s';
+    protected const LOG_MESSAGE_EXCEPTION = 'Caught unexpected exception while exporting orders for template ID %s and saved filter %s';
 
     /** @var OrderExporter */
     protected $orderExporter;
-    /** @var JsonModelFactory */
-    protected $jsonModelFactory;
 
     public function __construct(OrderExporter $orderExporter, JsonModelFactory $jsonModelFactory)
     {
+        parent::__construct($jsonModelFactory);
         $this->orderExporter = $orderExporter;
-        $this->jsonModelFactory = $jsonModelFactory;
     }
 
     public function downloadAction()
@@ -28,16 +35,26 @@ class OrderExportManualController extends AbstractActionController
         $templateId = $this->params()->fromPost('templateId');
         $savedFilterName = $this->params()->fromPost('savedFilterName');
         $sendViaEmail = filter_var($this->params()->fromPost('sendViaEmail'), FILTER_VALIDATE_BOOLEAN);
-        if ($sendViaEmail) {
-            return $this->sendViaEmail($templateId, $savedFilterName);
+
+        try {
+            if ($sendViaEmail) {
+                return $this->sendViaEmail($templateId, $savedFilterName);
+            }
+
+            return $this->downloadToBrowser($templateId, $savedFilterName);
+        } catch (NotFound $exception) {
+            $this->logWarningException($exception, static::LOG_MESSAGE_NO_ORDERS_FOUND, [$savedFilterName], static::LOG_CODE);
+            return $this->buildErrorResponse('No orders found for the selected filter.');
+        } catch (\Throwable $exception) {
+            $this->logWarningException($exception, static::LOG_MESSAGE_EXCEPTION, [$templateId, $savedFilterName], static::LOG_CODE);
+            return $this->buildErrorResponse('An error has occurred. Please try again or contact support if the problem persists.');
         }
-        return $this->downloadToBrowser($templateId, $savedFilterName);
     }
 
     protected function sendViaEmail(int $templateId, string $savedFilterName): JsonModel
     {
         $this->orderExporter->sendViaEmail($templateId, $savedFilterName);
-        return $this->jsonModelFactory->newInstance(['success' => true]);
+        return $this->buildSuccessResponse();
     }
 
     protected function downloadToBrowser(int $templateId, string $savedFilterName): FileResponse
