3e699ee92981fcec26076436d15a9215048451bd
Dominik Gajewski <dominikg@channelgrabber.com>
2020-04-03 13:42:22 +0100

    CGIV-10747 moved classes to libraries

--- module/Orders/src/Orders/Courier/Label/Cancel/CancelActionInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Orders\Courier\Label\Cancel;
-
-use CG\Order\Shared\Entity as Order;
-
-interface CancelActionInterface
-{
-    public function postTrackingNumberRemovalAction(Order $order): void;
-}
