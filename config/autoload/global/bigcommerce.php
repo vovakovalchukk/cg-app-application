737b948bd02e4e79075b8b4a1d440364ce13b8be
markduffieldCG <mark.duffield@channelgrabber.com>
2020-04-29 14:42:49 +0100

    IN-544: Adds DI to send big commerce listing import jobs to the bigcommerce queue

--- config/autoload/global/bigcommerce.php
+++ config/autoload/global/bigcommerce.php
@@ -1,10 +1,22 @@
 <?php
 use CG\BigCommerce\ListingImport as BigCommerceListingImport;
+Use CG\BigCommerce\Gearman\Generator\ListingDownload as BigCommercListingDownloadGenerator;
+use CG\BigCommerce\Gearman\Generator\Listing\CreateListing as BigCommercListingCreateListingGenerator;
 
 return [
     'di' => [
         'instance' => [
             BigCommerceListingImport::class => [
+                'parameters' => [
+                    'gearmanClient' => 'bigcommerceGearmanClient'
+                ]
+            ],
+            BigCommercListingDownloadGenerator::class => [
+                'parameters' => [
+                    'gearmanClient' => 'bigcommerceGearmanClient'
+                ]
+            ],
+            BigCommercListingCreateListingGenerator::class => [
                 'parameters' => [
                     'gearmanClient' => 'bigcommerceGearmanClient'
                 ]
