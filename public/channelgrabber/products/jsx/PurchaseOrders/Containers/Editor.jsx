20bab57ce3fc58d64fe023757bbd2e0f57e166cd
Oliver Stephenson <oliver@channelgrabber.com>
2020-08-19 15:22:25 +0100

    CGIV-11436: added handling for converting unicode characters to base64
    We need to convert the string to a byte array, then pass that to the function from the base64-js package.

--- public/channelgrabber/products/jsx/PurchaseOrders/Containers/Editor.jsx
+++ public/channelgrabber/products/jsx/PurchaseOrders/Containers/Editor.jsx
@@ -164,7 +164,8 @@ class EditorContainer extends React.Component {
             data: {id: this.state.purchaseOrderId},
             url: '/products/purchaseOrders/download',
             success: function (response) {
-                var uri = 'data:text/csv;charset=utf-8;base64,' + btoa(response);
+                var textEncoder = new TextEncoder();
+                var uri = 'data:text/csv;charset=utf-8;base64,' + fromByteArray(textEncoder.encode(response));
 
                 var downloadLink = document.createElement("a");
                 downloadLink.href = uri;
