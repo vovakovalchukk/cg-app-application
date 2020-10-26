e17d966246ccdb7113534f304bab896ae6c0bc15
Oliver Stephenson <oliver@channelgrabber.com>
2020-10-26 15:48:01 +0000

    CGIV-11667: we now have a fallback to using cookies to get the current account request id, and use this to get the success url

--- module/Settings/src/Settings/Controller/ChannelControllerAbstract.php
+++ module/Settings/src/Settings/Controller/ChannelControllerAbstract.php
@@ -2,11 +2,14 @@
 namespace Settings\Controller;
 
 use CG\Account\CreationServiceAbstract as AccountCreationService;
+use CG\Account\Shared\Entity as Account;
 use CG\FeatureFlags\Service as FeatureFlagsService;
 use CG\OrganisationUnit\Entity as OrganisationUnit;
 use CG\OrganisationUnit\Service as OrganisationUnitService;
+use CG\Stdlib\Exception\Runtime\NotFound;
 use CG\User\ActiveUserInterface;
 use CG\User\Entity as User;
+use CG_Permission\Service as PermissionService;
 use CG_UI\View\Prototyper\JsonModelFactory;
 use CG_UI\View\Prototyper\ViewModelFactory;
 use Partner\Account\AuthoriseService as PartnerAuthoriseService;
@@ -74,6 +77,25 @@ abstract class ChannelControllerAbstract extends AbstractActionController
     protected function getActiveUserRootOrganisationUnit(): OrganisationUnit
     {
         return $this->organisationUnitService->fetch($this->activeUserContainer->getActiveUserRootOrganisationUnitId());
+    }
+
+    protected function fetchPartnerSuccessRedirectUrl(Account $account): string
+    {
+        try {
+            $accountRequestId = $this->partnerAuthoriseService->fetchAccountRequestIdFromSession();
+        } catch (NotFound $e) {
+            $accountRequestId = $this->getPartnerAccountRequestIdFromCookie();
+        }
+        return $this->partnerAuthoriseService->fetchPartnerSuccessRedirectUrl($account, $accountRequestId);
+    }
+
+    protected function getPartnerAccountRequestIdFromCookie(): int
+    {
+        $cookie = $this->getRequest()->getCookie();
+        if ($cookie->offsetExists(PermissionService::PARTNER_MANAGED_LOGIN)) {
+            return $cookie->offsetGet(PermissionService::PARTNER_MANAGED_LOGIN);
+        }
+        throw new NotFound('Partner account request ID not found in cookie');
     }
 
     /**
