bd8193139ae9752547bddff91d606f1fe8510103
Oliver Stephenson <oliver@channelgrabber.com>
2021-01-04 17:09:02 +0000

    CGIV-11295: we now ensure that domestic delivery services are presented when the shipment country code is in the channel islands

--- library/CG/Intersoft/RoyalMail/DeliveryService.php
+++ library/CG/Intersoft/RoyalMail/DeliveryService.php
@@ -131,9 +131,20 @@ class DeliveryService implements DeliveryServiceInterface
         return true;
     }
 
-    protected function isCountryCodeUkOrGb(string $countryCode)
+    protected function isCountryCodeDomestic(string $countryCode): bool
+    {
+        return $this->isCountryCodeUkOrGb($countryCode) && $this->isCountryCodeChannelIslands($countryCode);
+    }
+
+    protected function isCountryCodeUkOrGb(string $countryCode): bool
     {
         return ($countryCode == 'GB' || $countryCode == 'UK');
+    }
+
+    protected function isCountryCodeChannelIslands(string $countryCode): bool
+    {
+        /* Jersey and Guernsey use domestic shipping methods */
+        return ($countryCode == 'JE' || $countryCode == 'GG');
     }
 
     protected function isInternationalServiceType(string $isoAlpha2CountryCode): bool
@@ -149,12 +160,12 @@ class DeliveryService implements DeliveryServiceInterface
     protected function hideInternationalService(string $countryCode): bool
     {
         return ((!$this->isInternationalServiceType($this->getServiceType()) && !$this->isArmedForcesServiceType($this->getServiceType()))
-            && !$this->isCountryCodeUkOrGb($countryCode));
+            && !$this->isCountryCodeDomestic($countryCode));
     }
 
     protected function hideDomesticService(string $isoAlpha2CountryCode): bool
     {
         return (($this->isInternationalServiceType($this->getServiceType()) || $this->isArmedForcesServiceType($this->getServiceType()))
-            && $this->isCountryCodeUkOrGb($isoAlpha2CountryCode));
+            && $this->isCountryCodeDomestic($isoAlpha2CountryCode));
     }
 }
