3e4e3c13d46cbbf5c7ec44a0733fcff1a4b78c7a
Dominik Gajewski <dominikg@channelgrabber.com>
2021-02-23 11:51:48 +0000

    CGIV-11886 added js code necessary for bulk action

--- public/channelgrabber/orders/js-vanilla/Courier/Specifics/Columns/TermsOfDelivery.js
+++ public/channelgrabber/orders/js-vanilla/Courier/Specifics/Columns/TermsOfDelivery.js
@@ -1,61 +1,48 @@
-define(['./ServiceDependantOptionsAbstract.js'], function(ServiceDependantOptionsAbstract)
+define([''], function()
 {
     function TermsOfDelivery()
     {
         var init = function()
         {
-            // this.listenForServiceChanges();
+            this.listenForTODChanges();
         };
         init.call(this);
     }
 
     TermsOfDelivery.SELECTOR_BULK_ACTION_TOD_CHECKBOX_ID = '#courier-terms-of-delivery-checkbox';
-    TermsOfDelivery.SELECTOR_BULK_ACTION_TOD_CHECKBOX_CLASS = '.courier-terms-of-delivery-checkbox';
     TermsOfDelivery.SELECTOR_TOD_CHECKBOX_CLASS = '.courier-order-termsOfDelivery';
 
-    TermsOfDelivery.prototype.listenForServiceChanges = function()
+    TermsOfDelivery.prototype.listenForTODChanges = function()
     {
-    //     var self = this;
-        $(TermsOfDelivery.SELECTOR_BULK_ACTION_TOD_CHECKBOX_ID).on('change', function(event, element, value)
+        var self = this;
+        $(document).on('change', TermsOfDelivery.SELECTOR_BULK_ACTION_TOD_CHECKBOX_ID, function()
         {
             if (this.checked) {
-                console.log('ELEMENT CHECKED');
-            } else {
-                console.log('ELEMENT UNCHECKED');
+                self.checkAllCheckboxes();
+                return true;
             }
-    //         if (value === undefined || value === "") {
-    //             return;
-    //         }
-    //         var orderId = $(element).data('elementName').match(/^orderData\[(.+?)\]/)[1];
-    //         self.updateShippingLabelCost(orderId, value, element);
+
+            self.uncheckAllCheckboxes();
         });
 
         return this;
     };
-    //
-    // TermsOfDelivery.prototype.updateShippingLabelCost = function(orderId, shippingService, element)
-    // {
-    //     var currentCostColumn = element.parents('tr').find(TermsOfDelivery.SELECTOR_COST_COLUMN_INPUT);
-    //     var labelCosts = this.getStorage().get("labelCosts");
-    //
-    //     if (labelCosts === undefined) {
-    //         return;
-    //     }
-    //
-    //     currentCostColumn.val(labelCosts[orderId][shippingService].cost);
-    //     this.updateTotalShippingCost();
-    //     return this;
-    // };
-    //
-    // TermsOfDelivery.prototype.updateTotalShippingCost = function()
-    // {
-    //     $(TermsOfDelivery.SELECTOR_CURRENCY_SYMBOL_DISPLAY).removeClass('hidden');
-    //
-    //     var totalLabelCost = 0;
-    //     $(TermsOfDelivery.SELECTOR_COST_COLUMN_INPUT).each(function() {
-    //         totalLabelCost += Number($(this).val());
-    //     });
-    //     $(TermsOfDelivery.SELECTOR_TOTAL_ORDER_LABEL_COST).text(totalLabelCost.toFixed(2));
-    // };
+
+    TermsOfDelivery.prototype.checkAllCheckboxes = function()
+    {
+        $(TermsOfDelivery.SELECTOR_TOD_CHECKBOX_CLASS).each(function () {
+            $(this).prop('checked', true);
+        });
+        return this;
+    };
+
+    TermsOfDelivery.prototype.uncheckAllCheckboxes = function()
+    {
+        $(TermsOfDelivery.SELECTOR_TOD_CHECKBOX_CLASS).each(function () {
+            $(this).prop('checked', false);
+        });
+        return this;
+    };
+
     return TermsOfDelivery;
 });
