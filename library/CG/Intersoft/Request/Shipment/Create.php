99eb2f85a49e73d50d279b99a1f9244289690af3
Oliver Stephenson <oliver@channelgrabber.com>
2021-04-15 17:17:21 +0100

    CGIV-11859: set shippingCharges as a child of customsInformation. we do this in CCP, but none of the customsInformation stuff is even documented by intersoft but this is what they advised.

--- library/CG/Intersoft/Request/Shipment/Create.php
+++ library/CG/Intersoft/Request/Shipment/Create.php
@@ -215,6 +215,7 @@ class Create extends PostAbstract
         $customsInformationXml = $xml->addChild('customsInformation');
         $customsInformationXml->addChild('preRegistrationNumber', $this->shipment->getEoriNumber());
         $customsInformationXml->addChild('preRegistrationType', static::PRE_REGISTRATION_TYPE_EORI);
+        $customsInformationXml->addChild('shippingCharges', number_format($this->shipment->getShippingCharges(), 2));
         return $xml;
     }
 
