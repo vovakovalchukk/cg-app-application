c8e8bce3cf4a3ddaf0a3d0773645eb04f5a122e1
Dominik Gajewski <dominikg@channelgrabber.com>
2021-07-22 12:01:53 +0100

    CGIV-11760 added Intl Response class and Service

---
+++ library/CG/UkMail/InternationalConsignment/Service.php
@@ -0,0 +1,54 @@
+<?php
+namespace CG\UkMail\InternationalConsignment;
+
+use CG\CourierAdapter\Exception\UserError;
+use CG\UkMail\Client\Factory as ClientFactory;
+use CG\UkMail\Request\Rest\InternationalConsignment as InternationalConsignmentRequest;
+use CG\UkMail\Response\Rest\InternationalConsignment as InternationalConsignmentResponse;
+use CG\UkMail\Shipment;
+use CG\Stdlib\Log\LoggerAwareInterface;
+use CG\Stdlib\Log\LogTrait;
+
+class Service implements LoggerAwareInterface
+{
+    use LogTrait;
+
+    protected const LOG_CODE = 'UkMailInternationalConsignmentService';
+    protected const LOG_REQUESTING_LABEL_MSG = 'Requesting UK Mail label for account %d order %s';
+
+    /** @var ClientFactory */
+    protected $clientFactory;
+    /** @var Mapper */
+    protected $mapper;
+
+    public function __construct(ClientFactory $clientFactory, Mapper $mapper)
+    {
+        $this->clientFactory = $clientFactory;
+        $this->mapper = $mapper;
+    }
+
+    protected function createInternationalConsignmentRequest(
+        Shipment $shipment,
+        string $authToken,
+        string $collectionJobNumber,
+        string $customsDeclarationType
+    ): InternationalConsignmentRequest {
+        return $this->mapper->createInternationalConsignmentRequest($shipment, $authToken, $collectionJobNumber, $customsDeclarationType);
+    }
+
+    public function requestDomesticConsignment(
+        Shipment $shipment,
+        string $authToken,
+        string $collectionJobNumber,
+        string $customsDeclarationType
+    ): InternationalConsignmentResponse {
+        $this->logDebug(static::LOG_REQUESTING_LABEL_MSG, [$shipment->getAccount()->getId(), $shipment->getCustomerReference()], static::LOG_CODE);
+        $intlConsignmentRequest = $this->createInternationalConsignmentRequest($shipment, $authToken, $collectionJobNumber, $customsDeclarationType);
+        try {
+            $client = ($this->clientFactory)($shipment->getAccount(), $intlConsignmentRequest);
+            return $client->sendRequest($intlConsignmentRequest);
+        } catch (\Exception $exception) {
+            throw new UserError($exception->getMessage());
+        }
+    }
+}
