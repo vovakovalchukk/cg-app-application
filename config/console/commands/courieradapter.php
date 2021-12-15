3740ab6906788ffd3f5f91c8d27a56ac0923f399
Dominik Gajewski <dominikg@channelgrabber.com>
2021-12-15 12:37:01 +0000

    CGIV-12281 constructing ImportGeogaz class using Di

--- config/console/commands/courieradapter.php
+++ config/console/commands/courieradapter.php
@@ -6,7 +6,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use CG\Command\NullActiveUser;
 use Zend\Di\Di;
-use CG\Courier\Geopost\Command\ImportGeogaz;
 
 /** @var Di $di */
 return [
@@ -44,17 +43,4 @@ return [
             $command($output);
         },
     ],
-
-    'courieradapter:geogaz' => [
-        'description' => '',
-        'arguments' => [],
-        'command' => function(InputInterface $input, OutputInterface $output) use ($di)
-        {
-            /** @var ImportGeogaz $command */
-            $di->instanceManager()->setTypePreference('CG\User\ActiveUserInterface', [new NullActiveUser()]);
-            $command = $di->get(ImportGeogaz::class);
-            $command();
-        },
-    ],
-
 ];
