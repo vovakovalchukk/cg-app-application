22d2cc54c6ade72ce6f6cbb6a32f3c923e54cb5f
Dominik Gajewski <dominikg@channelgrabber.com>
2022-05-17 14:22:01 +0100

    CGIV-12378 added explanatory comment for new metod

--- library/CG/UkMail/Consignment/MapperTrait.php
+++ library/CG/UkMail/Consignment/MapperTrait.php
@@ -5,9 +5,14 @@ use CG\UkMail\Shipment;
 
 trait MapperTrait
 {
+    /**
+     * In case of linked orders which $shipment->getCustomerReference() returns all linked orders' ids separated by comma
+     * We will return first order's id instead cut string in middle
+     * In case of string extending 20 characters as it is order id to be sure that is unambiguous we want to return last 20 characters
+     */
     protected function getCustomerReference(Shipment $shipment): string
     {
         $result = explode(',', $shipment->getCustomerReference());
-        return (strlen($result[0]) > 20) ? substr($result[0], -20) : $result[0];
+        return substr($result[0], -20);
     }
 }
