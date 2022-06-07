0fb8834b12f9a6622b5d2ddc68dd30f0a3968b8e
Dominik Gajewski <dominikg@channelgrabber.com>
2022-06-07 16:46:32 +0100

    TAC-678 added config

--- config/autoload/global/amazon.php
+++ config/autoload/global/amazon.php
@@ -1,10 +1,29 @@
 <?php
 use CG\Amazon\ListingImport as AmazonListingImport;
 use CG\Amazon\Region\Service as AmazonRegionService;
+use CG\Amazon\Authorisation\AccessToken\Storage as AccessTokenStorage;
+use CG\Amazon\Authorisation\AccessToken\Service as AccessTokenService;
+use CG\Amazon\Authorisation\Service as AuthorisationService;
+use SellingPartnerApi\Api\SellersApi;
+use SellingPartnerApi\Configuration;
+use SellingPartnerApi\Api\AuthorizationApi;
+use CG\Amazon\Marketplace\Participation\SpApi\Service as AmazonMarketplaceParticipationSpApiService;
+use CG\Amazon\Carrier\SpApi\GetEligibleShippingServices as SpApiGetEligibleShippingServices;
+use CG\Amazon\Serializer\NameConverter\LowerCamelCaseToUpperCamelCaseNameConverter;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 return [
     'di' => [
         'instance' => [
+            'alias' => [
+                'json_object_serializer' => Serializer::class,
+            ],
+            'preferences' => [
+                Configuration::class => 'sellingPartnerApiConfig',
+            ],
             AmazonRegionService::class => [
                 'parameters' => [
                     'cryptor' => 'amazon_cryptor',
@@ -14,7 +33,58 @@ return [
                 'parameters' => [
                     'gearmanClient' => 'amazonGearmanClient'
                 ]
-            ]
+            ],
+            AccessTokenStorage::class => [
+                'parameter' => [
+                    'predisClient' => 'reliable_redis'
+                ],
+            ],
+            AccessTokenService::class => [
+                'parameter' => [
+                    'configuration' => 'sellingPartnerApiConfig',
+                    'cryptor' => 'amazon_cryptor',
+                ],
+            ],
+            AuthorisationService::class => [
+                'parameter' => [
+                    'configuration' => 'sellingPartnerApiConfig',
+                    'cryptor' => 'amazon_cryptor',
+                ],
+            ],
+            AuthorizationApi::class => [
+                'parameter' => [
+                    'config' => 'sellingPartnerApiConfig'
+                ],
+            ],
+            AmazonMarketplaceParticipationSpApiService::class => [
+                'parameter' => [
+                    'cryptor' => 'amazon_cryptor',
+                ],
+            ],
+            SellersApi::class => [
+                'parameter' => [
+                    'config' => 'sellingPartnerApiConfig',
+                ],
+            ],
+            'json_object_serializer' => [
+                'parameters' => [
+                    'normalizers' => [
+                        new GetSetMethodNormalizer(
+                            null,
+                            new LowerCamelCaseToUpperCamelCaseNameConverter()
+                        ),
+                    ],
+                    'encoders' => [
+                        JsonEncoder::FORMAT => new JsonEncoder(),
+                        'xml' => new XmlEncoder(),
+                    ],
+                ],
+            ],
+            SpApiGetEligibleShippingServices::class => [
+                'parameters' => [
+                    'serializer' => 'json_object_serializer',
+                ],
+            ],
         ],
     ],
 ];
