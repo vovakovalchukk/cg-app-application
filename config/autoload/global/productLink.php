1cd87af5f6d45c9833dc05f5be240e1ffb526e3b
Dominik Gajewski <dominikg@channelgrabber.com>
2022-08-04 13:13:50 +0100

    CGIV-12410 saving debug code

--- config/autoload/global/productLink.php
+++ config/autoload/global/productLink.php
@@ -5,11 +5,22 @@ use CG\FileStorage\S3\Adapter as ProductLinkCsvS3Storage;
 use CG\Channel\Product\Gearman\Generator\ImportLinks\Csv as ImportCsvLinksJobGenerator;
 use CG\Product\Link\Gearman\Generator\ExportProductLinks as ExportCsvLinksJobGenerator;
 
+use CG\Product\Link\Storage\Api as ProductLinkApiStorage;
+use CG\Product\Link\StorageInterface as ProductLinkStorageInterface;
+use CG\Product\LinkNode\Storage\Api as ProductLinkNodeApiStorage;
+use CG\Product\LinkNode\StorageInterface as ProductLinkNodeStorageInterface;
+use CG\Product\StorageInterface as ProductStorage;
+use CG\Product\Storage\Api as ProductApiStorage;
+
 return [
     'di' => [
         'instance' => [
             'aliases' => [
                 'ProductLinkS3FileImportAdapter' => ProductLinkCsvS3Storage::class,
+            ],
+            'preference' => [
+                ProductLinkStorageInterface::class => ProductLinkApiStorage::class,
+                ProductStorage::class => ProductApiStorage::class,
             ],
             'ProductLinkS3FileImportAdapter' => [
                 'parameters' => [
