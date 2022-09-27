d5fea33ee9e1dfe37b26c356de179a692dd7894f
Dominik Gajewski <dominikg@channelgrabber.com>
2022-09-27 17:22:52 +0100

    PRD-47 working on redirecting in case user is not logged in to CG

--- module/Shopify/src/Shopify/App/Service.php
+++ module/Shopify/src/Shopify/App/Service.php
@@ -2,21 +2,46 @@
 namespace Shopify\App;
 
 use CG\User\ActiveUserInterface;
+use CG_Login\Service as LoginService;
+use Shopify\Account\Service as AccountService;
+use Zend\Mvc\MvcEvent;
 
 class Service
 {
+    /** @var AccountService */
+    protected $accountService;
     /** @var ActiveUserInterface */
     protected $activeUser;
+    /** @var LoginService */
+    protected $loginService;
 
-    public function __construct(ActiveUserInterface $activeUser)
+    public function __construct(AccountService $accountService, ActiveUserInterface $activeUser, LoginService $loginService)
     {
+        $this->accountService = $accountService;
         $this->activeUser = $activeUser;
+        $this->loginService = $loginService;
     }
 
-    public function processOauth($redirectUri, array $parameters)
+    public function processOauth($redirectUri, array $parameters): string
     {
         if (!$this->activeUser->getActiveUser()) {
             throw new LoginException('User is not logged in');
         }
+
+        $accountId = $parameters['accountId'] ?? null;
+
+        return $this->accountService->getLink($parameters['shop'], $accountId);
+    }
+
+    public function cacheOauthRequest($redirectUri, array $parameters)
+    {
+        $accountId = $parameters['accountId'] ?? null;
+        return $this->accountService->getLink($parameters['shop'], $accountId);
+    }
+
+    public function saveProgressAndRedirectToLogin(MvcEvent $event, $route, array $routeParams = [], array $routeOptions = []): void
+    {
+        $this->loginService->setLandingRoute($route, $routeParams, $routeOptions);
+        $this->loginService->loginRedirect($event);
     }
 }
