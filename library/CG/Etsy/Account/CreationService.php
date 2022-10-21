1aa4a995d2f1ac02800a7dbf21b125a2e8ebc7f7
Dominik Gajewski <dominikg@channelgrabber.com>
2022-10-21 11:15:50 +0100

    PRD-153 adding marketplace during account creation

--- library/CG/Etsy/Account/CreationService.php
+++ library/CG/Etsy/Account/CreationService.php
@@ -2,14 +2,35 @@
 namespace CG\Etsy\Account;
 
 use CG\Account\Client\Entity as AccountEntity;
+use CG\Account\Client\Mapper as AccountMapper;
+use CG\Account\Client\Service as AccountService;
 use CG\Account\CreationServiceAbstract;
+use CG\Account\Credentials\Cryptor;
+use CG\Channel\AccountInterface;
 use CG\Etsy\Credentials;
+use CG\Etsy\Marketplace\AccountService as MarketplaceAccountService;
 use CG\Etsy\Response\AccessToken as AccessTokenResponse;
 use CG\Etsy\Response\Shop as ShopResponse;
+use CG\Scraper\Client as ScraperClient;
 
 class CreationService extends CreationServiceAbstract
 {
     const CHANNEL = 'etsy';
+
+    /** @var MarketplaceAccountService */
+    protected $marketplaceAccountService;
+
+    public function __construct(
+        AccountService $accountService,
+        Cryptor $cryptor,
+        AccountMapper $accountMapper,
+        ScraperClient $scraperClient,
+        MarketplaceAccountService $marketplaceAccountService,
+        AccountInterface $channelAccount = null
+    ) {
+        $this->marketplaceAccountService = $marketplaceAccountService;
+        parent::__construct($accountService, $cryptor, $accountMapper, $scraperClient, $channelAccount);
+    }
 
     public function getChannelName()
     {
@@ -25,6 +46,7 @@ class CreationService extends CreationServiceAbstract
         $shopResponse = $params['shopResponse'];
 
         $externalData = $account->getExternalData();
+        $externalData['marketplace'] = $this->marketplaceAccountService->getMarketplaceMapping($shopResponse);
         $externalData['userId'] = $shopResponse->getUserId();
         $externalData['shopId'] = $shopResponse->getShopId();
         $account->setExternalData($externalData);
