c02ac540ea1e83de6de5833c1bd3999da43ea8d6
Adrian, Baghina <adrian.baghina@cloudcommercegroup.com>
2022-10-31 14:08:18 +0000

    CGIV-12451: add types, use guard instead of if else

--- library/CG/CourierAdapter/Provider/Implementation/ParamsMapperProcessor.php
+++ library/CG/CourierAdapter/Provider/Implementation/ParamsMapperProcessor.php
@@ -22,28 +22,28 @@ class ParamsMapperProcessor
         ]
     ];
 
-    private function processMapItem(string $channelRules, array $params)
+    private function processMapItem(array $channelRules, array $params): array
     {
         foreach ($params as $key => $value) {
             if (is_array($value)) {
                 $availableRuleSet = $channelRules[$key] ?? $channelRules;
                 $params[$key] = $this->processMapItem($availableRuleSet, $value);
-            } else {
-                foreach ($channelRules as $chKey => $rule) {
-                    if ($key == $chKey) {
-                        $valueToSearchFor = $rule['value'];
-                        $valueToReplaceWith = $rule['replace'];
-                        $valuesMatch = strtolower($value) == strtolower($valueToSearchFor);
-                        $params[$key] = $valuesMatch ? $valueToReplaceWith : $value;
-                    }
+                continue;
+            }
+            foreach ($channelRules as $chKey => $rule) {
+                if ($key != $chKey) {
+                    continue;
                 }
-
+                $valueToSearchFor = $rule['value'];
+                $valueToReplaceWith = $rule['replace'];
+                $valuesMatch = strtolower($value) == strtolower($valueToSearchFor);
+                $params[$key] = $valuesMatch ? $valueToReplaceWith : $value;
             }
         }
         return $params;
     }
 
-    public function runParamsMapper($channelName, $params)
+    public function runParamsMapper(string $channelName, array $params)
     {
         if (isset(ParamsMapperProcessor::RULES[$channelName])) {
             return $this->processMapItem(ParamsMapperProcessor::RULES[$channelName], $params);
